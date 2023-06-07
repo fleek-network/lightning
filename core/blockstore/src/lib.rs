3898837fd7f7942fb49c380594a758217423d0cc
Miguel <miguel@fleek.xyz>
2023-06-06 22:46:59 -0400

    Add blockstore

--- core/blockstore/src/lib.rs
+++ core/blockstore/src/lib.rs
@@ -1,5 +1,77 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::{sync::Arc, collections::HashMap};
+use draco_interfaces::{Blake3Hash, Blake3Tree, BlockStoreInterface, CompressionAlgorithm, CompressionAlgoSet, ConfigConsumer, ContentChunk, IncrementalPutInterface, PutFeedProofError, PutFinalizeError, PutWriteError};
+use serde::{Deserialize, Serialize};
+use parking_lot::RwLock;
+use async_trait::async_trait;
+
+
+#[derive(Serialize, Deserialize, Default)]
+pub struct Config;
+
+#[derive(Clone)]
+struct Blockstore {
+    inner: Arc<RwLock<HashMap<Blake3Hash, Block>>>,
+}
+
+impl ConfigConsumer for Blockstore {
+    const KEY: &'static str = "blockstore";
+    type Config = Config;
+}
+
+struct IncrementalPut;
+
+#[async_trait]
+impl IncrementalPutInterface for IncrementalPut {
+    fn feed_proof(&mut self, _proof: &[u8]) -> Result<(), PutFeedProofError> {
+        todo!()
+    }
+
+    fn write(&mut self, _content: &[u8], _compression: CompressionAlgorithm) -> Result<(), PutWriteError> {
+        todo!()
+    }
+
+    fn is_finished(&self) -> bool {
+        todo!()
+    }
+
+    async fn finalize(self) -> Result<Blake3Hash, PutFinalizeError> {
+        todo!()
+    }
+}
+
+pub enum Block {
+    Tree(Arc<Blake3Tree>),
+    Chunk(Arc<ContentChunk>),
+}
+
+#[async_trait]
+impl BlockStoreInterface for Blockstore {
+    type SharedPointer<T: ?Sized + Send + Sync> = Arc<T>;
+    type Put = IncrementalPut;
+
+    async fn init(_: Self::Config) -> anyhow::Result<Self> {
+        Ok(Self {
+            inner: Default::default()
+        })
+    }
+
+    async fn get_tree(&self, cid: &Blake3Hash) -> Option<Self::SharedPointer<Blake3Tree>> {
+        match self.inner.read().get(cid)? {
+            Block::Tree(tree) => Some(tree.clone()),
+            Block::Chunk(_) => None,
+        }
+    }
+
+    async fn get(&self, _block_counter: u32, block_hash: &Blake3Hash, _compression: CompressionAlgoSet) -> Option<Self::SharedPointer<ContentChunk>> {
+        match self.inner.read().get(block_hash)? {
+            Block::Chunk(chunk) => Some(chunk.clone()),
+            Block::Tree(_) => None,
+        }
+    }
+
+    fn put(&self, _cid: Option<Blake3Hash>) -> Self::Put {
+        todo!()
+    }
 }
 
 #[cfg(test)]
@@ -7,8 +79,8 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    fn put_basic() {}
+
+    #[test]
+    fn get_basic() {}
 }
