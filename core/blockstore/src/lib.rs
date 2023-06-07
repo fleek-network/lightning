62209a7d9c41868ccca2f682b58da50476e9cbfd
Miguel <miguel@fleek.xyz>
2023-06-07 15:26:50 -0400

    Move memory blockstore to its own file

--- core/blockstore/src/lib.rs
+++ core/blockstore/src/lib.rs
@@ -1,28 +1,16 @@
 mod config;
+mod memory;
 
-use std::{collections::HashMap, sync::Arc};
-
+use std::sync::Arc;
 use async_trait::async_trait;
 use draco_interfaces::{
-    Blake3Hash, Blake3Tree, BlockStoreInterface, CompressionAlgoSet, CompressionAlgorithm,
-    ConfigConsumer, ContentChunk, IncrementalPutInterface, PutFeedProofError, PutFinalizeError,
+    Blake3Hash, Blake3Tree, CompressionAlgorithm,
+    ContentChunk, IncrementalPutInterface, PutFeedProofError, PutFinalizeError,
     PutWriteError,
 };
-use parking_lot::RwLock;
-use crate::config::Config;
 
 #[derive(Hash, Eq, PartialEq)]
 pub struct Key<'a>(&'a Blake3Hash, Option<u32>);
-
-#[derive(Clone)]
-struct MemoryBlockStore {
-    inner: Arc<RwLock<HashMap<Key<'static>, Block>>>,
-}
-
-impl ConfigConsumer for MemoryBlockStore {
-    const KEY: &'static str = "blockstore";
-    type Config = Config;
-}
 
 struct IncrementalPut;
 
@@ -52,47 +40,4 @@ impl IncrementalPutInterface for IncrementalPut {
 pub enum Block {
     Tree(Arc<Blake3Tree>),
     Chunk(Arc<ContentChunk>),
-}
-
-#[async_trait]
-impl BlockStoreInterface for MemoryBlockStore {
-    type SharedPointer<T: ?Sized + Send + Sync> = Arc<T>;
-    type Put = IncrementalPut;
-
-    async fn init(_: Self::Config) -> anyhow::Result<Self> {
-        Ok(Self {
-            inner: Default::default(),
-        })
-    }
-
-    async fn get_tree(&self, cid: &Blake3Hash) -> Option<Self::SharedPointer<Blake3Tree>> {
-        match self.inner.read().get(&Key(cid, None))? {
-            Block::Tree(tree) => Some(tree.clone()),
-            _ => None,
-        }
-    }
-
-    async fn get(
-        &self,
-        block_counter: u32,
-        block_hash: &Blake3Hash,
-        _compression: CompressionAlgoSet,
-    ) -> Option<Self::SharedPointer<ContentChunk>> {
-        match self
-            .inner
-            .read()
-            .get(&Key(block_hash, Some(block_counter)))?
-        {
-            Block::Chunk(chunk) => Some(chunk.clone()),
-            _ => None,
-        }
-    }
-
-    fn put(&self, _cid: Option<Blake3Hash>) -> Self::Put {
-        todo!()
-    }
-}
-
-#[cfg(test)]
-mod tests {
 }
---
+++ core/blockstore/src/memory.rs
@@ -0,0 +1,54 @@
+use std::{collections::HashMap, sync::Arc};
+use async_trait::async_trait;
+use parking_lot::RwLock;
+use draco_interfaces::{Blake3Hash, Blake3Tree, BlockStoreInterface, CompressionAlgoSet, ConfigConsumer, ContentChunk};
+use crate::{config::Config, Block, Key, IncrementalPut};
+
+#[derive(Clone)]
+struct MemoryBlockStore {
+    inner: Arc<RwLock<HashMap<Key<'static>, Block>>>,
+}
+
+impl ConfigConsumer for MemoryBlockStore {
+    const KEY: &'static str = "blockstore";
+    type Config = Config;
+}
+
+#[async_trait]
+impl BlockStoreInterface for MemoryBlockStore {
+    type SharedPointer<T: ?Sized + Send + Sync> = Arc<T>;
+    type Put = IncrementalPut;
+
+    async fn init(_: Self::Config) -> anyhow::Result<Self> {
+        Ok(Self {
+            inner: Default::default(),
+        })
+    }
+
+    async fn get_tree(&self, cid: &Blake3Hash) -> Option<Self::SharedPointer<Blake3Tree>> {
+        match self.inner.read().get(&Key(cid, None))? {
+            Block::Tree(tree) => Some(tree.clone()),
+            _ => None,
+        }
+    }
+
+    async fn get(
+        &self,
+        block_counter: u32,
+        block_hash: &Blake3Hash,
+        _compression: CompressionAlgoSet,
+    ) -> Option<Self::SharedPointer<ContentChunk>> {
+        match self
+            .inner
+            .read()
+            .get(&Key(block_hash, Some(block_counter)))?
+        {
+            Block::Chunk(chunk) => Some(chunk.clone()),
+            _ => None,
+        }
+    }
+
+    fn put(&self, _cid: Option<Blake3Hash>) -> Self::Put {
+        todo!()
+    }
+}
