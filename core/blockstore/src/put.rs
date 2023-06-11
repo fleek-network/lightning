9729aacb2be788679a4b3f0a3f58ab7a1d2b9d7a
Miguel <miguel@fleek.xyz>
2023-06-11 14:42:41 -0400

    Add a mode to inceremental putter

--- core/blockstore/src/put.rs
+++ core/blockstore/src/put.rs
@@ -19,22 +19,41 @@ struct Chunk {
 }
 
 pub struct IncrementalPut {
-    store: MemoryBlockStore,
-    proof: Option<Vec<Blake3Hash>>,
-    root: Option<Blake3Hash>,
-    tree_builder: Option<HashTreeBuilder>,
-    stack: Vec<Chunk>,
-    block_counter: u32,
+    // Proof.
+    // proof: Option<Vec<Blake3Hash>>,
+    // TRee builder.
+    // tree_builder: Option<HashTreeBuilder>,
+
+    // Needed.
     buf: BytesMut,
+    stack: Vec<Chunk>,
+    store: MemoryBlockStore,
+    // root: Option<Blake3Hash>,
+
+    // We might be able to just use the length of the stack.
+    block_counter: u32,
+
+    mode: Mode,
+}
+
+enum Mode {
+    // Both need to return root.
+    Verify {
+        proof: Vec<Blake3Hash>,
+        root: Blake3Hash,
+    },
+    Trust {
+        tree_builder: HashTreeBuilder,
+    },
 }
 
 impl IncrementalPut {
     pub fn new(root: Option<Blake3Hash>, store: MemoryBlockStore) -> Self {
         Self {
             store,
-            root,
-            proof: None,
-            tree_builder: Some(HashTreeBuilder::new()),
+            mode: Mode::Trust {
+                tree_builder: HashTreeBuilder::new(),
+            },
             stack: Vec::new(),
             block_counter: 0,
             buf: BytesMut::new(),
@@ -45,19 +64,21 @@ impl IncrementalPut {
 #[async_trait]
 impl IncrementalPutInterface for IncrementalPut {
     fn feed_proof(&mut self, proof: &[u8]) -> Result<(), PutFeedProofError> {
-        if (self.buf.len() > 0 || self.block_counter > 0) && self.proof.is_none() {
+        if self.buf.len() > 0 || self.stack.len() > 0 {
             return Err(PutFeedProofError::UnexpectedCall);
         }
         let root = proof
             .try_into()
             .map_err(|_| PutFeedProofError::InvalidProof)?;
         match self.store.inner.read().get(&Key(root, None)) {
-            None | Some(Block::Chunk(_)) => Err(PutFeedProofError::InvalidProof),
             Some(Block::Tree(tree)) => {
-                self.proof = Some(tree.clone().0.clone());
-                self.root = Some(root);
+                self.mode = Mode::Verify {
+                    proof: tree.clone().0.clone(),
+                    root,
+                };
                 Ok(())
             },
+            _ => Err(PutFeedProofError::InvalidProof),
         }
     }
 
@@ -69,50 +90,66 @@ impl IncrementalPutInterface for IncrementalPut {
         self.buf.put(content);
 
         while self.buf.len() >= 256 * 1024 {
-            let content = self.buf.split_to(256 * 1024);
-            let block = {
-                let mut block = BlockHasher::new();
-                block.set_block(self.block_counter as usize);
-                block.update(content.as_ref());
-                block
-            };
+            let chunk = self.buf.split_to(256 * 1024);
+            let mut block = BlockHasher::new();
+            block.set_block(self.block_counter as usize);
+            block.update(chunk.as_ref());
 
-            let chunk = match self.proof.as_ref() {
-                Some(proof) => {
-                    self.tree_builder = None;
-                    let root = self.root.clone().expect("Root hash to have been provided");
-                    let mut verifier = IncrementalVerifier::new(root, self.block_counter as usize);
+            match &mut self.mode {
+                Mode::Verify { proof, root } => {
+                    let mut verifier =
+                        IncrementalVerifier::new(root.clone(), self.block_counter as usize);
                     let proof_buf = ProofBuf::new(proof, self.block_counter as usize);
                     verifier
                         .feed_proof(proof_buf.as_slice())
                         .map_err(|_| PutWriteError::InvalidContent)?;
                     verifier.verify(block.clone()).unwrap();
-                    let hash = block.finalize(true); // Is this arg always true?
-                    Chunk {
-                        hash,
-                        content: ContentChunk {
-                            compression,
-                            content: content.to_vec(),
-                        },
-                    }
                 },
-                None => {
-                    let hash = block.finalize(true); // Is this arg always true?
-                    let tree_builder = self
-                        .tree_builder
-                        .as_mut()
-                        .expect("There to be a tree builder");
-                    tree_builder.update(content.as_ref());
-                    Chunk {
-                        hash,
-                        content: ContentChunk {
-                            compression,
-                            content: content.to_vec(),
-                        },
-                    }
+                Mode::Trust { tree_builder } => tree_builder.update(chunk.as_ref()),
+            }
+
+            let hash = block.finalize(true); // Is this arg always true?
+            // let chunk = match self.proof.as_ref() {
+            //     Some(proof) => {
+            //         let root = self.root.clone().expect("Root hash to have been provided");
+            //         let mut verifier = IncrementalVerifier::new(root, self.block_counter as
+            // usize);         let proof_buf = ProofBuf::new(proof, self.block_counter
+            // as usize);         verifier
+            //             .feed_proof(proof_buf.as_slice())
+            //             .map_err(|_| PutWriteError::InvalidContent)?;
+            //         verifier.verify(block.clone()).unwrap();
+            //         let hash = block.finalize(true); // Is this arg always true?
+            //         Chunk {
+            //             hash,
+            //             content: ContentChunk {
+            //                 compression,
+            //                 content: content.to_vec(),
+            //             },
+            //         }
+            //     },
+            //     None => {
+            //         let hash = block.finalize(true); // Is this arg always true?
+            //         let tree_builder = self
+            //             .tree_builder
+            //             .as_mut()
+            //             .expect("There to be a tree builder");
+            //         tree_builder.update(content.as_ref());
+            //         Chunk {
+            //             hash,
+            //             content: ContentChunk {
+            //                 compression,
+            //                 content: content.to_vec(),
+            //             },
+            //         }
+            //     },
+            // };
+            self.stack.push(Chunk {
+                hash,
+                content: ContentChunk {
+                    compression,
+                    content: chunk.to_vec(),
                 },
-            };
-            self.stack.push(chunk);
+            });
             self.block_counter += 1
         }
 
@@ -131,15 +168,17 @@ impl IncrementalPutInterface for IncrementalPut {
                 Block::Chunk(Arc::new(chunk.content)),
             );
         }
-        if let Some(tree_builder) = self.tree_builder {
-            let hash_tree = tree_builder.finalize();
-            self.store.inner.write().insert(
-                Key(Blake3Hash::from(hash_tree.hash), None),
-                Block::Tree(Arc::new(Blake3Tree(hash_tree.tree))),
-            );
-            Ok(Blake3Hash::from(hash_tree.hash))
-        } else {
-            self.root.ok_or_else(|| PutFinalizeError::PartialContent)
+
+        match self.mode {
+            Mode::Verify { root, .. } => Ok(root),
+            Mode::Trust { tree_builder } => {
+                let hash_tree = tree_builder.finalize();
+                self.store.inner.write().insert(
+                    Key(Blake3Hash::from(hash_tree.hash), None),
+                    Block::Tree(Arc::new(Blake3Tree(hash_tree.tree))),
+                );
+                Ok(Blake3Hash::from(hash_tree.hash))
+            },
         }
     }
 }
