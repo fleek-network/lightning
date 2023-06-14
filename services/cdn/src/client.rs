3fba30dded8a588bf3f2b6fa0881e9c176596c5d
ozwaldorf <self@ossian.dev>
2023-06-13 23:04:38 -0400

    feat(cdn): initial client with optimistic and raw modes

---
+++ services/cdn/src/client.rs
@@ -0,0 +1,125 @@
+use anyhow::{anyhow, Result};
+use blake3_tree::{blake3::tree::BlockHasher, IncrementalVerifier};
+use fleek_crypto::ClientSignature;
+use tokio::io::{AsyncRead, AsyncWrite};
+
+use crate::connection::{consts::RESPONSE_BLOCK_TAG, CdnConnection, CdnFrame, ServiceMode};
+
+pub struct CdnClient<R: AsyncRead + Unpin, W: AsyncWrite + Unpin> {
+    conn: CdnConnection<R, W>,
+}
+
+impl<R: AsyncRead + Unpin, W: AsyncWrite + Unpin> CdnClient<R, W> {
+    pub fn new(reader: R, writer: W) -> Self {
+        let conn = CdnConnection::new(reader, writer);
+        Self { conn }
+    }
+
+    pub async fn request(&mut self, service_mode: ServiceMode, hash: [u8; 32]) -> Result<()> {
+        // Send request
+        self.conn
+            .write_frame(CdnFrame::Request { service_mode, hash })
+            .await?;
+
+        let mut verifier = IncrementalVerifier::new(hash, 0);
+        let mut block = 0;
+
+        // response loop
+        loop {
+            // Recieve response
+            match self.conn.read_frame(Some(RESPONSE_BLOCK_TAG)).await? {
+                Some(CdnFrame::ResponseBlock {
+                    // TODO: Implement compression support
+                    compression: _,
+                    bytes_len,
+                    proof_len,
+                }) => {
+                    match service_mode {
+                        ServiceMode::Tentative => todo!(),
+                        ServiceMode::Optimistic => {
+                            // Receive proof
+                            if proof_len != 0 {
+                                self.conn.read_buffer(proof_len as usize);
+                                match self.conn.read_frame(None).await? {
+                                    Some(CdnFrame::Buffer(bytes)) => {
+                                        // Feed the verifier the proof
+                                        if let Err(e) = verifier.feed_proof(&bytes) {
+                                            return Err(anyhow!("error feeding proof: {e:?}"));
+                                        }
+                                    },
+                                    Some(_) => unreachable!(), // Guaranteed by read_buffer()
+                                    None => return Err(anyhow!("connection disconnected")),
+                                }
+                            }
+
+                            // Recieve block
+                            self.conn.read_buffer(bytes_len as usize);
+                            match self.conn.read_frame(None).await? {
+                                Some(CdnFrame::Buffer(bytes)) => {
+                                    // Verify raw data chunk
+                                    let mut hasher = BlockHasher::new();
+                                    hasher.set_block(block);
+                                    hasher.update(&bytes);
+                                    if let Err(e) = verifier.verify(hasher) {
+                                        return Err(anyhow!("error verifying content: {e:?}"));
+                                    }
+                                },
+                                Some(_) => unreachable!(), // Guaranteed by read_buffer()
+                                None => return Err(anyhow!("connection disconnected")),
+                            }
+
+                            // Send delivery acknowledgement
+                            self.conn
+                                .write_frame(CdnFrame::DeliveryAcknowledgement {
+                                    signature: ClientSignature,
+                                })
+                                .await?;
+                        },
+                        ServiceMode::Raw => {
+                            // Receive proof
+                            if proof_len != 0 {
+                                self.conn.read_buffer(proof_len as usize);
+                                match self.conn.read_frame(None).await? {
+                                    Some(CdnFrame::Buffer(bytes)) => {
+                                        // Feed the verifier the proof
+                                        if let Err(e) = verifier.feed_proof(&bytes) {
+                                            return Err(anyhow!("error feeding proof: {e:?}"));
+                                        }
+                                    },
+                                    Some(_) => unreachable!(), // Guaranteed by read_buffer()
+                                    None => return Err(anyhow!("connection disconnected")),
+                                }
+                            }
+
+                            // Recieve block
+                            self.conn.read_buffer(bytes_len as usize);
+                            match self.conn.read_frame(None).await? {
+                                Some(CdnFrame::Buffer(bytes)) => {
+                                    // Verify raw data chunk
+                                    let mut hasher = BlockHasher::new();
+                                    hasher.set_block(block);
+                                    hasher.update(&bytes);
+                                    if let Err(e) = verifier.verify(hasher) {
+                                        return Err(anyhow!("error verifying content: {e:?}"));
+                                    }
+                                },
+                                Some(_) => unreachable!(), // Guaranteed by read_buffer()
+                                None => return Err(anyhow!("connection disconnected")),
+                            }
+                        },
+                    }
+
+                    if verifier.is_done() {
+                        break;
+                    } else {
+                        block += 1;
+                    }
+                },
+                Some(_) => unreachable!(),
+                None => return Err(anyhow!("connection disconnected")),
+            }
+        }
+
+        Ok(())
+    }
+}
