3fc7a13cc80d10e530fb7c6b0c1bf25c47f3080e
Matthias Wright <matthias.s.wright@gmail.com>
2023-06-15 16:57:59 +0300

    feat: add notifier interface

---
+++ core/interfaces/src/notifier.rs
@@ -0,0 +1,23 @@
+use std::time::Duration;
+
+use async_trait::async_trait;
+use tokio::sync::mpsc;
+
+use crate::application::SyncQueryRunnerInterface;
+
+#[derive(Debug)]
+pub enum Notification {
+    NewEpoch,
+    BeforeEpochChange,
+}
+
+#[async_trait]
+pub trait NotifierInterface {
+    type SyncQuery: SyncQueryRunnerInterface;
+
+    fn init(query_runner: Self::SyncQuery) -> Self;
+
+    fn notify_on_new_epoch(&self, tx: mpsc::Sender<Notification>);
+
+    fn notify_before_epoch_change(&self, duration: Duration, tx: mpsc::Sender<Notification>);
+}
