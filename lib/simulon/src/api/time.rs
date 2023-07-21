f155ed7f867208b632c954c7b7e0fa995340e2a6
Parsa Ghadimi <i@parsa.ooo>
2023-07-21 12:11:33 -0400

    add a todo sleep function

--- lib/simulon/src/api/time.rs
+++ lib/simulon/src/api/time.rs
@@ -1,6 +1,10 @@
+use std::time::Duration;
+
 use crate::state::with_node;
 
 /// Returns the current time.
 pub fn now() -> u128 {
     with_node(|n| n.now())
 }
+
+pub async fn sleep(_time: Duration) {}
