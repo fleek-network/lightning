c0048fa67fe291532513289bbf60eb0bdda507a9
Parsa Ghadimi <i@parsa.ooo>
2023-08-01 15:59:41 -0400

    add untrusted stream for origin

--- core/interfaces/src/origin.rs
+++ core/interfaces/src/origin.rs
@@ -1,9 +1,20 @@
 /// The abstraction layer for different origins and how we handle them in the codebase in
 /// a modular way, and [`OriginProvider`] can be something like a provider for resolving
 /// *IPFS* files.
-pub trait OriginProviderInterface<Stream: tokio_stream::Stream<Item = bytes::BytesMut>>:
-    Send + Sync
-{
-    /// Fetch the content from the provided `uri`.
+pub trait OriginProviderInterface<Stream: UntrustedStream>: Send + Sync {
+    /// Fetch the content from the provided `uri`. A uri is anything the origin
+    /// accepts.
     fn fetch(&self, uri: &[u8]) -> Stream;
+}
+
+/// An untrusted stream to an origin, this allows the origin provider to start the
+/// streaming of the content it receives before it is sure of the integrity of the
+/// content.
+///
+/// This way we will not have to store the entire data in memory for the sake of
+/// verification.
+pub trait UntrustedStream: tokio_stream::Stream<Item = bytes::BytesMut> {
+    /// Returns true if the stream was valid. This is meant to be called at the end
+    /// of the stream.
+    fn was_content_valid(&self) -> bool;
 }
