67e7e9c23459c1e50a0e29c2722482bba6887068
Parsa Ghadimi <i@parsa.ooo>
2023-08-03 16:06:16 -0400

    add schema crate

---
+++ core/schema/src/lib.rs
@@ -0,0 +1,55 @@
+//! The network messages for Fleek Network. The point of this crate is to have one common
+//! place where we define the schema for the messages that are part of the network stack.
+//!
+//! Although serde is great, we want to make it easy to use a schema-based encoder/decoder
+//! in future (such as flatbuffers or protobuff).
+//!
+//! However that task can be extra development effort at the current stage, so we would
+//! rather use serde for the time being. This crate serves as a layer of abstraction
+//! that works over serde + flexbuffers (schema-less cousin of flatbuffers). And makes
+//! the entire encoding/decoding effort hot-swapable in one place.
+//!
+//! Anywhere we need to care about serialization and deserialization of messages we can
+//! rely on the [`LightningMessage`] trait to use the encoding and decoding.
+
+use std::io::Write;
+
+use serde::{Deserialize, Serialize};
+
+pub mod broadcast;
+pub mod dht;
+
+/// Any networking message that can be serialized and deserialized.
+pub trait LightningMessage: Sized {
+    /// Decode the message. Returns an error if we can't deserialize.
+    fn decode(buffer: &[u8]) -> anyhow::Result<Self>;
+
+    /// Encode the message. Return the result from the `writer.write`
+    fn encode<W: Write>(&self, writer: &mut W) -> std::io::Result<usize>;
+}
+
+/// A marker type which enables auto implementation for serde serialize and deserialize. This
+/// allows us to skip the auto-trait implementation in case we prefer a custom serialization
+/// for a single type. But any other type should have the `impl AutoImplSerde for T`.
+pub trait AutoImplSerde: Serialize + for<'a> Deserialize<'a> + Sized {}
+
+// Auto implement the lighting message for any serde type using flex buffer.
+// This is to allow us to move forward without a fixed schema for the time being and to
+// have a faster development. However an schema based approach should be preferred in
+// future. This
+
+impl<T: AutoImplSerde> LightningMessage for T {
+    #[inline]
+    fn decode(buffer: &[u8]) -> anyhow::Result<Self> {
+        let der = flexbuffers::Reader::get_root(buffer)?;
+        let tmp = Self::deserialize(der)?;
+        Ok(tmp)
+    }
+
+    #[inline]
+    fn encode<W: Write>(&self, writer: &mut W) -> std::io::Result<usize> {
+        let mut ser = flexbuffers::FlexbufferSerializer::new();
+        self.serialize(&mut ser).expect("Failed to serialize");
+        writer.write(ser.view())
+    }
+}
