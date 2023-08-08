f2ae56d5b055eb14c03d940c934688dfe30de189
Matthias Wright <matthias.s.wright@gmail.com>
2023-08-08 21:13:31 +0200

    test(origin-ipfs): add dummy ipfs gateway to avoid unit test depending on internet connection

--- core/origin-ipfs/src/tests.rs
+++ core/origin-ipfs/src/tests.rs
@@ -3,39 +3,61 @@ use cid::{
     Cid,
 };
 use lightning_interfaces::{OriginProviderInterface, UntrustedStream, WithStartAndShutdown};
+use lightning_test_utils::ipfs_gateway::spawn_gateway;
 use tokio::io::AsyncReadExt;
 use tokio_util::io::StreamReader;
 
-use crate::{config::Config, IPFSOrigin};
+use crate::{
+    config::{Config, Gateway, Protocol},
+    IPFSOrigin,
+};
 
 #[tokio::test]
 async fn test_origin() {
     let req_cid =
         Cid::try_from("bafybeigdyrzt5sfp7udm7hu76uh7y26nf3efuylqabf3oclgtqy55fbzdi").unwrap();
-    let ipfs_origin = IPFSOrigin::init(Config::default()).await.unwrap();
-    ipfs_origin.start().await;
+    let mut config = Config::default();
 
-    let socket = ipfs_origin.get_socket();
-    let stream = socket.run(req_cid.to_bytes()).await.unwrap().unwrap();
+    let req_fut = async move {
+        config.gateways.push(Gateway {
+            protocol: Protocol::Http,
+            authority: "127.0.0.1:30100".to_string(),
+        });
+        let ipfs_origin = IPFSOrigin::init(config).await.unwrap();
+        ipfs_origin.start().await;
 
-    assert_eq!(stream.was_content_valid(), None);
+        let socket = ipfs_origin.get_socket();
+        let stream = socket.run(req_cid.to_bytes()).await.unwrap().unwrap();
 
-    let mut read = StreamReader::new(stream);
-    let mut buf = [0; 1024];
-    let mut bytes: Vec<u8> = Vec::new();
-    loop {
-        let len = read.read(&mut buf).await.unwrap();
-        bytes.extend(&buf[..len]);
-        if len == 0 {
-            break;
+        assert_eq!(stream.was_content_valid(), None);
+
+        let mut read = StreamReader::new(stream);
+        let mut buf = [0; 1024];
+        let mut bytes: Vec<u8> = Vec::new();
+        loop {
+            let len = read.read(&mut buf).await.unwrap();
+            bytes.extend(&buf[..len]);
+            if len == 0 {
+                break;
+            }
         }
+        assert!(
+            Code::try_from(req_cid.hash().code())
+                .ok()
+                .map(|code| &code.digest(&bytes) == req_cid.hash())
+                .unwrap()
+        );
+    };
+
+    tokio::select! {
+        _ = spawn_gateway(30100) => {
+
+        }
+        _ = req_fut => {
+
+        }
+
     }
-    assert!(
-        Code::try_from(req_cid.hash().code())
-            .ok()
-            .map(|code| &code.digest(&bytes) == req_cid.hash())
-            .unwrap()
-    );
 }
 
 #[tokio::test]
--- core/test-utils/src/lib.rs
+++ core/test-utils/src/lib.rs
@@ -1,5 +1,6 @@
 pub mod app;
 pub mod blockstore;
 pub mod consensus;
+pub mod ipfs_gateway;
 pub mod random;
 pub mod reputation;
