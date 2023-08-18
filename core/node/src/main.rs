9994f50a200a7e8137b29d991f5ac4502c9354ff
Parsa Ghadimi <i@parsa.ooo>
2023-08-18 00:01:03 -0400

    remove template dir

--- core/node/src/main.rs
+++ core/node/src/main.rs
@@ -2,7 +2,6 @@ mod cli;
 mod config;
 mod node;
 mod shutdown;
-mod template;
 
 use std::fs::File;
 
@@ -11,7 +10,7 @@ use chrono::Local;
 use clap::Parser;
 use cli::Cli;
 use lightning_interfaces::{
-    transformers, ApplicationInterface, BroadcastInterface, LightningTypes,
+    ApplicationInterface, BroadcastInterface, LightningTypes,
 };
 use log::LevelFilter;
 use mock::consensus::MockConsensus;
@@ -70,15 +69,15 @@ async fn main() -> Result<()> {
     if args.with_mock_consensus {
         log::info!("Using MockConsensus");
 
-        type Node = transformers::WithConsensus<
-            FinalTypes,
-            MockConsensus<
-                <<FinalTypes as LightningTypes>::Application as ApplicationInterface>::SyncExecutor,
-                <<FinalTypes as LightningTypes>::Broadcast as BroadcastInterface>::PubSub<()>,
-            >,
-        >;
-
-        Cli::<Node>::new(args).exec().await
+        // type Node = transformers::WithConsensus<
+        //     FinalTypes,
+        //     MockConsensus<
+        //         <<FinalTypes as LightningTypes>::Application as ApplicationInterface>::SyncExecutor,
+        //         <<FinalTypes as LightningTypes>::Broadcast as BroadcastInterface>::PubSub<()>,
+        //     >,
+        // >;
+        // Cli::<Node>::new(args).exec().await
+        todo!("partial! should work for reassignment.")
     } else {
         Cli::<FinalTypes>::new(args).exec().await
     }
--- core/node/src/template/mod.rs
+++
@@ -1,6 +0,0 @@
-pub mod broadcast;
-pub mod config;
-pub mod origin;
-pub mod pod;
-pub mod pool;
-pub mod topology;
