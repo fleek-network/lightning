956d2c86b64b1ff947ccaf51886f845329a8dcb6
Parsa Ghadimi <i@parsa.ooo>
2023-08-29 13:15:17 -0400

    implement hello world service

--- services/ping/src/lib.rs
+++ services/ping/src/lib.rs
@@ -22,10 +22,25 @@ pub fn on_event_response(args: OnEventResponseArgs) {
 // ---- END OF SDK SETUP --->
 
 #[no_mangle]
-pub fn on_message(_args: OnMessageArgs) {}
+pub fn on_connected(args: OnConnectedArgs) {
+    println!(
+        "connection[{}]: connected '{}'",
+        args.connection_id, args.client
+    );
+}
 
 #[no_mangle]
-pub fn on_connected(_args: OnConnectedArgs) {}
+pub fn on_message(args: OnMessageArgs) {
+    println!(
+        "connection[{}]: message '{:?}'",
+        args.connection_id, args.payload
+    );
+
+    // send the hello world message.
+    fn_sdk::api::connection_send(args.connection_id, b"Hello World".to_vec());
+}
 
 #[no_mangle]
-pub fn on_disconnected(_args: OnDisconnectedArgs) {}
+pub fn on_disconnected(args: OnDisconnectedArgs) {
+    println!("connection[{}]: disconnected", args.connection_id);
+}
