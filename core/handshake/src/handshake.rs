4d692ccf89173fa8fc8c32f51176591b7ab49e78
Parsa Ghadimi <i@parsa.ooo>
2023-08-30 00:46:33 -0400

    add interface shell for handshake

--- core/handshake/src/handshake.rs
+++ core/handshake/src/handshake.rs
@@ -1,7 +1,36 @@
+use async_trait::async_trait;
+use lightning_interfaces::infu_collection::Collection;
+use lightning_interfaces::{ConfigConsumer, HandshakeInterface, WithStartAndShutdown};
+use serde::{Deserialize, Serialize};
+
 use crate::transports::Transport;
 
-pub struct Context {
-    //..
+pub struct Handshake<C: Collection> {
+    collection: C,
 }
 
-fn spawn_transport_loop<T: Transport>(config: T::Config) {}
+#[derive(Default, Serialize, Deserialize)]
+pub struct HandshakeConfig {}
+
+impl<C: Collection> HandshakeInterface<C> for Handshake<C> {
+    #[doc = " Initialize a new handshake server."]
+    fn init(config: Self::Config) -> anyhow::Result<Self> {
+        todo!()
+    }
+}
+
+impl<C: Collection> ConfigConsumer for Handshake<C> {
+    const KEY: &'static str = "handshake";
+    type Config = HandshakeConfig;
+}
+
+#[async_trait]
+impl<C: Collection> WithStartAndShutdown for Handshake<C> {
+    fn is_running(&self) -> bool {
+        true
+    }
+
+    async fn start(&self) {}
+
+    async fn shutdown(&self) {}
+}
