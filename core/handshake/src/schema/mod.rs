37bebf8f18da7ffb4ee1afe0a7cab950f3a7d21f
ozwaldorf <self@ossian.dev>
2023-08-29 21:55:42 -0400

    feat(handshake): impl encode and decode for frames

--- core/handshake/src/schema/mod.rs
+++ core/handshake/src/schema/mod.rs
@@ -1,3 +1,7 @@
+use anyhow::{anyhow, Result};
+use arrayref::array_ref;
+use arrayvec::ArrayVec;
+use bytes::{BufMut, Bytes};
 use fleek_crypto::{ClientPublicKey, ClientSignature, NodePublicKey, NodeSignature};
 use lightning_interfaces::types::ServiceId;
 
@@ -5,23 +9,120 @@ pub struct ChallengeFrame {
     pub challenge: [u8; 32],
 }
 
+#[derive(Debug, PartialEq, Eq)]
 pub enum HandshakeRequestFrame {
     Handshake {
+        retry: Option<u64>,
+        service: ServiceId,
         pk: ClientPublicKey,
         pop: ClientSignature,
-        service: ServiceId,
-        retry: Option<u64>,
     },
     ContinueRequest {
         access_token: [u8; 48],
     },
 }
 
+impl HandshakeRequestFrame {
+    pub fn encode(&self) -> Bytes {
+        match self {
+            HandshakeRequestFrame::Handshake {
+                retry,
+                service,
+                pk,
+                pop,
+            } => {
+                // reserve capacity for the bigger variant
+                let mut buf = Vec::with_capacity(157);
+                match retry {
+                    None => buf.put_u8(0x00),
+                    Some(id) => {
+                        buf.put_u8(0x01);
+                        buf.put_u64(*id);
+                    },
+                }
+                buf.put_u32(*service);
+                buf.put_slice(&pk.0);
+                buf.put_slice(&pop.0);
+                buf.into()
+            },
+            HandshakeRequestFrame::ContinueRequest { access_token } => {
+                let mut buf = Vec::with_capacity(49);
+                buf.put_u8(0x02);
+                buf.put_slice(access_token);
+                buf.into()
+            },
+        }
+    }
+
+    pub fn decode(bytes: &[u8]) -> Result<Self> {
+        match bytes[0] {
+            0x00 => {
+                if bytes.len() != 149 {
+                    return Err(anyhow!("wrong number of bytes"));
+                }
+                let service = u32::from_be_bytes(*array_ref!(bytes, 1, 4));
+                let pk = ClientPublicKey(*array_ref!(bytes, 5, 96));
+                let pop = ClientSignature(*array_ref!(bytes, 101, 48));
+                Ok(Self::Handshake {
+                    pk,
+                    pop,
+                    service,
+                    retry: None,
+                })
+            },
+            0x01 => {
+                if bytes.len() != 157 {
+                    return Err(anyhow!("wrong number of bytes"));
+                }
+                let retry = Some(u64::from_be_bytes(*array_ref!(bytes, 1, 8)));
+                let service = u32::from_be_bytes(*array_ref!(bytes, 9, 4));
+                let pk = ClientPublicKey(*array_ref!(bytes, 13, 96));
+                let pop = ClientSignature(*array_ref!(bytes, 109, 48));
+                Ok(Self::Handshake {
+                    retry,
+                    service,
+                    pk,
+                    pop,
+                })
+            },
+            0x80 => {
+                if bytes.len() != 49 {
+                    return Err(anyhow!("wrong number of bytes"));
+                }
+                let access_token = *array_ref!(bytes, 1, 48);
+                Ok(Self::ContinueRequest { access_token })
+            },
+            _ => Err(anyhow!("invalid frame tag")),
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
 pub struct HandshakeResponse {
     pub pk: NodePublicKey,
     pub pop: NodeSignature,
 }
 
+impl HandshakeResponse {
+    pub fn encode(&self) -> Bytes {
+        let mut buf = Vec::with_capacity(96);
+        buf.put_slice(&self.pk.0);
+        buf.put_slice(&self.pop.0);
+        buf.into()
+    }
+
+    pub fn decode(bytes: &[u8]) -> Result<Self> {
+        if bytes.len() != 96 {
+            return Err(anyhow!("wrong number of bytes"));
+        }
+
+        let pk = NodePublicKey(*array_ref!(bytes, 0, 32));
+        let pop = NodeSignature(*array_ref!(bytes, 32, 64));
+        Ok(Self { pk, pop })
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
 #[non_exhaustive]
 pub enum RequestFrame {
     /// Raw message to be sent to the service implementation.
@@ -30,12 +131,65 @@ pub enum RequestFrame {
     AccessToken { ttl: u64 },
     /// Refresh an access token.
     RefreshAccessToken { access_token: Box<[u8; 48]> },
-    ///
     DeliveryAcknowledgment {
         // TODO
     },
 }
 
+impl RequestFrame {
+    pub fn encode(&self) -> Bytes {
+        match self {
+            Self::ServicePayload { bytes } => {
+                let mut buf = Vec::new();
+                buf.put_u8(0x00);
+                buf.put_slice(bytes);
+                buf.into()
+            },
+            Self::AccessToken { ttl } => {
+                let mut buf = Vec::with_capacity(9);
+                buf.put_u8(0x01);
+                buf.put_u64(*ttl);
+                buf.into()
+            },
+            Self::RefreshAccessToken { access_token } => {
+                let mut buf = Vec::with_capacity(49);
+                buf.put_u8(0x02);
+                buf.put_slice(access_token.as_slice());
+                buf.into()
+            },
+            Self::DeliveryAcknowledgment {} => vec![0x03].into(),
+        }
+    }
+
+    pub fn decode(bytes: &[u8]) -> Result<Self> {
+        match bytes[0] {
+            0x00 => {
+                let bytes = bytes[1..].to_vec().into();
+                Ok(Self::ServicePayload { bytes })
+            },
+            0x01 => {
+                if bytes.len() != 9 {
+                    return Err(anyhow!("wrong number of bytes"));
+                }
+
+                let ttl = u64::from_be_bytes(*array_ref!(bytes, 1, 8));
+                Ok(Self::AccessToken { ttl })
+            },
+            0x02 => {
+                if bytes.len() != 49 {
+                    return Err(anyhow!("wrong number of bytes"));
+                }
+
+                let access_token = Box::new(*array_ref!(bytes, 1, 48));
+                Ok(Self::RefreshAccessToken { access_token })
+            },
+            0x03 => Ok(Self::DeliveryAcknowledgment {}),
+            _ => Err(anyhow!("invalid frame tag")),
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
 #[non_exhaustive]
 pub enum ResponseFrame {
     /// Message to be passed.
@@ -51,11 +205,149 @@ pub enum ResponseFrame {
     },
 }
 
+impl ResponseFrame {
+    pub fn encode(&self) -> Bytes {
+        match self {
+            Self::ServicePayload { bytes } => {
+                let mut buf = Vec::with_capacity(1 + bytes.len());
+                buf.put_u8(0x80);
+                buf.put_slice(bytes);
+                buf.into()
+            },
+            Self::AccessToken { ttl, access_token } => {
+                let mut buf = Vec::with_capacity(57);
+                buf.put_u8(0x81);
+                buf.put_u64(*ttl);
+                buf.put_slice(access_token.as_slice());
+                buf.into()
+            },
+            Self::Termination { reason } => vec![*reason as u8].into(),
+        }
+    }
+
+    pub fn decode(bytes: &[u8]) -> Result<Self> {
+        match bytes[0] {
+            0x80 => {
+                let bytes = bytes[1..].to_vec().into();
+                Ok(Self::ServicePayload { bytes })
+            },
+            0x81 => {
+                if bytes.len() != 57 {
+                    return Err(anyhow!("wrong number of bytes"));
+                }
+                let ttl = u64::from_be_bytes(*array_ref!(bytes, 1, 8));
+                let access_token = Box::new(*array_ref!(bytes, 9, 48));
+                Ok(Self::AccessToken { ttl, access_token })
+            },
+            byte => {
+                if bytes.len() > 1 {
+                    return Err(anyhow!("too many bytes"));
+                }
+                Ok(Self::Termination {
+                    reason: TerminationReason::from_u8(byte),
+                })
+            },
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+#[repr(u8)]
+#[non_exhaustive]
 pub enum TerminationReason {
-    Timeout,
+    Timeout = 0x00,
     InvalidHandshake,
     InvalidToken,
     InvalidDeliveryAcknowledgment,
     InvalidService,
     ServiceTerminated,
+    Unknown = 0xFF,
+}
+
+impl TerminationReason {
+    pub fn from_u8(byte: u8) -> Self {
+        match byte {
+            0x00 => Self::Timeout,
+            0x01 => Self::InvalidHandshake,
+            0x02 => Self::InvalidToken,
+            0x03 => Self::InvalidDeliveryAcknowledgment,
+            0x04 => Self::InvalidService,
+            0x05 => Self::ServiceTerminated,
+            _ => Self::Unknown,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn handshake_frames() {
+        let frame = HandshakeRequestFrame::Handshake {
+            retry: None,
+            service: 0,
+            pk: ClientPublicKey([1; 96]),
+            pop: ClientSignature([2; 48]),
+        };
+        assert_eq!(
+            frame,
+            HandshakeRequestFrame::decode(&frame.encode()).unwrap()
+        );
+
+        let frame = HandshakeRequestFrame::Handshake {
+            retry: Some(99),
+            service: 0,
+            pk: ClientPublicKey([1; 96]),
+            pop: ClientSignature([2; 48]),
+        };
+        assert_eq!(
+            frame,
+            HandshakeRequestFrame::decode(&frame.encode()).unwrap()
+        );
+
+        let frame = HandshakeResponse {
+            pk: NodePublicKey([0; 32]),
+            pop: NodeSignature([0; 64]),
+        };
+        assert_eq!(frame, HandshakeResponse::decode(&frame.encode()).unwrap());
+    }
+
+    #[test]
+    fn request_frames() {
+        let frame = RequestFrame::ServicePayload {
+            bytes: vec![64; 64].into(),
+        };
+        assert_eq!(frame, RequestFrame::decode(&frame.encode()).unwrap());
+
+        let frame = RequestFrame::AccessToken { ttl: 0 };
+        assert_eq!(frame, RequestFrame::decode(&frame.encode()).unwrap());
+
+        let frame = RequestFrame::RefreshAccessToken {
+            access_token: [1; 48].into(),
+        };
+        assert_eq!(frame, RequestFrame::decode(&frame.encode()).unwrap());
+
+        let frame = RequestFrame::DeliveryAcknowledgment {};
+        assert_eq!(frame, RequestFrame::decode(&frame.encode()).unwrap());
+    }
+
+    #[test]
+    fn response_frames() {
+        let frame = ResponseFrame::ServicePayload {
+            bytes: vec![64; 64].into(),
+        };
+        assert_eq!(frame, ResponseFrame::decode(&frame.encode()).unwrap());
+
+        let frame = ResponseFrame::AccessToken {
+            ttl: 0,
+            access_token: [1; 48].into(),
+        };
+        assert_eq!(frame, ResponseFrame::decode(&frame.encode()).unwrap());
+
+        let frame = ResponseFrame::Termination {
+            reason: TerminationReason::Timeout,
+        };
+        assert_eq!(frame, ResponseFrame::decode(&frame.encode()).unwrap());
+    }
 }
