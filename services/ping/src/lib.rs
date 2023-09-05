126ee137c7960591bbf8f6acd406d07c02185a85
Parsa Ghadimi <i@parsa.ooo>
2023-09-05 15:28:07 -0400

    fetch service

--- services/ping/src/lib.rs
+++ services/ping/src/lib.rs
@@ -1,4 +1,6 @@
-//! This is meant to turn into a *very dumb* service to experiment with service loading.
+use dashmap::DashMap;
+use fn_sdk::api::{connection_close, connection_send};
+use fn_sdk::blockstore::HashTree;
 use fn_sdk::internal::{
     OnConnectedArgs,
     OnDisconnectedArgs,
@@ -6,6 +8,8 @@ use fn_sdk::internal::{
     OnMessageArgs,
     OnStartArgs,
 };
+use fxhash::FxBuildHasher;
+use lazy_static::lazy_static;
 
 // --- SDK Setup
 
@@ -21,26 +25,75 @@ pub fn on_event_response(args: OnEventResponseArgs) {
 
 // ---- END OF SDK SETUP --->
 
+lazy_static! {
+    static ref CONNECTIONS: DashMap<u64, Connection, FxBuildHasher> =
+        DashMap::with_capacity_and_hasher(1024, FxBuildHasher::default());
+}
+
+// our webRTC transport does not handle >64KB messages.
+const CHUNK_SIZE: usize = 32 * 1024;
+
+#[derive(Default)]
+struct Connection {
+    tree: Option<HashTree>,
+    cursor: usize,
+}
+
 #[no_mangle]
 pub fn on_connected(args: OnConnectedArgs) {
-    println!(
-        "connection[{}]: connected '{}'",
-        args.connection_id, args.client
-    );
+    CONNECTIONS.insert(args.connection_id, Connection::default());
 }
 
 #[no_mangle]
 pub fn on_message(args: OnMessageArgs) {
-    println!(
-        "connection[{}]: message '{:?}'",
-        args.connection_id, args.payload
-    );
+    fn_sdk::api::spawn(async move {
+        let Some(mut conn) = CONNECTIONS.get_mut(&args.connection_id) else {
+            log::error!("connection not found.");
+            return;
+        };
 
-    // send the hello world message.
-    fn_sdk::api::connection_send(args.connection_id, b"Hello World".to_vec());
+        let connection: &mut Connection = &mut conn;
+
+        let tree = connection.tree.as_ref();
+        let cursor = &mut connection.cursor;
+
+        let Some(tree) = tree else {
+            if args.payload.len() != 32 {
+                connection_close(args.connection_id);
+                return;
+            }
+
+            let Ok(tree) = HashTree::load(arrayref::array_ref![args.payload, 0, 32]).await else {
+                connection_send(args.connection_id, "NOTFOUND".into());
+                connection_close(args.connection_id);
+                return;
+            };
+
+            conn.tree = Some(tree);
+            return;
+        };
+
+        let block_counter = *cursor;
+        if block_counter >= tree.len() {
+            connection_close(args.connection_id);
+            return;
+        }
+        *cursor += 1;
+
+        let Ok(content) = tree.get(block_counter).await else {
+            connection_close(args.connection_id);
+            return;
+        };
+
+        for (i, chunk) in content.chunks(CHUNK_SIZE).enumerate() {
+            let mut vec = chunk.to_vec();
+            vec.push(i as u8);
+            fn_sdk::api::connection_send(args.connection_id, vec);
+        }
+    });
 }
 
 #[no_mangle]
 pub fn on_disconnected(args: OnDisconnectedArgs) {
-    println!("connection[{}]: disconnected", args.connection_id);
+    CONNECTIONS.remove(&args.connection_id);
 }
