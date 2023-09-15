aa8d083735c97e50ec59f57f11d77e9999ab8a5e
Parsa Ghadimi <i@parsa.ooo>
2023-09-15 14:21:53 -0400

    test against testvec: bug found

---
+++ lib/blake3-js/src/blake3_tests.ts
@@ -0,0 +1,26 @@
+import { VECTOR } from "./testvec.ts";
+import { hash } from "./blake3.ts";
+import { assertStrictEquals } from "https://deno.land/std/assert/mod.ts";
+
+function buf2hex(buffer: ArrayLike<number>) {
+  return [...new Uint8Array(buffer)]
+    .map((x) => x.toString(16).padStart(2, "0"))
+    .join("");
+}
+
+Deno.test({
+  name: "TEST VECTOR",
+  fn() {
+    const buffer = new Uint8Array(128 * 1024);
+    for (let i = 0; i < buffer.length; ++i) {
+      buffer[i] = i & 0xff;
+    }
+
+    for (let id = 0; id < VECTOR.length; ++id) {
+      const [size, expectedHash] = VECTOR[id]! as [number, string];
+      const input = new Uint8Array(buffer.buffer, 0, size);
+      const actualHash = buf2hex(hash(input));
+      assertStrictEquals(actualHash, expectedHash, `size=${size}` + input);
+    }
+  },
+});
