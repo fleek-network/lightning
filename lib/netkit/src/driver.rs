49f5b2c44ba902edd1f1653ec09ea6e990a88c47
Miguel <miguel@fleek.xyz>
2023-09-22 18:47:21 -0400

    Remove some waiting in the main loop of driver

--- lib/netkit/src/driver.rs
+++ lib/netkit/src/driver.rs
@@ -5,8 +5,9 @@ use bytes::Bytes;
 use fleek_crypto::NodePublicKey;
 use futures::{SinkExt, StreamExt};
 use log::{error, info};
-use quinn::Connection;
+use quinn::{Connection, ConnectionError, RecvStream, SendStream};
 use tokio::sync::mpsc::{Receiver, Sender};
+use tokio::sync::oneshot;
 use tokio_util::codec::{FramedRead, FramedWrite, LengthDelimitedCodec};
 
 use crate::endpoint::{DriverRequest, Event, Message, ServiceScope};
@@ -59,38 +60,32 @@ pub async fn start_driver(mut ctx: Context) -> Result<()> {
     loop {
         tokio::select! {
             accept_bi = ctx.connection.accept_bi() => {
-                // The peer opened a stream.
-                // The first byte identifies the service.
-                let (tx, mut rx) = accept_bi?;
-                let mut buf = [0u8; 1];
-                rx.read_exact(&mut buf).await?;
-                let service = buf[0];
-                match ctx.network_event_tx.get(&service) {
-                    Some(sender) => {
-                        let event = Event::NewStream { peer: ctx.peer, tx, rx };
-                        if sender.send(event).await.is_err() {
-                            tracing::error!("failed to send incoming network event");
-                        }
-                    },
-                    None => info!("received stream request with invalid service scope: {service}"),
-                }
+                let (tx, rx) = accept_bi?;
+                let network_event_tx = ctx.network_event_tx.clone();
+                let peer = ctx.peer;
+                tokio::spawn(async move {
+                    if let Err(e) =
+                        handle_incoming_streams((tx, rx), network_event_tx, peer).await
+                    {
+                        tracing::error!("failed to handle incoming stream: {e:?}");
+                    }
+                });
             }
             request = ctx.service_request_rx.recv() => {
                 match request {
                     None => break,
                     Some(DriverRequest::Message(message)) => {
+                        // Todo: make this to a worker and use a socket here.
+                        // to avoid waiting on this main loop.
                         message_stream_tx.send(Bytes::from(message)).await?
                     },
                     Some(DriverRequest::NewStream { service, respond }) => {
-                        // We open a new QUIC stream.
-                        let (mut tx, rx) =  ctx.connection.open_bi().await?;
-                        // We send the service scope first so
-                        // that the receiver knows the service
-                        // that this stream belongs to.
-                        tx.write_all(&[service]).await?;
-                        if let Err(e) = respond.send(Ok((tx, rx))) {
-                            tracing::error!("failed to send new stream to client: {e:?}");
-                        }
+                        let connection = ctx.connection.clone();
+                        tokio::spawn(async move {
+                            if let Err(e) = create_stream(connection, respond, service).await {
+                                tracing::error!("failed to create stream: {e:?}");
+                            }
+                        });
                     }
                 }
             }
@@ -101,14 +96,18 @@ pub async fn start_driver(mut ctx: Context) -> Result<()> {
                 };
                 match Message::try_from(message) {
                     Ok(message) => {
-                        match ctx.network_event_tx.get(&message.service) {
+                        match ctx.network_event_tx.get(&message.service).cloned() {
                             Some(sender) => {
-                                if sender
-                                .send(Event::Message{ peer: ctx.peer, message })
-                                .await
-                                .is_err() {
-                                    tracing::error!("failed to send incoming network event");
-                                }
+                                let peer = ctx.peer;
+                                tokio::spawn(async move {
+                                    if sender
+                                        .send(Event::Message{ peer, message })
+                                        .await
+                                        .is_err() {
+                                            tracing::error!("failed to send incoming network event");
+                                        }
+                                });
+
                             },
                             None => info!("received stream request with invalid service scope: {}", message.service),
                         }
@@ -119,4 +118,42 @@ pub async fn start_driver(mut ctx: Context) -> Result<()> {
         }
     }
     Ok(())
+}
+
+async fn handle_incoming_streams(
+    (tx, mut rx): (SendStream, RecvStream),
+    network_event_tx: HashMap<ServiceScope, Sender<Event>>,
+    peer: NodePublicKey,
+) -> Result<()> {
+    // The peer opened a stream.
+    // The first byte identifies the service.
+    let mut buf = [0u8; 1];
+    rx.read_exact(&mut buf).await?;
+    let service = buf[0];
+    match network_event_tx.get(&service) {
+        Some(sender) => {
+            let event = Event::NewStream { peer, tx, rx };
+            if sender.send(event).await.is_err() {
+                tracing::error!("failed to send incoming network event");
+            }
+        },
+        None => info!("received stream request with invalid service scope: {service}"),
+    }
+    Ok(())
+}
+
+async fn create_stream(
+    connection: Connection,
+    respond: oneshot::Sender<Result<(SendStream, RecvStream), ConnectionError>>,
+    service: ServiceScope,
+) -> Result<()> {
+    // We open a new QUIC stream.
+    let (mut tx, rx) = connection.open_bi().await?;
+    // We send the service scope first so
+    // that the receiver knows the service
+    // that this stream belongs to.
+    tx.write_all(&[service]).await?;
+    respond
+        .send(Ok((tx, rx)))
+        .map_err(|e| anyhow::anyhow!("failed to send new stream to client: {e:?}"))
 }
