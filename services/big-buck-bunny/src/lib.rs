f55d4baf4174c24d8f1a872fc7725cb4bc7837e3
ozwaldorf <self@ossian.dev>
2023-10-09 15:01:17 -0400

    chore(bbb): minor cleanup

--- services/big-buck-bunny/src/lib.rs
+++ services/big-buck-bunny/src/lib.rs
@@ -23,19 +23,22 @@ pub fn on_event_response(args: OnEventResponseArgs) {
 /// Blake3 hash of big buck bunny.
 static HASH: [u8; 32] = hex!("1065b2fd8291ee2d37b490fa47791b1fc99043e0b3243456f221a4371b8c2bd1");
 lazy_static! {
-    static ref TREE: Option<fn_sdk::blockstore::ContentHandle> = {
-        let Ok(tree) = fn_sdk::blockstore::ContentHandle::load_sync(&HASH) else {
+    static ref CONTENT_HANDLE: Option<fn_sdk::blockstore::ContentHandle> = {
+        let Ok(handle) = fn_sdk::blockstore::ContentHandle::load_sync(&HASH) else {
             log::error!("How could we? To not have Big Buck Bunny is a crime.");
             return None;
         };
 
-        Some(tree)
+        Some(handle)
     };
 }
 
 pub fn on_connected(args: OnConnectedArgs) {
-    if let Some(tree) = TREE.as_ref() {
-        fn_sdk::api::connection_send(args.connection_id, (tree.len() as u32).to_be_bytes().into());
+    if let Some(handle) = CONTENT_HANDLE.as_ref() {
+        fn_sdk::api::connection_send(
+            args.connection_id,
+            (handle.len() as u32).to_be_bytes().into(),
+        );
     } else {
         fn_sdk::api::connection_close(args.connection_id);
     }
@@ -45,21 +48,16 @@ pub fn on_message(args: OnMessageArgs) {
     if args.payload.len() != 4 {
         return;
     }
-
     let block = u32::from_be_bytes(*arrayref::array_ref![args.payload, 0, 4]) as usize;
-
-    let Some(tree) = TREE.as_ref() else {
+    let Some(handle) = CONTENT_HANDLE.as_ref() else {
         return;
     };
-
-    if block >= tree.len() {
+    if block >= handle.len() {
         return;
     }
 
-    log::info!("block {block}");
-
     fn_sdk::api::spawn(async move {
-        let content = tree.get(block).await.unwrap();
+        let content = handle.get(block).await.unwrap();
         fn_sdk::api::connection_send(args.connection_id, content);
     });
 }
