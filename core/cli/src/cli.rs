5ef6ebce7a1e6797bf436db16f10953a784c29b2
ozwaldorf <self@ossian.dev>
2023-10-13 13:48:47 -0400

    refactor(cli): switch to tracing subscriber and env filter

--- core/cli/src/cli.rs
+++ core/cli/src/cli.rs
@@ -4,24 +4,16 @@ use lightning_interfaces::infu_collection::Collection;
 use lightning_node::config::TomlConfigProvider;
 use lightning_node::{FinalTypes, WithMockConsensus};
 use lightning_signer::Signer;
-use log4rs::append::console::ConsoleAppender;
-use log4rs::append::rolling_file::policy::compound::roll::fixed_window::FixedWindowRoller;
-use log4rs::append::rolling_file::policy::compound::trigger::size::SizeTrigger;
-use log4rs::append::rolling_file::policy::compound::CompoundPolicy;
-use log4rs::append::rolling_file::RollingFileAppender;
-use log4rs::config::runtime::Logger;
-use log4rs::config::{Appender, Config, Root};
-use log4rs::encode::pattern::PatternEncoder;
-use log4rs::filter::threshold::ThresholdFilter;
 use resolved_pathbuf::ResolvedPathBuf;
 use tracing::info;
-use tracing::log::LevelFilter;
+use tracing::level_filters::LevelFilter;
+use tracing_subscriber::prelude::*;
+use tracing_subscriber::EnvFilter;
 
 use crate::args::{Args, Command};
 use crate::commands::run::CustomStartShutdown;
 use crate::commands::{dev, keys, print_config, run};
 use crate::utils::fs::ensure_parent_exist;
-use crate::utils::log_filter::CustomLogFilter;
 
 pub struct Cli {
     args: Args,
@@ -76,76 +68,32 @@ impl Cli {
     }
 
     fn setup(&self) {
-        console_subscriber::init();
-        let log_filter = match self.args.verbose {
-            0 => LevelFilter::Warn,
-            1 => LevelFilter::Info,
-            2 => LevelFilter::Debug,
-            _3_or_more => LevelFilter::Trace,
-        };
+        // Spawn tokio_console server
+        let console_layer = console_subscriber::Builder::default()
+            .with_default_env()
+            .server_addr(([0, 0, 0, 0], 9001))
+            .spawn();
 
-        let fmt = "{d(%Y-%m-%d %H:%M:%S)(utc)} | {h({l}):5.5} | {M} - {f}:{L} - {m}{n}";
-
-        let stdout_appender = ConsoleAppender::builder()
-            .encoder(Box::new(PatternEncoder::new(fmt)))
-            .build();
-
-        let file_rolling_appender = RollingFileAppender::builder()
-            .encoder(Box::new(PatternEncoder::new(fmt)))
-            .build(
-                std::env::temp_dir().join("lightning.log"), // /tmp/lightning.log
-                // compound policy
-                Box::new(CompoundPolicy::new(
-                    // trigger
-                    Box::new(SizeTrigger::new(150 * 1024 * 1024)), // 150 MB
-                    // roller
-                    Box::new(
-                        FixedWindowRoller::builder()
-                            .build(
-                                std::env::temp_dir()
-                                    .join("lightning.log.{}.gz")
-                                    .to_str()
-                                    .unwrap(),
-                                10,
-                            )
-                            .unwrap(),
-                    ),
-                )),
+        // Build the filter from cli args, or environment variable
+        let env_filter = EnvFilter::builder()
+            .with_default_directive(
+                match self.args.verbose {
+                    0 => LevelFilter::INFO,
+                    1 => LevelFilter::DEBUG,
+                    _2_or_more => LevelFilter::TRACE,
+                }
+                .into(),
             )
-            .unwrap();
+            .from_env_lossy()
+            .add_directive("quinn=off".parse().unwrap())
+            .add_directive("anemo=off".parse().unwrap());
 
-        // the bullshark logger is ignored for console only
-        let bullshark_ignore_logger = Logger::builder()
-            .appenders(vec!["file"])
-            .additive(false)
-            .build("narwhal_consensus::bullshark", LevelFilter::Trace);
-
-        let custom_filter = CustomLogFilter::new()
-            .insert("quinn", LevelFilter::Off)
-            .insert("anemo", LevelFilter::Off);
-
-        let config = Config::builder()
-            .appender(
-                Appender::builder()
-                    .filter(Box::new(custom_filter.clone()))
-                    .build("file", Box::new(file_rolling_appender)),
-            )
-            .appender(
-                Appender::builder()
-                    .filter(Box::new(ThresholdFilter::new(log_filter)))
-                    .filter(Box::new(custom_filter))
-                    .build("stdout", Box::new(stdout_appender)),
-            )
-            .loggers(vec![bullshark_ignore_logger])
-            .build(
-                Root::builder()
-                    .appender("file")
-                    .appender("stdout")
-                    .build(LevelFilter::Trace),
-            )
-            .unwrap();
-
-        log4rs::init_config(config).unwrap();
+        // Initialize the registry for logging events
+        tracing_subscriber::registry()
+            .with(console_layer)
+            .with(tracing_subscriber::fmt::layer().with_file(true))
+            .with(env_filter)
+            .init();
     }
 
     fn resolve_config_path(&self) -> Result<ResolvedPathBuf> {
---
+++ core/cli/src/utils.rs
@@ -0,0 +1,14 @@
+pub mod fs {
+    use std::fs::create_dir_all;
+
+    use anyhow::{bail, Result};
+    use resolved_pathbuf::ResolvedPathBuf;
+
+    pub fn ensure_parent_exist(path: &ResolvedPathBuf) -> Result<()> {
+        if let Some(parent_dir) = path.parent() {
+            Ok(create_dir_all(parent_dir)?)
+        } else {
+            bail!("Failed to get parent directory from given file: {:?}", path)
+        }
+    }
+}
