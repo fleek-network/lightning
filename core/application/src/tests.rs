2345aba88ca2d2dededfd5a2c1963886517a1a08
Daniel Bigos <daniel.bigos@icloud.com>
2023-11-22 16:57:05 +0100

    test(application): refactor test distribute rewards

--- core/application/src/tests.rs
+++ core/application/src/tests.rs
@@ -1,5 +1,6 @@
 use std::collections::BTreeMap;
 use std::net::IpAddr;
+use std::time::SystemTime;
 
 use affair::Socket;
 use anyhow::{anyhow, Result};
@@ -51,6 +52,16 @@ use crate::query_runner::QueryRunner;
 partial!(TestBinding {
     ApplicationInterface = Application<Self>;
 });
+
+pub struct Params {
+    epoch_time: Option<u64>,
+    max_inflation: Option<u16>,
+    protocol_share: Option<u16>,
+    node_share: Option<u16>,
+    service_builder_share: Option<u16>,
+    max_boost: Option<u16>,
+    supply_at_genesis: Option<u64>,
+}
 
 // This is a helper struct for keeping track of a node's private keys.
 // Many tests require us to submit transactions.
@@ -115,6 +126,34 @@ macro_rules! change_epoch {
     }};
 }
 
+// Helper macro that performs an epoch change.
+// In order to submit the `ChangeEpoch` transactions, this function needs access to the committee's
+// private keys. These are supplied in the `committee_keystore`.
+macro_rules! simple_epoch_change {
+    ($socket:expr,$committee_keystore:expr,$query_runner:expr,$epoch:expr) => {{
+        let required_signals = calculate_required_signals($committee_keystore.len());
+        // make call epoch change for 2/3rd committe members
+        for (index, node) in $committee_keystore
+            .iter()
+            .enumerate()
+            .take(required_signals)
+        {
+            let nonce = $query_runner
+                .get_node_info(&node.node_secret_key.to_pk())
+                .unwrap()
+                .nonce
+                + 1;
+            let req = prepare_change_epoch_request($epoch, &node.node_secret_key, nonce);
+
+            let res = run_transaction!(req, $socket);
+            // check epoch change
+            if index == required_signals - 1 {
+                assert!(res.change_epoch);
+            }
+        }
+    }};
+}
+
 macro_rules! submit_reputation_measurements {
     ($socket:expr,$secret_key:expr,$account_nonce:expr,$measurements:expr) => {{
         let req = get_update_request_node(
@@ -162,6 +201,13 @@ macro_rules! stake {
     }};
 }
 
+macro_rules! stake_lock {
+    ($socket:expr,$secret_key:expr,$nonce:expr,$node_pk:expr,$locked_for:expr) => {{
+        let req = prepare_stake_lock_request($locked_for, $node_pk, $secret_key, $nonce);
+        expect_tx_success!(req, $socket, ExecutionData::None)
+    }};
+}
+
 fn init_app(config: Option<Config>) -> (ExecutionEngineSocket, QueryRunner) {
     let config = config.or(Some(Config {
         genesis: None,
@@ -171,13 +217,16 @@ fn init_app(config: Option<Config>) -> (ExecutionEngineSocket, QueryRunner) {
         db_path: None,
         db_options: None,
     }));
-    let app = Application::<TestBinding>::init(config.unwrap(), Default::default()).unwrap();
+    do_init_app(config.unwrap())
+}
+
+fn do_init_app(config: Config) -> (ExecutionEngineSocket, QueryRunner) {
+    let app = Application::<TestBinding>::init(config, Default::default()).unwrap();
 
     (app.transaction_executor(), app.sync_query())
 }
-
 fn test_genesis() -> Genesis {
-    Genesis::load().unwrap()
+    Genesis::load().expect("Failed to load genesis from file.")
 }
 
 fn test_init_app(committee: Vec<GenesisNode>) -> (ExecutionEngineSocket, QueryRunner) {
@@ -188,6 +237,59 @@ fn test_init_app(committee: Vec<GenesisNode>) -> (ExecutionEngineSocket, QueryRu
 
 fn init_app_with_genesis(genesis: &Genesis) -> (ExecutionEngineSocket, QueryRunner) {
     init_app(Some(test_config(genesis.clone())))
+}
+
+fn init_app_with_params(
+    params: Params,
+    committee: Option<Vec<GenesisNode>>,
+) -> (ExecutionEngineSocket, QueryRunner) {
+    let mut genesis = test_genesis();
+
+    if let Some(committee) = committee {
+        genesis.node_info = committee;
+    }
+
+    genesis.epoch_start = SystemTime::now()
+        .duration_since(SystemTime::UNIX_EPOCH)
+        .unwrap()
+        .as_millis() as u64;
+    if let Some(epoch_time) = params.epoch_time {
+        genesis.epoch_time = epoch_time;
+    }
+
+    if let Some(max_inflation) = params.max_inflation {
+        genesis.max_inflation = max_inflation;
+    }
+
+    if let Some(protocol_share) = params.protocol_share {
+        genesis.protocol_share = protocol_share;
+    }
+
+    if let Some(node_share) = params.node_share {
+        genesis.node_share = node_share;
+    }
+
+    if let Some(service_builder_share) = params.service_builder_share {
+        genesis.service_builder_share = service_builder_share;
+    }
+
+    if let Some(max_boost) = params.max_boost {
+        genesis.max_boost = max_boost;
+    }
+
+    if let Some(supply_at_genesis) = params.supply_at_genesis {
+        genesis.supply_at_genesis = supply_at_genesis;
+    }
+    let config = Config {
+        genesis: Some(genesis),
+        mode: Mode::Test,
+        testnet: false,
+        storage: StorageConfig::InMemory,
+        db_path: None,
+        db_options: None,
+    };
+
+    init_app(Some(config))
 }
 
 fn test_config(genesis: Genesis) -> Config {
@@ -446,6 +548,23 @@ fn prepare_pod_request(
             service_id, // service 0 serving bandwidth
             proofs: vec![DeliveryAcknowledgment],
             metadata: None,
+        },
+        secret_key,
+        nonce,
+    )
+}
+
+fn prepare_stake_lock_request(
+    locked_for: u64,
+    node: &NodePublicKey,
+    secret_key: &AccountOwnerSecretKey,
+    nonce: u64,
+) -> UpdateRequest {
+    // Deposit some FLK into account 1
+    get_update_request_account(
+        UpdateMethod::StakeLock {
+            node: *node,
+            locked_for,
         },
         secret_key,
         nonce,
@@ -1031,4 +1150,159 @@ async fn test_validate_txn() {
         res.txn_receipts[0].response,
         query_runner.validate_txn(req.into())
     );
+}
+
+#[tokio::test]
+async fn test_distribute_rewards() {
+    let committee_size = 4;
+    let (committee, keystore) = create_genesis_committee(committee_size);
+
+    let max_inflation = 10;
+    let protocol_part = 10;
+    let node_part = 80;
+    let service_part = 10;
+    let boost = 4;
+    let supply_at_genesis = 1_000_000;
+    let (update_socket, query_runner) = init_app_with_params(
+        Params {
+            epoch_time: None,
+            max_inflation: Some(max_inflation),
+            protocol_share: Some(protocol_part),
+            node_share: Some(node_part),
+            service_builder_share: Some(service_part),
+            max_boost: Some(boost),
+            supply_at_genesis: Some(supply_at_genesis),
+        },
+        Some(committee),
+    );
+
+    // get params for emission calculations
+    let percentage_divisor: HpUfixed<18> = 100_u16.into();
+    let supply_at_year_start: HpUfixed<18> = supply_at_genesis.into();
+    let inflation: HpUfixed<18> = HpUfixed::from(max_inflation) / &percentage_divisor;
+    let node_share = HpUfixed::from(node_part) / &percentage_divisor;
+    let protocol_share = HpUfixed::from(protocol_part) / &percentage_divisor;
+    let service_share = HpUfixed::from(service_part) / &percentage_divisor;
+
+    let owner_secret_key1 = AccountOwnerSecretKey::generate();
+    let node_secret_key1 = NodeSecretKey::generate();
+    let owner_secret_key2 = AccountOwnerSecretKey::generate();
+    let node_secret_key2 = NodeSecretKey::generate();
+
+    let deposit_amount = 10_000_u64.into();
+    let locked_for = 1460;
+    // deposit FLK tokens and stake it
+    deposit!(&update_socket, &owner_secret_key1, 1, &deposit_amount);
+    stake!(
+        &update_socket,
+        &owner_secret_key1,
+        2,
+        &deposit_amount,
+        &node_secret_key1.to_pk(),
+        [0; 96].into()
+    );
+    deposit!(&update_socket, &owner_secret_key2, 1, &deposit_amount);
+    stake!(
+        &update_socket,
+        &owner_secret_key2,
+        2,
+        &deposit_amount,
+        &node_secret_key2.to_pk(),
+        [1; 96].into()
+    );
+    stake_lock!(
+        &update_socket,
+        &owner_secret_key2,
+        3,
+        &node_secret_key2.to_pk(),
+        locked_for
+    );
+
+    // submit pods for usage
+    let commodity_10 = 12_800;
+    let commodity_11 = 3_600;
+    let commodity_21 = 5000;
+    let pod_10 = prepare_pod_request(commodity_10, 0, &node_secret_key1, 1);
+    let pod_11 = prepare_pod_request(commodity_11, 1, &node_secret_key1, 2);
+    let pod_21 = prepare_pod_request(commodity_21, 1, &node_secret_key2, 1);
+
+    let node_1_usd = 0.1 * (commodity_10 as f64) + 0.2 * (commodity_11 as f64); // 2_000 in revenue
+    let node_2_usd = 0.2 * (commodity_21 as f64); // 1_000 in revenue
+    let reward_pool: HpUfixed<6> = (node_1_usd + node_2_usd).into();
+
+    let node_1_proportion: HpUfixed<18> = HpUfixed::from(2000_u64) / HpUfixed::from(3000_u64);
+    let node_2_proportion: HpUfixed<18> = HpUfixed::from(1000_u64) / HpUfixed::from(3000_u64);
+
+    let service_proportions: Vec<HpUfixed<18>> = vec![
+        HpUfixed::from(1280_u64) / HpUfixed::from(3000_u64),
+        HpUfixed::from(1720_u64) / HpUfixed::from(3000_u64),
+    ];
+
+    // run the delivery ack transaction
+    run_transactions!(vec![pod_10, pod_11, pod_21], &update_socket);
+
+    // call epoch change that will trigger distribute rewards
+    simple_epoch_change!(&update_socket, &keystore, &query_runner, 0);
+
+    // assert stable balances
+    assert_eq!(
+        query_runner.get_stables_balance(&owner_secret_key1.to_pk().into()),
+        HpUfixed::<6>::from(node_1_usd) * node_share.convert_precision()
+    );
+    assert_eq!(
+        query_runner.get_stables_balance(&owner_secret_key2.to_pk().into()),
+        HpUfixed::<6>::from(node_2_usd) * node_share.convert_precision()
+    );
+
+    let total_share =
+        &node_1_proportion * HpUfixed::from(1_u64) + &node_2_proportion * HpUfixed::from(4_u64);
+
+    // calculate emissions per unit
+    let emissions: HpUfixed<18> = (inflation * supply_at_year_start) / &365.0.into();
+    let emissions_for_node = &emissions * &node_share;
+
+    // assert flk balances node 1
+    assert_eq!(
+        // node_flk_balance1
+        query_runner.get_flk_balance(&owner_secret_key1.to_pk().into()),
+        // node_flk_rewards1
+        (&emissions_for_node * &node_1_proportion) / &total_share
+    );
+
+    // assert flk balances node 2
+    assert_eq!(
+        // node_flk_balance2
+        query_runner.get_flk_balance(&owner_secret_key2.to_pk().into()),
+        // node_flk_rewards2
+        (&emissions_for_node * (&node_2_proportion * HpUfixed::from(4_u64))) / &total_share
+    );
+
+    // assert protocols share
+    let protocol_account = query_runner.get_protocol_fund_address();
+    let protocol_balance = query_runner.get_flk_balance(&protocol_account);
+    let protocol_rewards = &emissions * &protocol_share;
+    assert_eq!(protocol_balance, protocol_rewards);
+
+    let protocol_stables_balance = query_runner.get_stables_balance(&protocol_account);
+    assert_eq!(
+        &reward_pool * &protocol_share.convert_precision(),
+        protocol_stables_balance
+    );
+
+    // assert service balances with service id 0 and 1
+    for s in 0..2 {
+        let service_owner = query_runner.get_service_info(s).owner;
+        let service_balance = query_runner.get_flk_balance(&service_owner);
+        assert_eq!(
+            service_balance,
+            &emissions * &service_share * &service_proportions[s as usize]
+        );
+        let service_stables_balance = query_runner.get_stables_balance(&service_owner);
+        assert_eq!(
+            service_stables_balance,
+            &reward_pool
+                * &service_share.convert_precision()
+                * &service_proportions[s as usize].convert_precision()
+        );
+    }
 }
