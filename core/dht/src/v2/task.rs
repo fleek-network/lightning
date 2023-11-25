a290700de1ddf2f94858269381c2b593d6dca1cc
Miguel <miguel@fleek.xyz>
2023-11-25 18:02:01 -0500

    dht: replace task handler with worker pool

--- core/dht/src/v2/task.rs
+++ core/dht/src/v2/task.rs
@@ -2,20 +2,24 @@ use std::collections::HashMap;
 use std::future::Future;
 use std::marker::PhantomData;
 use std::sync::Arc;
+use std::time::Duration;
 use bytes::Bytes;
-use deadqueue::limited::Queue;
-use futures::stream::FuturesUnordered;
-use tokio::net::UdpSocket;
+use tokio::sync::mpsc::{Receiver, Sender};
 use tokio::sync::Notify;
-use tokio::task::JoinHandle;
+use tokio::task::{JoinHandle, JoinSet};
 use lightning_interfaces::infu_collection::Collection;
-use lightning_interfaces::SyncQueryRunnerInterface;
+use lightning_interfaces::types::NodeIndex;
 use crate::v2::lookup::LookupInterface;
 
-pub enum Requests {
-    Get,
-    Put,
-    Bootstrap,
+pub enum Task {
+    LookUp {
+        hash: u32,
+        value: bool,
+    },
+    Ping {
+        peer: NodeIndex,
+        timeout: Duration,
+    }
 }
 
 pub struct NetworkMessage {
@@ -23,20 +27,29 @@ pub struct NetworkMessage {
     payload: Bytes,
 }
 
+pub enum Event {
+    Pong {
+        peer: NodeIndex,
+    },
+    Timeout {
+        peer: NodeIndex,
+    }
+}
+
 // 1. Manage tasks from user.
 // 2. Read messages from incoming-queue which are messages received from the network.
 //      - These include tasks like FIND_NODE or STORE_VALUE.
 //      - Some include RPC responses that must be passed on to ongoing tasks.
 // 3. (Needs thinking because this might be able to be done by Table itself) Schedule routine tasks related to pruning table.
-pub struct TaskManager<C: Collection, L: LookupInterface<C>> {
+pub struct WorkerPool<C: Collection, L: LookupInterface<C>> {
     /// Look-up task.
     lookup_task: L,
-    /// Messages received from the network.
-    incoming_message: Queue<NetworkMessage>,
     /// Requests from user.
-    requests: Queue<Requests>,
+    task_queue: Receiver<Task>,
+    /// Queue to send ping events.
+    ping_queue_tx: Sender<Event>,
     /// Ongoing tasks.
-    ongoing_tasks: FuturesUnordered<JoinHandle<u64>>,
+    ongoing_tasks: JoinSet<u64>,
     /// Ongoing lookup tasks.
     ongoing: HashMap<u64, ()>,
     /// Maximum pool size.
@@ -46,7 +59,7 @@ pub struct TaskManager<C: Collection, L: LookupInterface<C>> {
     _marker: PhantomData<C>,
 }
 
-impl<C: Collection, L: LookupInterface<C>> TaskManager<C, L> {
+impl<C: Collection, L: LookupInterface<C>> WorkerPool<C, L> {
     pub async fn run(self) {
         loop {
             tokio::select! {
