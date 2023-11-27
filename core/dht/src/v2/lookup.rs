a2fad77d86f629b87e5a5183eedaac87b2262b8c
Miguel <miguel@fleek.xyz>
2023-11-26 22:41:35 -0500

    dht: handle incoming look-up tasks in pool

--- core/dht/src/v2/lookup.rs
+++ core/dht/src/v2/lookup.rs
@@ -1,23 +1,24 @@
+use std::marker::PhantomData;
 use std::net::IpAddr;
 use std::sync::Arc;
 
 use bytes::Bytes;
-use tokio::net::UdpSocket;
-use tokio::sync::oneshot;
 use infusion::c;
 use lightning_interfaces::infu_collection::Collection;
-use lightning_interfaces::SyncQueryRunnerInterface;
 use lightning_interfaces::types::NodeIndex;
+use lightning_interfaces::SyncQueryRunnerInterface;
+use tokio::net::UdpSocket;
+use tokio::sync::oneshot;
 
-pub type LookUpValueRespond =  oneshot::Sender<Result<Option<Bytes>>>;
-pub type LookUpProviderRespond =  oneshot::Sender<Result<Vec<NodeIndex>>>;
+pub type ValueRespond = oneshot::Sender<Result<Option<Bytes>>>;
+pub type ProviderRespond = oneshot::Sender<Result<Vec<NodeIndex>>>;
 
 pub type Result<T> = std::result::Result<T, Error>;
 
 /// Look-up response.
 pub struct Response {
     id: u64,
-    payload: Bytes
+    payload: Bytes,
 }
 
 /// Look-up response payload.
@@ -30,12 +31,28 @@ pub enum Error {
     Canceled,
 }
 
-pub trait LookupInterface<C: Collection>: Clone {
-    fn init(
-        table_client: (),
+pub trait LookupInterface: Clone + Send + Sync + Unpin {
+    async fn find_node(&self, hash: u32) -> Result<Vec<NodeIndex>>;
+    async fn find_value(&self, hash: u32) -> Result<Option<Bytes>>;
+}
+
+#[derive(Clone)]
+pub struct Looker<C: Collection> {
+    sync_query: c![C::ApplicationInterface::SyncExecutor],
+    socket: Arc<UdpSocket>,
+    _marker: PhantomData<C>,
+}
+
+impl<C: Collection> Looker<C> {
+    fn new(
+        _table_client: (),
         socket: Arc<UdpSocket>,
         sync_query: c!(C::ApplicationInterface::SyncExecutor),
-    ) -> Self;
-    async fn find_node(&self, hash: u32, respond: LookUpProviderRespond) -> Result<()>;
-    async fn find_value(&self, hash: u32, respond: LookUpValueRespond) -> Result<()>;
+    ) -> Self {
+        Self {
+            socket,
+            sync_query,
+            _marker: PhantomData,
+        }
+    }
 }
