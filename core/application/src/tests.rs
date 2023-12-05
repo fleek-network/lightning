a3ed87fe95ac4559614892a9e5ab9a1c122984bf
Daniel Bigos <daniel.bigos@icloud.com>
2023-12-05 12:54:57 +0100

    test(application): improve test coverage for ChangeProtocolParam

--- core/application/src/tests.rs
+++ core/application/src/tests.rs
@@ -69,7 +69,7 @@ pub struct Params {
 struct GenesisCommitteeKeystore {
     _owner_secret_key: AccountOwnerSecretKey,
     node_secret_key: NodeSecretKey,
-    _consensus_secret_key: ConsensusSecretKey,
+    consensus_secret_key: ConsensusSecretKey,
     _worker_secret_key: NodeSecretKey,
 }
 
@@ -555,7 +555,7 @@ fn create_genesis_committee(
             _owner_secret_key: owner_secret_key,
             _worker_secret_key: node_secret_key.clone(),
             node_secret_key,
-            _consensus_secret_key: consensus_secret_key,
+            consensus_secret_key,
         });
     });
     (committee, keystore)
@@ -949,7 +949,7 @@ fn add_to_committee(
         _owner_secret_key: owner_secret_key,
         _worker_secret_key: node_secret_key.clone(),
         node_secret_key,
-        _consensus_secret_key: consensus_secret_key,
+        consensus_secret_key,
     });
 }
 
@@ -1441,6 +1441,43 @@ async fn test_change_protocol_params() {
 }
 
 #[tokio::test]
+async fn test_change_protocol_params_reverts_not_account_key() {
+    let committee_size = 4;
+    let (committee, keystore) = create_genesis_committee(committee_size);
+    let (update_socket, query_runner) = test_init_app(committee);
+
+    let param = ProtocolParams::LockTime;
+    let initial_value = query_runner.get_protocol_params(param.clone());
+    let new_value = initial_value + 1;
+
+    let change_method = UpdateMethod::ChangeProtocolParam {
+        param: param.clone(),
+        value: new_value,
+    };
+
+    // Assert that reverts for Node Key
+    let update =
+        prepare_update_request_node(change_method.clone(), &keystore[0].node_secret_key, 1);
+    expect_tx_revert!(update, &update_socket, ExecutionError::OnlyAccountOwner);
+    assert_eq!(
+        query_runner.get_protocol_params(param.clone()),
+        initial_value
+    );
+
+    // Assert that reverts for Consensus Key
+    let update = prepare_update_request_consensus(
+        change_method.clone(),
+        &keystore[0].consensus_secret_key,
+        2,
+    );
+    expect_tx_revert!(update, &update_socket, ExecutionError::OnlyAccountOwner);
+    assert_eq!(
+        query_runner.get_protocol_params(param.clone()),
+        initial_value
+    );
+}
+
+#[tokio::test]
 async fn test_validate_txn() {
     let committee_size = 4;
     let (committee, keystore) = create_genesis_committee(committee_size);
@@ -1890,7 +1927,7 @@ async fn test_revert_transfer_not_account_key() {
     );
 
     // Check that trying to transfer funds with Consensus Key reverts
-    let consensus_secret_key = &keystore[0]._consensus_secret_key;
+    let consensus_secret_key = &keystore[0].consensus_secret_key;
     let update_node_key = prepare_update_request_consensus(transfer, consensus_secret_key, 2);
     expect_tx_revert!(
         update_node_key,
