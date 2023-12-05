cba8a1bf781ecd8463507014a2f3d3216e242851
Daniel Bigos <daniel.bigos@icloud.com>
2023-12-05 13:56:18 +0100

    test(application): improve test coverage for Deposit

--- core/application/src/tests.rs
+++ core/application/src/tests.rs
@@ -1992,3 +1992,82 @@ async fn test_transfer_works_properly() {
         zero_balance + transfer_amount
     );
 }
+
+#[tokio::test]
+async fn test_deposit_flk_works_properly() {
+    let (update_socket, query_runner) = init_app(None);
+
+    let owner_secret_key = AccountOwnerSecretKey::generate();
+    let owner: EthAddress = owner_secret_key.to_pk().into();
+
+    let deposit_amount: HpUfixed<18> = 1_000u64.into();
+    let intial_balance = query_runner.get_flk_balance(&owner);
+
+    let deposit = UpdateMethod::Deposit {
+        proof: ProofOfConsensus {},
+        token: Tokens::FLK,
+        amount: deposit_amount.clone(),
+    };
+    let update = prepare_update_request_account(deposit, &owner_secret_key, 1);
+    expect_tx_success!(update, &update_socket);
+
+    assert_eq!(
+        query_runner.get_flk_balance(&owner),
+        intial_balance + deposit_amount
+    );
+}
+
+#[tokio::test]
+async fn test_revert_deposit_not_account_key() {
+    let committee_size = 4;
+    let (committee, keystore) = create_genesis_committee(committee_size);
+    let (update_socket, _query_runner) = test_init_app(committee);
+
+    let amount: HpUfixed<18> = 10_u64.into();
+    let deposit = UpdateMethod::Deposit {
+        proof: ProofOfConsensus {},
+        token: Tokens::FLK,
+        amount,
+    };
+
+    // Check that trying to deposit funds with Node Key reverts
+    let node_secret_key = &keystore[0].node_secret_key;
+    let update_node_key = prepare_update_request_node(deposit.clone(), node_secret_key, 1);
+    expect_tx_revert!(
+        update_node_key,
+        &update_socket,
+        ExecutionError::OnlyAccountOwner
+    );
+
+    // Check that trying to deposit funds with Consensus Key reverts
+    let consensus_secret_key = &keystore[0].consensus_secret_key;
+    let update_node_key = prepare_update_request_consensus(deposit, consensus_secret_key, 2);
+    expect_tx_revert!(
+        update_node_key,
+        &update_socket,
+        ExecutionError::OnlyAccountOwner
+    );
+}
+
+#[tokio::test]
+async fn test_deposit_usdc_works_properly() {
+    let (update_socket, query_runner) = init_app(None);
+
+    let owner_secret_key = AccountOwnerSecretKey::generate();
+    let owner: EthAddress = owner_secret_key.to_pk().into();
+
+    let intial_balance = query_runner.get_account_balance(&owner);
+    let deposit_amount = 1_000;
+    let deposit = UpdateMethod::Deposit {
+        proof: ProofOfConsensus {},
+        token: Tokens::USDC,
+        amount: deposit_amount.into(),
+    };
+    let update = prepare_update_request_account(deposit, &owner_secret_key, 1);
+    expect_tx_success!(update, &update_socket);
+
+    assert_eq!(
+        query_runner.get_account_balance(&owner),
+        intial_balance + deposit_amount
+    );
+}
