39d0f5b590d51fbcefe8950d45c630619d81aaff
Daniel Bigos <daniel.bigos@icloud.com>
2023-12-06 11:41:45 +0100

    test(application): improve test coverage for Stake

--- core/application/src/tests.rs
+++ core/application/src/tests.rs
@@ -2287,3 +2287,191 @@ async fn test_opt_out_works() {
         Participation::OptedOut
     );
 }
+
+#[tokio::test]
+async fn test_revert_stake_not_account_key() {
+    let committee_size = 4;
+    let (committee, keystore) = create_genesis_committee(committee_size);
+    let (update_socket, _query_runner) = test_init_app(committee);
+
+    let amount: HpUfixed<18> = 1000_u64.into();
+
+    let stake = UpdateMethod::Stake {
+        amount,
+        node_public_key: keystore[0].node_secret_key.to_pk(),
+        consensus_key: None,
+        node_domain: None,
+        worker_public_key: None,
+        worker_domain: None,
+        ports: None,
+    };
+
+    // Check that trying to Stake funds with Node Key reverts
+    let node_secret_key = &keystore[0].node_secret_key;
+    let update_node_key = prepare_update_request_node(stake.clone(), node_secret_key, 1);
+    expect_tx_revert!(
+        update_node_key,
+        &update_socket,
+        ExecutionError::OnlyAccountOwner
+    );
+
+    // Check that trying to Stake funds with Consensus Key reverts
+    let consensus_secret_key = &keystore[0].consensus_secret_key;
+    let update_node_key = prepare_update_request_consensus(stake, consensus_secret_key, 2);
+    expect_tx_revert!(
+        update_node_key,
+        &update_socket,
+        ExecutionError::OnlyAccountOwner
+    );
+}
+
+#[tokio::test]
+async fn test_revert_stake_insufficient_balance() {
+    let committee_size = 4;
+    let (committee, _keystore) = create_genesis_committee(committee_size);
+    let (update_socket, query_runner) = test_init_app(committee);
+
+    let owner_secret_key = AccountOwnerSecretKey::generate();
+    let address: EthAddress = owner_secret_key.to_pk().into();
+
+    let peer_pub_key = NodeSecretKey::generate().to_pk();
+
+    // Deposit some FLK into an account
+    let deposit = 1000_u64.into();
+    deposit!(&update_socket, &owner_secret_key, 1, &deposit);
+
+    let balance = query_runner.get_flk_balance(&address);
+
+    // Now try with the correct details for a new node
+    let update = prepare_initial_stake_update(
+        &(deposit + <u64 as Into<HpUfixed<18>>>::into(1)),
+        &peer_pub_key,
+        [0; 96].into(),
+        "127.0.0.1".parse().unwrap(),
+        [0; 32].into(),
+        "127.0.0.1".parse().unwrap(),
+        NodePorts::default(),
+        &owner_secret_key,
+        2,
+    );
+
+    // Expect Revert Error
+    expect_tx_revert!(update, &update_socket, ExecutionError::InsufficientBalance);
+
+    // Flk balance has not changed
+    assert_eq!(query_runner.get_flk_balance(&address), balance);
+}
+
+#[tokio::test]
+async fn test_revert_stake_consensus_key_already_indexed() {
+    let committee_size = 4;
+    let (committee, keystore) = create_genesis_committee(committee_size);
+    let (update_socket, query_runner) = test_init_app(committee);
+
+    let owner_secret_key = AccountOwnerSecretKey::generate();
+    let address: EthAddress = owner_secret_key.to_pk().into();
+
+    let peer_pub_key = NodeSecretKey::generate().to_pk();
+
+    // Deposit some FLK into an account
+    let deposit = 1000_u64.into();
+    deposit!(&update_socket, &owner_secret_key, 1, &deposit);
+
+    let balance = query_runner.get_flk_balance(&address);
+
+    // Now try with the correct details for a new node
+    let update = prepare_initial_stake_update(
+        &deposit,
+        &peer_pub_key,
+        keystore[0].consensus_secret_key.to_pk(),
+        "127.0.0.1".parse().unwrap(),
+        [0; 32].into(),
+        "127.0.0.1".parse().unwrap(),
+        NodePorts::default(),
+        &owner_secret_key,
+        2,
+    );
+
+    // Expect Revert Error
+    expect_tx_revert!(
+        update,
+        &update_socket,
+        ExecutionError::ConsensusKeyAlreadyIndexed
+    );
+
+    // Flk balance has not changed
+    assert_eq!(query_runner.get_flk_balance(&address), balance);
+}
+
+#[tokio::test]
+async fn test_stake_works() {
+    let committee_size = 4;
+    let (committee, _keystore) = create_genesis_committee(committee_size);
+    let (update_socket, query_runner) = test_init_app(committee);
+
+    let owner_secret_key = AccountOwnerSecretKey::generate();
+    let address: EthAddress = owner_secret_key.to_pk().into();
+
+    let peer_pub_key = NodeSecretKey::generate().to_pk();
+
+    // Deposit some FLK into an account
+    let stake = 1000_u64.into();
+    deposit!(&update_socket, &owner_secret_key, 1, &stake);
+
+    let balance = query_runner.get_flk_balance(&address);
+    let consensus_key: ConsensusPublicKey = [0; 96].into();
+    let node_domain: IpAddr = "89.64.54.26".parse().unwrap();
+    let worker_pub_key: NodePublicKey = [0; 32].into();
+    let worker_domain: IpAddr = "127.0.0.1".parse().unwrap();
+    let node_ports = NodePorts {
+        primary: 4001,
+        worker: 4002,
+        mempool: 4003,
+        rpc: 4004,
+        pool: 4005,
+        dht: 4006,
+        pinger: 4007,
+        handshake: HandshakePorts {
+            http: 5001,
+            webrtc: 5002,
+            webtransport: 5003,
+        },
+    };
+    // Now try with the correct details for a new node
+    let update = prepare_initial_stake_update(
+        &stake,
+        &peer_pub_key,
+        consensus_key,
+        node_domain,
+        worker_pub_key,
+        worker_domain,
+        node_ports.clone(),
+        &owner_secret_key,
+        2,
+    );
+
+    // Expect Success
+    expect_tx_success!(update, &update_socket);
+
+    // Flk balance has not changed
+    assert_eq!(
+        query_runner.get_flk_balance(&address),
+        balance - stake.clone()
+    );
+
+    let node_info = query_runner.get_node_info(&peer_pub_key).unwrap();
+    assert_eq!(node_info.consensus_key, consensus_key);
+    assert_eq!(node_info.domain, node_domain);
+    assert_eq!(node_info.worker_public_key, worker_pub_key);
+    assert_eq!(node_info.worker_domain, worker_domain);
+    assert_eq!(node_info.ports, node_ports);
+
+    // Query the new node and make sure he has the proper stake
+    assert_eq!(query_runner.get_staked(&peer_pub_key), stake);
+
+    let node_idx = query_runner.pubkey_to_index(peer_pub_key).unwrap();
+    assert_eq!(
+        query_runner.index_to_pubkey(node_idx).unwrap(),
+        peer_pub_key
+    );
+}
