310426c174fd7fa3dbc496fa7ef70411985450ed
ozwaldorf <self@ossian.dev>
2024-01-05 17:27:19 -0500

    chore(cli): replace human_panic with panic_report

--- core/cli/src/main.rs
+++ core/cli/src/main.rs
@@ -3,10 +3,11 @@ pub mod cli;
 mod commands;
 mod utils;
 
+use std::time::{SystemTime, UNIX_EPOCH};
+
 use anyhow::Result;
 use clap::Parser;
 use cli::Cli;
-use compile_time_run::run_command_str;
 use infusion::c;
 use lightning_interfaces::infu_collection::Collection;
 use lightning_interfaces::ServiceExecutorInterface;
@@ -29,17 +30,26 @@ fn main() -> Result<()> {
         std::process::exit(0);
     }
 
-    human_panic::setup_panic!(Metadata {
+    panic_report::setup! {
+        name: "lightning-node".into(),
         version: concat!(
             env!("CARGO_PKG_VERSION"),
             "-",
-            run_command_str!("git", "rev-parse", "HEAD")
+            compile_time_run::run_command_str!("git", "rev-parse", "HEAD")
         )
         .into(),
-        name: "lightning-node".into(),
-        authors: "Fleek Network Team <reports@fleek.network>".into(),
-        homepage: "https://github.com/fleek-network/lightning".into()
-    });
+        homepage: "https://github.com/fleek-network/lightning",
+        contacts: ["Fleek Network Team <reports@fleek.network>".to_string()]
+    };
+
+    panic_report::add_context("os", os_info::get().to_string());
+    panic_report::add_context(
+        "node_start",
+        SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .unwrap()
+            .as_secs(),
+    );
 
     // Create the tokio runtime and execute the cli
     tokio::runtime::Builder::new_multi_thread()
