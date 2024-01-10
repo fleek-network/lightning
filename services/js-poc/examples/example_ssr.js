ba0cee2cc992a613253133ee429f961a77929779
ozwaldorf <self@ossian.dev>
2024-01-09 20:01:56 -0500

    chore(js-poc): simple ssr test script for browser experimentation

---
+++ services/js-poc/examples/example_ssr.js
@@ -0,0 +1,15 @@
+// Example simple ssr script for testing ideas for a browser request
+// cargo run --example js-poc-client $(lgtn-old dev store services/js-poc/examples/example_ssr.js | awk '{print $1}') blake3 '{"path":"/index.html"}'
+
+const main = (params = {}) => {
+  Fleek.log(JSON.stringify(params));
+
+  let path = params.path || '';
+
+  switch (path) {
+    case '/index.html':
+      return '<html><body><h1>Hello World!</h1></body></html>'
+    default:
+      return '<html><body><p>404 not found</p></body></html>'
+  }
+}
