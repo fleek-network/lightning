4061380bbd7280b57ffb1d341508444c3c24a5b2
Miguel <miguel@fleek.xyz>
2024-01-13 17:28:59 -0500

    sri: impl verifier

--- lib/fast-sri/src/verify.rs
+++ lib/fast-sri/src/verify.rs
@@ -1,22 +1,22 @@
-use fastcrypto::hash::HashFunction;
+use fastcrypto::hash::{Digest, HashFunction};
 
 use crate::integrity::IncrementalBuilder;
 
 pub struct Verifier<H: HashFunction<DIGEST_LEN>, const DIGEST_LEN: usize> {
     builder: IncrementalBuilder<H, DIGEST_LEN>,
-    digest: Vec<u8>,
+    digest: Digest<DIGEST_LEN>,
 }
 
 impl<H: HashFunction<DIGEST_LEN>, const DIGEST_LEN: usize> Verifier<H, DIGEST_LEN> {
-    pub fn new(builder: IncrementalBuilder<H, DIGEST_LEN>, digest: Vec<u8>) -> Self {
+    pub fn new(builder: IncrementalBuilder<H, DIGEST_LEN>, digest: Digest<DIGEST_LEN>) -> Self {
         Self { builder, digest }
     }
 
     pub fn update<T: AsRef<[u8]>>(&mut self, data: T) {
-        todo!()
+        self.builder.update(data);
     }
 
     pub fn verify(self) -> bool {
-        todo!()
+        self.digest == self.builder.inner_finalize()
     }
 }
