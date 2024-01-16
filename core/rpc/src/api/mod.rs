014f98eec94a71e6dbdbbe1b2f1671581ea9f69e
Miguel <miguel@fleek.xyz>
2024-01-16 16:45:21 -0500

    rpc: impl admin rpc

--- core/rpc/src/api/mod.rs
+++ core/rpc/src/api/mod.rs
@@ -1,6 +1,9 @@
+mod admin;
 mod eth;
 mod flk;
 mod net;
+
+pub use admin::{AdminApiClient, AdminApiServer};
 pub use eth::{EthApiClient, EthApiServer};
 pub use flk::{FleekApiClient, FleekApiOpenRpc, FleekApiServer};
 pub use net::{NetApiClient, NetApiOpenRpc, NetApiServer};
--- core/rpc/src/logic/mod.rs
+++ core/rpc/src/logic/mod.rs
@@ -1,6 +1,8 @@
+mod admin;
 mod eth_impl;
 mod flk_impl;
 mod net_impl;
+
 pub use eth_impl::EthApi;
 pub use flk_impl::FleekApi;
 pub use net_impl::NetApi;
