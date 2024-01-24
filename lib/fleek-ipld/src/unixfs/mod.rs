1b30902ee2212d644c8c9444a5c01d86a1f1f4d5
Miguel <miguel@fleek.xyz>
2024-01-24 16:44:13 -0500

    ipld: add try from

--- lib/fleek-ipld/src/unixfs/mod.rs
+++ lib/fleek-ipld/src/unixfs/mod.rs
@@ -1,4 +1,16 @@
 mod proto;
 
+use std::io;
+
 pub use proto::unixfs::mod_Data::DataType;
 pub use proto::unixfs::{Data, Metadata};
+
+impl<'a> TryFrom<&'a [u8]> for Data<'a> {
+    type Error = io::Error;
+
+    fn try_from(data: &'a [u8]) -> Result<Self, Self::Error> {
+        use quick_protobuf::{BytesReader, MessageRead};
+        Data::from_reader(&mut BytesReader::from_bytes(data), data)
+            .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))
+    }
+}
