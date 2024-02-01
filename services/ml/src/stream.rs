25514b0d09097d9ed7279312991732e8f69bfbe2
Miguel <mi9uel9@gmail.com>
2024-02-01 18:19:20 -0500

    ai: add service stream

--- services/ml/src/stream.rs
+++ services/ml/src/stream.rs
@@ -1,5 +1,8 @@
+use bytes::{Buf, BytesMut};
 use fn_sdk::connection::Connection;
 use serde::{Deserialize, Serialize};
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use crate::Request;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Data {
@@ -19,4 +22,46 @@ pub enum Origin {
 #[allow(dead_code)]
 pub struct ServiceStream {
     connection: Connection,
+    buffer: BytesMut,
+}
+
+#[allow(dead_code)]
+impl ServiceStream {
+    pub fn new(connection: Connection) -> Self {
+        Self {
+            connection,
+            buffer: BytesMut::new(),
+        }
+    }
+
+    // Note: this is not cancel safe.
+    pub async fn recv(&mut self) -> Option<Request> {
+        while self.buffer.len() < 5 {
+            if self.connection.stream.read_buf(&mut self.buffer).await.ok()? == 0 {
+                return None;
+            }
+        }
+
+        let len = self.buffer.get_u32() as usize;
+
+        if len == 0 || len > 1025 {
+            return None;
+        }
+
+        self.buffer.reserve(len + 4);
+
+        while self.buffer.len() < len {
+            if self.connection.stream.read_buf(&mut self.buffer).await.ok()? == 0 {
+                return None;
+            }
+        }
+
+        serde_json::from_slice(&self.buffer.split_to(len)).ok()
+    }
+
+    pub async fn send(&mut self, data: &[u8]) -> anyhow::Result<()> {
+        self.connection.start_write(data.len()).await?;
+        self.connection.write_all(data).await?;
+        Ok(())
+    }
 }
