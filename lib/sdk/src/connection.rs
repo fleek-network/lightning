0860f8857bebdea19016a37f2b11453574a7432d
Parsa Ghadimi <i@parsa.ooo>
2024-02-07 12:43:12 -0500

    define header type

--- lib/sdk/src/connection.rs
+++ lib/sdk/src/connection.rs
@@ -1,39 +1,73 @@
 use std::pin::Pin;
 
-use fleek_crypto::ClientPublicKey;
 use tokio::io::{AsyncRead, AsyncWrite, AsyncWriteExt};
 use tokio::net::{UnixListener, UnixStream};
+use tokio::sync::mpsc;
+
+use crate::header::{read_header, ConnectionHeader, TransportDetail};
 
 /// Listener for incoming connections
 pub struct ConnectionListener {
-    pub listener: UnixListener,
+    rx: mpsc::Receiver<std::io::Result<Connection>>,
 }
 
 impl ConnectionListener {
+    /// Create a new connection listener from the provided unix socket.
+    pub fn new(listener: UnixListener) -> Self {
+        let (tx, rx) = mpsc::channel(32);
+        let this = Self { rx };
+
+        // Since creation of a connection is a task that awaits at two different points (1-
+        // Accepting new streams. 2- Reading the connection header.) we can't simply await
+        // the two in the accept function because otherwise the `accept` would not be cancel
+        // safe and a slow header coming in would block the progress of other connections.
+        // So we use a channel to send the accepted connections after getting the header to
+        // the caller of `accept`.
+        tokio::spawn(async move {
+            loop {
+                let maybe_stream = listener.accept().await.map(|(v, _)| v);
+                let stream = match maybe_stream {
+                    Err(e) => {
+                        let _ = tx.send(Err(e)).await;
+                        break;
+                    },
+                    Ok(s) => s,
+                };
+
+                let tx = tx.clone();
+                tokio::spawn(async move {
+                    let conn = Connection::new_with_info(stream).await;
+                    let _ = tx.send(conn).await;
+                });
+            }
+        });
+
+        this
+    }
+
     /// Accept a new connection
-    pub async fn accept(&self) -> std::io::Result<Connection> {
-        let (stream, _) = self.listener.accept().await?;
-        Connection::new_with_info(stream).await
+    pub async fn accept(&mut self) -> std::io::Result<Connection> {
+        self.rx
+            .recv()
+            .await
+            .expect("Could not get new connection from the socket.")
     }
 }
 
 /// A client connection
 pub struct Connection {
     pub stream: UnixStream,
-    pub _id: u64,
-    pub _pubkey: ClientPublicKey,
+    pub header: ConnectionHeader,
     // TODO: Should the wrapper have a debug assertion to ensure the correct
     //       number of bytes are written to the stream?
 }
 
 impl Connection {
-    async fn new_with_info(stream: UnixStream) -> std::io::Result<Self> {
-        // TODO: read client information and connection id from a hello frame
-        Ok(Self {
-            stream,
-            _id: 0,
-            _pubkey: ClientPublicKey([0; 96]),
-        })
+    async fn new_with_info(mut stream: UnixStream) -> std::io::Result<Self> {
+        let header = read_header(&mut stream)
+            .await
+            .ok_or(std::io::ErrorKind::Other)?;
+        Ok(Self { stream, header })
     }
 
     /// Start writing a payload to the handshake server for a new payload to the client.
@@ -60,12 +94,25 @@ impl Connection {
         self.stream.write_u32(len as u32).await?;
         Ok(())
     }
+
+    /// Returns true if this connection is an HTTP request.
+    pub fn is_http_request(&self) -> bool {
+        matches!(
+            self.header.transport_detail,
+            TransportDetail::HttpRequest { .. }
+        )
+    }
+
+    /// Returns true if this connection is an anonymous connection without a public key.
+    pub fn is_anonymous(&self) -> bool {
+        self.header.pk.is_none()
+    }
 }
 
 impl AsyncWrite for Connection {
     #[inline(always)]
     fn poll_write(
-        self: std::pin::Pin<&mut Self>,
+        self: Pin<&mut Self>,
         cx: &mut std::task::Context<'_>,
         buf: &[u8],
     ) -> std::task::Poll<Result<usize, std::io::Error>> {
@@ -74,7 +121,7 @@ impl AsyncWrite for Connection {
 
     #[inline(always)]
     fn poll_flush(
-        self: std::pin::Pin<&mut Self>,
+        self: Pin<&mut Self>,
         cx: &mut std::task::Context<'_>,
     ) -> std::task::Poll<Result<(), std::io::Error>> {
         Pin::new(&mut self.get_mut().stream).poll_flush(cx)
@@ -82,7 +129,7 @@ impl AsyncWrite for Connection {
 
     #[inline(always)]
     fn poll_shutdown(
-        self: std::pin::Pin<&mut Self>,
+        self: Pin<&mut Self>,
         cx: &mut std::task::Context<'_>,
     ) -> std::task::Poll<Result<(), std::io::Error>> {
         Pin::new(&mut self.get_mut().stream).poll_shutdown(cx)
