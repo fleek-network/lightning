b2c044fab06b0ce91d42bd11adb9bb97c29f8291
Parsa Ghadimi <i@parsa.ooo>
2024-02-09 17:10:31 -0500

    handshake: send http request on http transport

--- core/handshake/src/transports/http/handler.rs
+++ core/handshake/src/transports/http/handler.rs
@@ -1,116 +1,46 @@
 use std::collections::HashMap;
 use std::fmt::Display;
+use std::str::FromStr;
 
 use axum::body::Body;
-use axum::extract::{Path, Query};
-use axum::http::StatusCode;
+use axum::extract::{OriginalUri, Path, Query};
+use axum::http::{Method, StatusCode, Uri};
 use axum::response::{IntoResponse, Response};
 use axum::Extension;
-use cid::Cid;
+use bytes::Bytes;
 use fleek_crypto::{ClientPublicKey, ClientSignature};
-use fleek_service_js_poc::stream::{Origin, Request};
+use fn_sdk::header::{HttpMethod, TransportDetail};
 use lightning_interfaces::ExecutorProviderInterface;
 use lightning_schema::handshake::{HandshakeRequestFrame, RequestFrame};
-use serde_json::Value;
 use tokio::sync::oneshot;
+use url::Url;
 
 use crate::handshake::Context;
 use crate::transports::http::{HttpReceiver, HttpSender, Service};
 
-pub async fn fetcher_service_handler<P: ExecutorProviderInterface>(
-    Path((origin, uri)): Path<(String, String)>,
+pub async fn handler<P: ExecutorProviderInterface>(
+    method: Method,
+    OriginalUri(uri): OriginalUri,
+    Path((service_id, path)): Path<(String, String)>,
     Query(params): Query<HashMap<String, String>>,
     Extension(provider): Extension<Context<P>>,
+    payload: Bytes,
 ) -> Result<impl IntoResponse, (StatusCode, String)> {
-    let origin = match origin.as_str() {
-        "ipfs" => Origin::Ipfs,
-        "blake3" => Origin::Blake3,
-        _ => return Err(bad_request("unknown origin")),
-    };
-    let uri = match origin {
-        Origin::Blake3 => hex::decode(uri).map_err(|_| bad_request("invalid uri value"))?,
-        Origin::Ipfs => Cid::try_from(uri)
-            .map_err(|_| bad_request("invalid uri value"))?
-            .into(),
-        Origin::Unknown => unreachable!(),
+    let id = u32::from_str(&service_id)
+        .map_err(|_| (StatusCode::NOT_FOUND, "route not found".to_string()))?;
+
+    let method = match method {
+        Method::GET => HttpMethod::Get,
+        Method::POST => HttpMethod::Post,
+        Method::PUT => HttpMethod::Put,
+        Method::DELETE => HttpMethod::Delete,
+        _ => return Err((StatusCode::NOT_FOUND, "invalid method".to_string())),
     };
 
-    let mut payload = Vec::with_capacity(1 + uri.len());
-    payload.push(origin as u8);
-    payload.extend(uri);
+    let body_frame = RequestFrame::ServicePayload { bytes: payload };
 
-    let request_frame = RequestFrame::ServicePayload {
-        bytes: payload.into(),
-    };
     let handshake_frame = HandshakeRequestFrame::Handshake {
-        service: 0,
-        pk: ClientPublicKey([0; 96]),
-        pop: ClientSignature([0; 48]),
-        retry: None,
-    };
-
-    let (frame_tx, frame_rx) = async_channel::bounded(8);
-    let (body_tx, body_rx) = async_channel::bounded(1024);
-    let (termination_tx, termination_rx) = oneshot::channel();
-
-    let sender = HttpSender::new(Service::Fetcher, frame_tx, body_tx, termination_tx);
-    let receiver = HttpReceiver::new(frame_rx);
-    let body = Body::from_stream(body_rx);
-
-    sender.frame_tx.try_send(Some(request_frame)).map_err(|_| {
-        (
-            StatusCode::INTERNAL_SERVER_ERROR,
-            "unexpected error".to_string(),
-        )
-    })?;
-
-    provider
-        .handle_new_connection(handshake_frame, sender, receiver)
-        .await;
-
-    // If there is an error while streaming, the status header has already been sent,
-    // this is a hacky way of returning an error status before beginning streaming the body.
-    match termination_rx.await {
-        Ok(reason) => Err(bad_request(format!("handshake failed: {reason:?}"))),
-        Err(_) => {
-            let mut builder = Response::builder();
-            if let Some(content_type) = params.get("mime") {
-                builder = builder.header("Content-Type", content_type);
-            }
-            builder
-                .body(body)
-                .map_err(|_| bad_request("invalid type value"))
-        },
-    }
-}
-
-pub async fn js_service_handler<P: ExecutorProviderInterface>(
-    Path((origin, uri)): Path<(String, String)>,
-    Query(params): Query<HashMap<String, String>>,
-    Extension(provider): Extension<Context<P>>,
-) -> Result<impl IntoResponse, (StatusCode, String)> {
-    let origin = match origin.as_str() {
-        "ipfs" => Origin::Ipfs,
-        "blake3" => Origin::Blake3,
-        _ => return Err(bad_request("unknown origin")),
-    };
-    let param = params
-        .get("param")
-        .map(|param| serde_json::from_str::<Value>(param).map_err(|_| bad_request("invalid param")))
-        .transpose()?;
-
-    let request_frame = RequestFrame::ServicePayload {
-        bytes: serde_json::to_string(&Request {
-            origin,
-            uri,
-            param,
-            path: None,
-        })
-        .map_err(|_| bad_request("failed to encode request"))?
-        .into(),
-    };
-    let handshake_frame = HandshakeRequestFrame::Handshake {
-        service: 1,
+        service: id,
         pk: ClientPublicKey([0; 96]),
         pop: ClientSignature([0; 48]),
         retry: None,
@@ -121,10 +51,18 @@ pub async fn js_service_handler<P: ExecutorProviderInterface>(
     let (termination_tx, termination_rx) = oneshot::channel();
 
     let sender = HttpSender::new(Service::Js, frame_tx, body_tx, termination_tx);
-    let receiver = HttpReceiver::new(frame_rx);
+    let receiver = HttpReceiver::new(
+        frame_rx,
+        TransportDetail::HttpRequest {
+            method,
+            uri: extract_url(&path, uri),
+            // TODO
+            header: Default::default(),
+        },
+    );
     let body = Body::from_stream(body_rx);
 
-    sender.frame_tx.try_send(Some(request_frame)).map_err(|_| {
+    sender.frame_tx.try_send(Some(body_frame)).map_err(|_| {
         (
             StatusCode::INTERNAL_SERVER_ERROR,
             "unexpected error".to_string(),
@@ -154,4 +92,12 @@ pub async fn js_service_handler<P: ExecutorProviderInterface>(
 #[inline(always)]
 fn bad_request<T: AsRef<str> + Display>(msg: T) -> (StatusCode, String) {
     (StatusCode::BAD_REQUEST, msg.to_string())
+}
+
+#[inline(always)]
+fn extract_url(path: &str, url: Uri) -> Url {
+    let mut result = Url::parse("http://fleek/").unwrap();
+    result.set_path(path);
+    result.set_query(url.query());
+    result
 }
