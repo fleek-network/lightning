6be10725f18f092c3560c7026855eb7ff6b57cd8
Miguel <mi9uel9@gmail.com>
2024-02-21 21:52:26 -0500

    ebpf: drop packets for sources in blocklist

--- ebpf/tea/src/bin/xdp_lightning_app.rs
+++ ebpf/tea/src/bin/xdp_lightning_app.rs
@@ -1,4 +1,7 @@
+use std::net::Ipv4Addr;
+
 use anyhow::Context;
+use aya::maps::HashMap;
 use aya::programs::{Xdp, XdpFlags};
 use aya::{include_bytes_aligned, Bpf};
 use aya_log::BpfLogger;
@@ -10,6 +13,9 @@ struct Opts {
     /// Interface to attach xdp program to.
     #[clap(short, long, default_value = "eth0")]
     iface: String,
+    /// Ip address to block.
+    #[clap(short, long)]
+    block: Option<Ipv4Addr>,
 }
 
 #[tokio::main]
@@ -23,7 +29,7 @@ async fn main() -> Result<(), anyhow::Error> {
         "../../../xdp/target/bpfel-unknown-none/debug/packet_filter"
     ))?;
     #[cfg(not(debug_assertions))]
-    let mut bpf = Bpf::load(include_bytes_aligned!(
+    let mut handle = Bpf::load(include_bytes_aligned!(
         "../../../xdp/target/bpfel-unknown-none/release/packet_filter"
     ))?;
 
@@ -39,6 +45,14 @@ async fn main() -> Result<(), anyhow::Error> {
     program
         .attach(&opt.iface, XdpFlags::default())
         .context("failed to attach the XDP program")?;
+
+    let mut blocklist: HashMap<_, u32, u32> =
+        HashMap::try_from(handle.map_mut("BLOCK_LIST").unwrap())?;
+
+    if let Some(block_addr) = opt.block {
+        let addr: u32 = block_addr.try_into()?;
+        blocklist.insert(addr, 0, 0)?;
+    }
 
     log::info!("Enter Ctrl-C to shutdown");
     signal::ctrl_c().await?;
--- ebpf/xdp/src/bin/packet_filter.rs
+++ ebpf/xdp/src/bin/packet_filter.rs
@@ -4,12 +4,16 @@
 use core::mem;
 
 use aya_bpf::bindings::xdp_action;
-use aya_bpf::macros::xdp;
+use aya_bpf::macros::{map, xdp};
+use aya_bpf::maps::HashMap;
 use aya_bpf::programs::XdpContext;
 use aya_log_ebpf::info;
 use memoffset::offset_of;
 use network_types::eth::{EthHdr, EtherType};
 use network_types::ip::Ipv4Hdr;
+
+#[map]
+static BLOCK_LIST: HashMap<u32, u32> = HashMap::<u32, u32>::with_max_entries(1024, 0);
 
 #[xdp]
 pub fn xdp_packet_filter(ctx: XdpContext) -> u32 {
@@ -28,7 +32,13 @@ unsafe fn filter(ctx: XdpContext) -> Result<u32, ()> {
     let source =
         u32::from_be_bytes(unsafe { *ptr_at(&ctx, EthHdr::LEN + offset_of!(Ipv4Hdr, src_addr))? });
     info!(&ctx, "received a packet from {:i}", source);
-    Ok(xdp_action::XDP_PASS)
+
+    // Todo: let's filter by port as well.
+    if not_allowed(source) {
+        Ok(xdp_action::XDP_DROP)
+    } else {
+        Ok(xdp_action::XDP_PASS)
+    }
 }
 
 fn ptr_at<T>(ctx: &XdpContext, offset: usize) -> Result<*const T, ()> {
@@ -41,6 +51,10 @@ fn ptr_at<T>(ctx: &XdpContext, offset: usize) -> Result<*const T, ()> {
     }
 
     Ok((start + offset) as *const T)
+}
+
+fn not_allowed(address: u32) -> bool {
+    unsafe { BLOCK_LIST.get(&address).is_some() }
 }
 
 #[panic_handler]
