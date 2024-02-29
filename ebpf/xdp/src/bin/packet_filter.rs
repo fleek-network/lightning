35967c88ba88a9732db03be206b54309bdc9396a
Miguel <miguel@fleek.xyz>
2024-02-28 20:38:32 -0500

    ebpf: parse port from transport headers

--- ebpf/xdp/src/bin/packet_filter.rs
+++ ebpf/xdp/src/bin/packet_filter.rs
@@ -10,7 +10,11 @@ use aya_bpf::programs::XdpContext;
 use aya_log_ebpf::info;
 use memoffset::offset_of;
 use network_types::eth::{EthHdr, EtherType};
-use network_types::ip::Ipv4Hdr;
+use network_types::ip::{IpProto, Ipv4Hdr};
+use network_types::tcp::TcpHdr;
+use network_types::udp::UdpHdr;
+
+type XdpAction = xdp_action::Type;
 
 #[map]
 static BLOCK_LIST: HashMap<u32, u32> = HashMap::<u32, u32>::with_max_entries(1024, 0);
@@ -19,28 +23,45 @@ static BLOCK_LIST: HashMap<u32, u32> = HashMap::<u32, u32>::with_max_entries(102
 pub fn xdp_packet_filter(ctx: XdpContext) -> u32 {
     match unsafe { filter(ctx) } {
         Ok(ret) => ret,
-        // Todo: let's handle this differently.
         Err(_) => xdp_action::XDP_ABORTED,
     }
 }
 
 unsafe fn filter(ctx: XdpContext) -> Result<u32, ()> {
     let h_proto = unsafe { *ptr_at::<EtherType>(&ctx, offset_of!(EthHdr, ether_type))? };
-    if h_proto != EtherType::Ipv4 {
-        return Ok(xdp_action::XDP_PASS);
+    match h_proto {
+        EtherType::Ipv4 => process_ipv4(&ctx),
+        _ => Ok(xdp_action::XDP_PASS),
     }
-    let source =
-        u32::from_be_bytes(unsafe { *ptr_at(&ctx, EthHdr::LEN + offset_of!(Ipv4Hdr, src_addr))? });
-    info!(&ctx, "received a packet from {:i}", source);
+}
 
-    // Todo: let's filter by port as well.
-    if not_allowed(source) {
+fn process_ipv4(ctx: &XdpContext) -> Result<XdpAction, ()> {
+    let proto = unsafe { *ptr_at::<IpProto>(&ctx, EthHdr::LEN + offset_of!(Ipv4Hdr, proto))? };
+    let ip =
+        u32::from_be_bytes(unsafe { *ptr_at(&ctx, EthHdr::LEN + offset_of!(Ipv4Hdr, src_addr))? });
+
+    info!(ctx, "received a packet from {:i}", ip);
+
+    let port = match proto {
+        IpProto::Tcp => u16::from_be_bytes(unsafe {
+            *ptr_at(&ctx, EthHdr::LEN + Ipv4Hdr::LEN + offset_of!(TcpHdr, dest))?
+        }),
+        IpProto::Udp => u16::from_be_bytes(unsafe {
+            *ptr_at(&ctx, EthHdr::LEN + Ipv4Hdr::LEN + offset_of!(UdpHdr, dest))?
+        }),
+        _ => {
+            return Ok(xdp_action::XDP_PASS);
+        },
+    };
+
+    if not_allowed(ip, port) {
         Ok(xdp_action::XDP_DROP)
     } else {
         Ok(xdp_action::XDP_PASS)
     }
 }
 
+// Before any data access, the verifier requires us to do a bound check.
 fn ptr_at<T>(ctx: &XdpContext, offset: usize) -> Result<*const T, ()> {
     let start = ctx.data();
     let end = ctx.data_end();
@@ -53,7 +74,7 @@ fn ptr_at<T>(ctx: &XdpContext, offset: usize) -> Result<*const T, ()> {
     Ok((start + offset) as *const T)
 }
 
-fn not_allowed(address: u32) -> bool {
+fn not_allowed(address: u32, _port: u16) -> bool {
     unsafe { BLOCK_LIST.get(&address).is_some() }
 }
 
