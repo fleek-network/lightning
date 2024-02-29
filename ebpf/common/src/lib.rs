c22ef00ef9bcb75010c0b8bb0fb6607f030f14ec
Miguel <miguel@fleek.xyz>
2024-02-29 17:44:30 -0500

    ebpf: update userspace prog

--- ebpf/common/src/lib.rs
+++ ebpf/common/src/lib.rs
@@ -1,7 +1,11 @@
 #![no_std]
 
+#[derive(Copy, Clone)]
 #[repr(C)]
 pub struct IpPortKey {
     pub ip: u32,
     pub port: u32,
 }
+
+#[cfg(feature = "userspace")]
+unsafe impl aya::Pod for IpPortKey {}
