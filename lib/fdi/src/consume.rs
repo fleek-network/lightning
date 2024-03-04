a0e9fb1a4a32b4da4fe84ca884f99aa3d90e0eac
Parsa Ghadimi <i@parsa.ooo>
2024-03-04 14:46:46 -0500

    add docs for consume

--- lib/fdi/src/consume.rs
+++ lib/fdi/src/consume.rs
@@ -7,9 +7,32 @@ pub struct Consume<F, T, P, O> {
     _p: PhantomData<(F, T, P, O)>,
 }
 
-pub fn consume<F, T, P, O>(f: F) -> impl Method<F, (O, T)>
+/// Turns any function that takes an owned value as the first argument into a valid [`Method`].
+///
+/// # Example
+///
+/// ```
+/// use fdi::*;
+///
+/// #[derive(Default)]
+/// struct A;
+///
+/// impl A {
+///     fn start(self) {
+///         //   ^-- The goal is to have access to `self`.
+///         // The function could also take other parameters.
+///     }
+/// }
+///
+/// let mut graph =
+///     DependencyGraph::new().with(A::default.to_infallible().on("start", consume(A::start)));
+/// let mut registry = Registry::default();
+/// graph.init_one::<A>(&mut registry).unwrap();
+/// registry.trigger("start");
+/// ```
+pub fn consume<F, T, P, O>(f: F) -> impl Method<T, (O, P)>
 where
-    Consume<F, T, P, O>: Method<F, (O, T)>,
+    Consume<F, T, P, O>: Method<T, (O, P)>,
 {
-    Consume { f, _p: PhantomData }
+    Consume::<F, T, P, O> { f, _p: PhantomData }
 }
