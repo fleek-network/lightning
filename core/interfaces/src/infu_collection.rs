6b5d4f6e16868ca6bc7925dba8c10069751994e9
ozwaldorf <self@ossian.dev>
2024-03-04 21:00:04 -0500

    chore(collection): add custom display names to all deps

--- core/interfaces/src/infu_collection.rs
+++ core/interfaces/src/infu_collection.rs
@@ -1,10 +1,9 @@
 use std::marker::PhantomData;
 
-use fdi::{consume, DependencyGraph, MethodExt, Registry};
+use fdi::{DependencyGraph, MethodExt, Registry};
 use futures::executor::block_on;
 pub use infusion::c;
 use infusion::collection;
-use tokio::runtime::Handle;
 
 use super::*;
 
@@ -46,45 +45,59 @@ pub struct Node<C: Collection> {
 impl<C: Collection> Node<C> {
     pub fn init(config: C::ConfigProviderInterface) -> Result<Self, infusion::InitializationError> {
         let graph = DependencyGraph::new()
-            .with_infallible(Handle::current) // tokio runtime handle
-            .with_infallible(|| config)
-            .with(C::KeystoreInterface::infu_initialize_hack)
-            .with(C::BlockStoreInterface::infu_initialize_hack)
-            .with(C::BlockStoreServerInterface::infu_initialize_hack.on(
-                "start",
-                consume(|c: C::BlockStoreServerInterface| block_on(c.start())),
-            ))
+            .with_infallible((|| config).with_display_name("Config_Provider"))
+            .with(C::KeystoreInterface::infu_initialize_hack.with_display_name("Keystore"))
+            .with(C::BlockStoreInterface::infu_initialize_hack.with_display_name("Blockstore"))
+            .with(C::NotifierInterface::infu_initialize_hack.with_display_name("Notifier"))
+            .with(
+                <C::IndexerInterface as IndexerInterface<C>>::infu_initialize_hack
+                    .with_display_name("Indexer"),
+            )
+            .with(
+                C::BlockStoreServerInterface::infu_initialize_hack
+                    .with_display_name("Blockstore_Server")
+                    .on("start", |c: &C::BlockStoreServerInterface| {
+                        block_on(c.start())
+                    }),
+            )
             .with(
                 <C::ApplicationInterface as ApplicationInterface<C>>::infu_initialize_hack
+                    .with_display_name("Application")
                     .on("start", |c: &C::ApplicationInterface| block_on(c.start())),
             )
             .with(
                 C::SyncronizerInterface::infu_initialize_hack
+                    .with_display_name("Syncronizer")
                     .on("start", |c: &C::SyncronizerInterface| block_on(c.start())),
             )
             .with(
                 C::BroadcastInterface::infu_initialize_hack
+                    .with_display_name("Broadcast")
                     .on("start", |c: &C::BroadcastInterface| block_on(c.start())),
             )
             .with(
                 C::TopologyInterface::infu_initialize_hack
+                    .with_display_name("Topology")
                     .on("start", |c: &C::TopologyInterface| block_on(c.start())),
             )
             .with(
                 C::ArchiveInterface::infu_initialize_hack
+                    .with_display_name("Archive")
                     .on("start", |c: &C::ArchiveInterface| block_on(c.start())),
             )
             .with(
                 C::ConsensusInterface::infu_initialize_hack
+                    .with_display_name("Consensus")
                     .on("start", |c: &C::ConsensusInterface| block_on(c.start())),
             )
             .with(
                 C::HandshakeInterface::infu_initialize_hack
+                    .with_display_name("Handshake")
                     .on("start", |c: &C::HandshakeInterface| block_on(c.start())),
             )
-            .with(C::NotifierInterface::infu_initialize_hack)
             .with(
                 C::OriginProviderInterface::infu_initialize_hack
+                    .with_display_name("Origin_Provider")
                     .on("start", |c: &C::OriginProviderInterface| {
                         block_on(c.start())
                     }),
@@ -99,44 +112,51 @@ impl<C: Collection> Node<C> {
             // )
             .with(
                 C::ReputationAggregatorInterface::infu_initialize_hack
+                    .with_display_name("Reputation_Aggregator")
                     .on("start", |c: &C::ReputationAggregatorInterface| {
                         block_on(c.start())
                     }),
             )
             .with(
                 <C::ResolverInterface as ResolverInterface<C>>::infu_initialize_hack
+                    .with_display_name("Resolver")
                     .on("start", |c: &C::ResolverInterface| block_on(c.start())),
             )
             .with(
                 C::RpcInterface::infu_initialize_hack
+                    .with_display_name("Rpc")
                     .on("start", |c: &C::RpcInterface| block_on(c.start())),
             )
             .with(
                 C::ServiceExecutorInterface::infu_initialize_hack
+                    .with_display_name("Service_Executor")
                     .on("start", |c: &C::ServiceExecutorInterface| {
                         block_on(c.start())
                     }),
             )
             .with(
                 C::SignerInterface::infu_initialize_hack
+                    .with_display_name("Signer")
                     .on("_post", C::SignerInterface::infu_post_hack)
                     .on("start", |c: &C::SignerInterface| block_on(c.start())),
             )
             .with(
                 C::FetcherInterface::infu_initialize_hack
+                    .with_display_name("Fetcher")
                     .on("start", |c: &C::FetcherInterface| block_on(c.start())),
             )
             .with(
                 C::PoolInterface::infu_initialize_hack
+                    .with_display_name("Pool")
                     .on("start", |c: &C::PoolInterface| block_on(c.start())),
             )
             .with(
                 C::PingerInterface::infu_initialize_hack
+                    .with_display_name("Pinger")
                     .on("start", |c: &C::PingerInterface| block_on(c.start())),
-            )
-            .with(<C::IndexerInterface as IndexerInterface<C>>::infu_initialize_hack);
+            );
 
-        let vis = graph.viz();
+        let vis = graph.viz("Lightning Dependency Graph");
         println!("{vis}");
 
         let mut container = Registry::default();
