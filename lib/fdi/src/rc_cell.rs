26d213e146e3b3b01e7e31ac3d8e7aa8fa64dc8c
Parsa Ghadimi <i@parsa.ooo>
2024-03-06 11:32:43 -0500

    make registry references owned

--- lib/fdi/src/rc_cell.rs
+++ lib/fdi/src/rc_cell.rs
@@ -45,11 +45,13 @@ impl<T> RcCell<T> {
 
     pub fn borrow_mut(&self) -> RefMut<T> {
         self.cell.ensure_not_mut();
+        self.cell.ensure_not_borrowed();
         RefMut::new(Cell(self.cell.0))
     }
 }
 
 impl<T> Cell<T> {
+    #[inline(always)]
     fn ensure_not_mut(&self) {
         let unsafe_cell = unsafe { self.0.as_ref() };
         let inner = unsafe { &*unsafe_cell.get() };
@@ -58,42 +60,58 @@ impl<T> Cell<T> {
         }
     }
 
+    #[inline(always)]
+    fn ensure_not_borrowed(&self) {
+        let unsafe_cell = unsafe { self.0.as_ref() };
+        let inner = unsafe { &*unsafe_cell.get() };
+        if inner.borrowed > 0 {
+            panic!("Could not borrow RcCell: borrow already exists.");
+        }
+    }
+
+    #[inline(always)]
     fn mark_mut_borrow(&self) {
         let unsafe_cell = unsafe { self.0.as_ref() };
         let inner = unsafe { &mut *unsafe_cell.get() };
         inner.mutable = true;
     }
 
+    #[inline(always)]
     fn unmark_mut_borrow(&self) {
         let unsafe_cell = unsafe { self.0.as_ref() };
         let inner = unsafe { &mut *unsafe_cell.get() };
         inner.mutable = false;
     }
 
+    #[inline(always)]
     fn unmark_cell(&self) {
         let unsafe_cell = unsafe { self.0.as_ref() };
         let inner = unsafe { &mut *unsafe_cell.get() };
         inner.cell = false;
     }
 
+    #[inline(always)]
     fn add_borrow(&self) {
         let unsafe_cell = unsafe { self.0.as_ref() };
         let inner = unsafe { &mut *unsafe_cell.get() };
         inner.borrowed += 1;
     }
 
+    #[inline(always)]
     fn remove_borrow(&self) {
         let unsafe_cell = unsafe { self.0.as_ref() };
         let inner = unsafe { &mut *unsafe_cell.get() };
         inner.borrowed -= 1;
     }
 
+    #[inline(always)]
     unsafe fn get(&self) -> &T {
         let unsafe_cell = unsafe { self.0.as_ref() };
         let inner = unsafe { &*unsafe_cell.get() };
         &inner.value
     }
 
+    #[inline(always)]
     #[allow(clippy::mut_from_ref)]
     unsafe fn get_mut(&self) -> &mut T {
         let unsafe_cell = unsafe { self.0.as_ref() };
@@ -103,6 +121,7 @@ impl<T> Cell<T> {
 }
 
 impl<T> Ref<T> {
+    #[inline(always)]
     fn new(cell: Cell<T>) -> Self {
         cell.add_borrow();
         Self { cell }
@@ -110,6 +129,7 @@ impl<T> Ref<T> {
 }
 
 impl<T> RefMut<T> {
+    #[inline(always)]
     fn new(cell: Cell<T>) -> Self {
         cell.mark_mut_borrow();
         Self { cell }
@@ -165,5 +185,79 @@ impl<T> Drop for Cell<T> {
             let b = unsafe { Box::from_raw(ptr) };
             drop(b);
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::RcCell;
+
+    #[test]
+    fn borrow_borrow() {
+        let obj = RcCell::new(String::from("Hello"));
+        let a = obj.borrow();
+        let b = obj.borrow();
+        drop((a, b));
+    }
+
+    #[test]
+    fn borrow_drop_borrow_mut() {
+        let obj = RcCell::new(String::from("Hello"));
+        let a = obj.borrow();
+        drop(a);
+        let b = obj.borrow_mut();
+        drop(b);
+    }
+
+    #[test]
+    fn borrow_mut_drop_borrow_mut() {
+        let obj = RcCell::new(String::from("Hello"));
+        let a = obj.borrow_mut();
+        drop(a);
+        let b = obj.borrow_mut();
+        drop(b);
+    }
+
+    #[test]
+    fn borrow_mut_drop_borrow() {
+        let obj = RcCell::new(String::from("Hello"));
+        let a = obj.borrow_mut();
+        drop(a);
+        let b = obj.borrow();
+        drop(b);
+    }
+
+    #[test]
+    #[should_panic(expected = "Could not borrow RcCell: borrow already exists.")]
+    fn borrow_borrow_mut() {
+        let obj = RcCell::new(String::from("Hello"));
+        let a = obj.borrow();
+        let b = obj.borrow_mut();
+        drop((a, b));
+    }
+
+    #[test]
+    #[should_panic(expected = "Could not borrow RcCell: mutable borrow already exists")]
+    fn borrow_mut_borrow_mut() {
+        let obj = RcCell::new(String::from("Hello"));
+        let a = obj.borrow_mut();
+        let b = obj.borrow_mut();
+        drop((a, b));
+    }
+
+    #[test]
+    fn borrow_mut_obj_drop() {
+        let obj = RcCell::new(String::from("Hello"));
+        let a = obj.borrow_mut();
+        drop(obj);
+        assert_eq!(*a, "Hello");
+    }
+
+    #[test]
+    fn borrow_obj_drop() {
+        let obj = RcCell::new(String::from("Hello"));
+        let a = obj.borrow();
+        drop(obj);
+        assert_eq!(*a, "Hello");
     }
 }
