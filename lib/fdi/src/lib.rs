d479475e75eef71ec0ad087f8d18e0af3530da16
Parsa Ghadimi <i@parsa.ooo>
2024-03-06 10:54:02 -0500

    wip: make registry refs to owned references

--- lib/fdi/src/lib.rs
+++ lib/fdi/src/lib.rs
@@ -1,16 +1,19 @@
 mod consume;
 pub mod event;
+mod executor;
 mod ext;
 pub mod graph;
 mod helpers;
 mod impl_tuple;
 pub mod method;
 pub mod object;
+mod rc_cell;
 pub mod registry;
 pub mod ty;
 
 pub use consume::consume;
 pub use event::Eventstore;
+pub use executor::Executor;
 pub use ext::MethodExt;
 pub use graph::DependencyGraph;
 pub use method::Method;
