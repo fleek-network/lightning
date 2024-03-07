9781a47a076073162955f2468fb152281de90558
Parsa Ghadimi <i@parsa.ooo>
2024-03-07 16:24:36 -0500

    make every mod private

--- lib/fdi/src/lib.rs
+++ lib/fdi/src/lib.rs
@@ -1,15 +1,15 @@
 mod consume;
-pub mod event;
+mod event;
 mod executor;
 mod ext;
-pub mod graph;
+mod graph;
 mod helpers;
 mod impl_tuple;
-pub mod method;
-pub mod object;
+mod method;
+mod object;
 mod rc_cell;
-pub mod registry;
-pub mod ty;
+mod registry;
+mod ty;
 
 pub use consume::consume;
 pub use event::Eventstore;
