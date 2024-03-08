fb231e8d5fe87611eae7248e4c4c1435d5a3e6d9
Parsa Ghadimi <i@parsa.ooo>
2024-03-08 18:17:21 -0500

    wip: try unsafe stuff

--- lib/fdi/src/event.rs
+++ lib/fdi/src/event.rs
@@ -2,64 +2,65 @@ use std::collections::HashMap;
 
 use indexmap::IndexSet;
 
-use crate::method::{DynMethod, Method};
+use crate::method::Method;
 use crate::ty::Ty;
 use crate::Provider;
 
 /// The [`Eventstore`] can be used to store a list of event handlers under each event name.
 #[derive(Default)]
 pub struct Eventstore {
-    pub handlers: HashMap<&'static str, Vec<DynMethod>>,
+    // pub handlers: HashMap<&'static str, Vec<DynMethod>>,
 }
 
-impl Eventstore {
-    /// Extend the current event store with another event store.
-    pub fn extend(&mut self, other: Eventstore) {
-        for (ev, handlers) in other.handlers {
-            self.handlers.entry(ev).or_default().extend(handlers);
-        }
-    }
+// impl Eventstore {
+//     /// Extend the current event store with another event store.
+//     pub fn extend(&mut self, other: Eventstore) {
+//         for (ev, handlers) in other.handlers {
+//             self.handlers.entry(ev).or_default().extend(handlers);
+//         }
+//     }
 
-    /// Return a set of all of the dependencies required to trigger an event.
-    pub fn get_dependencies(&self, event: &'static str) -> IndexSet<Ty> {
-        let mut result = IndexSet::new();
+//     /// Return a set of all of the dependencies required to trigger an event.
+//     pub fn get_dependencies(&self, event: &'static str) -> IndexSet<Ty> {
+//         let mut result = IndexSet::new();
 
-        if let Some(handlers) = self.handlers.get(event) {
-            for handler in handlers {
-                result.extend(handler.dependencies().iter());
-            }
-        }
+//         if let Some(handlers) = self.handlers.get(event) {
+//             for handler in handlers {
+//                 result.extend(handler.dependencies().iter());
+//             }
+//         }
 
-        result
-    }
+//         result
+//     }
 
-    pub(crate) fn insert(&mut self, event: &'static str, handler: DynMethod) {
-        if handler.events().is_some() {
-            panic!("Event handler can not be a WithEvents.");
-        }
+//     pub(crate) fn insert(&mut self, event: &'static str, handler: dynmethod) {
+//         if handler.events().is_some() {
+//             panic!("event handler can not be a withevents.");
+//         }
 
-        self.handlers.entry(event).or_default().push(handler);
-    }
+//         self.handlers.entry(event).or_default().push(handler);
+//     }
 
-    /// Register a new handler for the given event. The handler will only be called once when the
-    /// event is triggered.
-    pub fn on<F, T, P>(&mut self, event: &'static str, handler: F)
-    where
-        F: Method<T, P>,
-        T: 'static,
-    {
-        self.insert(event, DynMethod::new(handler))
-    }
+//     // /// Register a new handler for the given event. The handler will only be called once when
+// the     // /// event is triggered.
+//     // pub fn on<F, T, P>(&mut self, event: &'static str, handler: F)
+//     // where
+//     //     F: Method<T, P>,
+//     //     T: 'static,
+//     // {
+//     //     self.insert(event, DynMethod::new(handler))
+//     // }
 
-    /// Trigger the event. This is used internally.
-    pub(crate) fn trigger(&mut self, event: &'static str, registry: &Provider) -> usize {
-        let mut result = 0;
-        if let Some(handlers) = self.handlers.remove(event) {
-            for handler in handlers {
-                result += 1;
-                handler.call(registry);
-            }
-        }
-        result
-    }
-}
+//     /// Trigger the event. This is used internally.
+//     pub(crate) fn trigger(&mut self, event: &'static str, registry: &Provider) -> usize {
+//         let mut result = 0;
+//         if let Some(handlers) = self.handlers.remove(event) {
+//             for handler in handlers {
+//                 result += 1;
+//                 let guard = registry.guard();
+//                 handler.call(guard);
+//             }
+//         }
+//         result
+//     }
+// }
