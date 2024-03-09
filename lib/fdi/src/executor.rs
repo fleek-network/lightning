92311e34dc3d66f1f7f2fbc0175b46c8f064eb1f
Parsa Ghadimi <i@parsa.ooo>
2024-03-09 14:08:43 -0500

    improve async executor

--- lib/fdi/src/executor.rs
+++ lib/fdi/src/executor.rs
@@ -1,20 +1,36 @@
-use futures::future::LocalBoxFuture;
+use futures::future::{BoxFuture, LocalBoxFuture};
 use futures::stream::FuturesUnordered;
 use futures::{Future, StreamExt};
 
 #[derive(Default)]
 pub struct Executor {
+    spawn_fn: Option<Box<dyn Fn(BoxFuture<'static, ()>)>>,
     futures: FuturesUnordered<LocalBoxFuture<'static, ()>>,
 }
 
 impl Executor {
+    /// Set the spawn callback that the executor should proxy async tasks to.
+    pub fn set_spawn_cb<F>(&mut self, spawn: F)
+    where
+        F: 'static + Fn(BoxFuture<'static, ()>),
+    {
+        if self.spawn_fn.is_some() {
+            panic!("Executor spawn callback is already set.");
+        }
+        self.spawn_fn = Some(Box::new(spawn));
+    }
+
     /// Insert the given future into this executor. Note that inserting the future does not drive it
     /// forward unless the executor is awaited on.
-    pub fn spawn<F, O>(&mut self, fut: F)
+    pub fn spawn<F, O>(&self, fut: F)
     where
-        F: Future<Output = O> + 'static,
+        F: Future<Output = O> + Send + 'static,
     {
-        self.futures.push(Box::pin(async move {
+        let f = self
+            .spawn_fn
+            .as_ref()
+            .unwrap_or_else(|| panic!("Executor spawn callback is not set."));
+        (f)(Box::pin(async move {
             fut.await;
         }));
     }
