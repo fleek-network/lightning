5dfb436ae5642535e75bfa871cbd37ae20b762fb
Parsa Ghadimi <i@parsa.ooo>
2024-03-09 12:19:41 -0500

    fix dep-graph

--- lib/fdi/src/ext.rs
+++ lib/fdi/src/ext.rs
@@ -2,19 +2,19 @@ use futures::Future;
 
 use crate::{helpers, Method};
 
-pub trait MethodExt<'a, P>: Sized + Method<'a, P> {
+pub trait MethodExt<P>: Sized + Method<P> {
     #[inline(always)]
-    fn with_display_name(self, name: &'static str) -> impl Method<'a, P, Output = Self::Output> {
+    fn with_display_name(self, name: &'static str) -> impl Method<P, Output = Self::Output> {
         helpers::display_name(name, self)
     }
 
     #[inline(always)]
-    fn to_infallible(self) -> impl Method<'a, P, Output = anyhow::Result<Self::Output>> {
+    fn to_infallible(self) -> impl Method<P, Output = anyhow::Result<Self::Output>> {
         helpers::to_infalliable(self)
     }
 
     #[inline(always)]
-    fn map<M, U>(self, transform: M) -> impl Method<'a, P, Output = U>
+    fn map<M, U>(self, transform: M) -> impl Method<P, Output = U>
     where
         M: FnOnce(Self::Output) -> U,
         U: 'static,
@@ -23,30 +23,38 @@ pub trait MethodExt<'a, P>: Sized + Method<'a, P> {
     }
 
     #[inline(always)]
-    fn on<H, Y>(self, event: &'static str, handler: H) -> impl Method<'a, P, Output = Self::Output>
+    fn flatten<B>(self) -> impl Method<(P, B), Output = <Self::Output as Method<B>>::Output>
     where
-        H: Method<'a, Y>,
+        Self::Output: Method<B>,
+    {
+        helpers::flatten(self)
+    }
+
+    #[inline(always)]
+    fn on<H, Y>(self, event: &'static str, handler: H) -> impl Method<P, Output = Self::Output>
+    where
+        H: Method<Y>,
     {
         helpers::on(self, event, handler)
     }
 
     #[inline(always)]
-    fn spawn(self) -> impl Method<'a, P, Output = ()>
+    fn spawn(self) -> impl Method<P, Output = ()>
     where
-        Self: 'static + Method<'a, P> + Sized,
+        Self: 'static + Method<P> + Sized,
         Self::Output: Future,
     {
         helpers::spawn(self)
     }
 
     #[inline(always)]
-    fn block_on(self) -> impl Method<'a, P, Output = <Self::Output as Future>::Output>
+    fn block_on(self) -> impl Method<P, Output = <Self::Output as Future>::Output>
     where
-        Self: 'static + Method<'a, P> + Sized,
+        Self: 'static + Method<P> + Sized,
         Self::Output: Future,
     {
         helpers::block_on(self)
     }
 }
 
-impl<'a, F, P> MethodExt<'a, P> for F where F: Method<'a, P> {}
+impl<F, P> MethodExt<P> for F where F: Method<P> {}
