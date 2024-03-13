264fb3659c73c3b9e1792373501c9fc509e2aeee
ozwaldorf <self@ossian.dev>
2024-03-13 02:23:18 -0400

    tests(blockstore-server): use new testing pattern

--- core/blockstore-server/src/tests.rs
+++ core/blockstore-server/src/tests.rs
@@ -3,8 +3,8 @@ use std::collections::VecDeque;
 use std::path::PathBuf;
 use std::time::Duration;
 
-use affair::Socket;
 use blake3_tree::ProofBuf;
+use fdi::Ref;
 use fleek_crypto::{AccountOwnerSecretKey, NodePublicKey, SecretKey};
 use lightning_application::app::Application;
 use lightning_application::config::{Config as AppConfig, Mode, StorageConfig};
@@ -12,7 +12,7 @@ use lightning_application::genesis::{Genesis, GenesisNode};
 use lightning_blockstore::blockstore::{Blockstore, BLOCK_SIZE};
 use lightning_blockstore::config::Config as BlockstoreConfig;
 use lightning_indexer::Indexer;
-use lightning_interfaces::infu_collection::Collection;
+use lightning_interfaces::infu_collection::{Collection, Node};
 use lightning_interfaces::types::{
     CompressionAlgoSet,
     CompressionAlgorithm,
@@ -25,28 +25,23 @@ use lightning_interfaces::{
     BlockStoreInterface,
     BlockStoreServerInterface,
     IncrementalPutInterface,
-    IndexerInterface,
     KeystoreInterface,
-    NotifierInterface,
-    PoolInterface,
-    ReputationAggregatorInterface,
-    SignerInterface,
     SyncQueryRunnerInterface,
-    TopologyInterface,
-    WithStartAndShutdown,
 };
 use lightning_notifier::Notifier;
-use lightning_pool::{muxer, Config as PoolConfig, PoolProvider};
+use lightning_pool::{Config as PoolConfig, PoolProvider};
 use lightning_rep_collector::ReputationAggregator;
 use lightning_signer::Signer;
+use lightning_test_utils::json_config::JsonConfigProvider;
 use lightning_test_utils::keys::EphemeralKeystore;
-use lightning_topology::{Config as TopologyConfig, Topology};
+use lightning_topology::Topology;
 
 use super::BlockStoreServer;
 use crate::blockstore_server::Frame;
 use crate::config::Config;
 
 partial!(TestBinding {
+    ConfigProviderInterface = JsonConfigProvider;
     BlockStoreInterface = Blockstore<Self>;
     BlockStoreServerInterface = BlockStoreServer<Self>;
     ApplicationInterface = Application<Self>;
@@ -67,19 +62,29 @@ fn create_content() -> Vec<u8> {
 }
 
 struct Peer<C: Collection> {
-    pool: C::PoolInterface,
-    blockstore: Blockstore<C>,
-    blockstore_server: C::BlockStoreServerInterface,
-    topology: C::TopologyInterface,
-    _rep_aggregator: C::ReputationAggregatorInterface,
+    inner: Node<C>,
     node_public_key: NodePublicKey,
+}
+
+impl<C: Collection> Peer<C> {
+    fn blockstore(&self) -> Ref<C::BlockStoreInterface> {
+        self.inner.provider.get::<C::BlockStoreInterface>()
+    }
+
+    fn app(&self) -> Ref<C::ApplicationInterface> {
+        self.inner.provider.get::<C::ApplicationInterface>()
+    }
+
+    fn blockstore_server(&self) -> Ref<C::BlockStoreServerInterface> {
+        self.inner.provider.get::<C::BlockStoreServerInterface>()
+    }
 }
 
 async fn get_peers(
     test_name: &str,
     port_offset: u16,
     num_peers: usize,
-) -> (Vec<Peer<TestBinding>>, Application<TestBinding>, PathBuf) {
+) -> (Vec<Peer<TestBinding>>, PathBuf) {
     let mut keystores = Vec::new();
     let mut genesis = Genesis::load().unwrap();
     let path = std::env::temp_dir()
@@ -93,7 +98,7 @@ async fn get_peers(
 
     genesis.node_info = vec![];
     for i in 0..num_peers {
-        let keystore = EphemeralKeystore::default();
+        let keystore = EphemeralKeystore::<TestBinding>::default();
         let (consensus_secret_key, node_secret_key) =
             (keystore.get_bls_sk(), keystore.get_ed25519_sk());
         keystores.push(keystore);
@@ -119,116 +124,58 @@ async fn get_peers(
         ));
     }
 
-    let blockstore_config = BlockstoreConfig {
-        root: path.join("dummy-blockstore").try_into().unwrap(),
-    };
-    let dummy_blockstore = Blockstore::init(blockstore_config).unwrap();
-    let app = Application::<TestBinding>::init(
-        AppConfig {
-            genesis: Some(genesis),
-            mode: Mode::Test,
-            testnet: false,
-            storage: StorageConfig::InMemory,
-            db_path: None,
-            db_options: None,
-        },
-        dummy_blockstore,
-    )
-    .unwrap();
-    app.start().await;
-
     let mut peers = Vec::new();
     for (i, keystore) in keystores.into_iter().enumerate() {
         let node_public_key = keystore.get_ed25519_pk();
-        let query_runner = app.sync_query();
-        let signer = Signer::<TestBinding>::init(
-            Default::default(),
-            keystore.clone(),
-            query_runner.clone(),
-            Socket::raw_bounded(128).0,
-        )
-        .unwrap();
-        let notifier = Notifier::<TestBinding>::init(&app);
-        let topology = Topology::<TestBinding>::init(
-            TopologyConfig::default(),
-            node_public_key,
-            notifier.clone(),
-            query_runner.clone(),
-        )
-        .unwrap();
-        let rep_aggregator = ReputationAggregator::<TestBinding>::init(
-            Default::default(),
-            signer.get_socket(),
-            notifier.clone(),
-            query_runner.clone(),
-        )
-        .unwrap();
-
-        let indexer = Indexer::<TestBinding>::init(
-            Default::default(),
-            query_runner.clone(),
-            keystore.clone(),
-            &signer,
-        )
-        .unwrap();
-
-        let config = PoolConfig {
-            max_idle_timeout: Duration::from_secs(5),
-            address: format!("0.0.0.0:{}", port_offset + i as u16)
-                .parse()
-                .unwrap(),
-            ..Default::default()
-        };
-        let pool = PoolProvider::<TestBinding, muxer::quinn::QuinnMuxer>::init(
-            config,
-            keystore,
-            query_runner,
-            notifier,
-            topology.get_receiver(),
-            rep_aggregator.get_reporter(),
-        )
-        .unwrap();
-
-        let blockstore_config = BlockstoreConfig {
-            root: path.join(format!("node{i}/blockstore")).try_into().unwrap(),
-        };
-        let mut blockstore = Blockstore::init(blockstore_config).unwrap();
-        blockstore.provide_indexer(indexer);
-
-        let bs_config = Config {
-            max_conc_req: 10,
-            max_conc_res: 10,
-        };
-        let blockstore_server = BlockStoreServer::<TestBinding>::init(
-            bs_config,
-            blockstore.clone(),
-            &pool,
-            rep_aggregator.get_reporter(),
+        let node = Node::<TestBinding>::init_with_provider(
+            fdi::Provider::default()
+                .with(
+                    JsonConfigProvider::default()
+                        .with::<Application<TestBinding>>(AppConfig {
+                            genesis: Some(genesis.clone()),
+                            mode: Mode::Test,
+                            testnet: false,
+                            storage: StorageConfig::InMemory,
+                            db_path: None,
+                            db_options: None,
+                        })
+                        .with::<PoolProvider<TestBinding>>(PoolConfig {
+                            max_idle_timeout: Duration::from_secs(5),
+                            address: format!("0.0.0.0:{}", port_offset + i as u16)
+                                .parse()
+                                .unwrap(),
+                            ..Default::default()
+                        })
+                        .with::<Blockstore<TestBinding>>(BlockstoreConfig {
+                            root: path.join(format!("node{i}/blockstore")).try_into().unwrap(),
+                        })
+                        .with::<BlockStoreServer<TestBinding>>(Config {
+                            max_conc_req: 10,
+                            max_conc_res: 10,
+                        }),
+                )
+                .with(keystore.clone()),
         )
         .unwrap();
 
         let peer = Peer::<TestBinding> {
-            pool,
-            blockstore,
-            blockstore_server,
-            topology,
-            _rep_aggregator: rep_aggregator,
+            inner: node,
             node_public_key,
         };
         peers.push(peer);
     }
-    (peers, app, path)
+    (peers, path)
 }
 
 /// Temporary sanity check on the flow
 #[tokio::test]
 async fn test_stream_verified_content() {
-    let (peers, _app, path) = get_peers("stream_verified_content", 49200, 2).await;
+    let (peers, path) = get_peers("stream_verified_content", 49200, 2).await;
 
     let content = create_content();
 
     // Put some content into the sender's blockstore
-    let mut putter = peers[0].blockstore.put(None);
+    let mut putter = peers[0].blockstore().put(None);
     putter
         .write(content.as_slice(), CompressionAlgorithm::Uncompressed)
         .unwrap();
@@ -237,11 +184,11 @@ async fn test_stream_verified_content() {
     let mut network_wire = VecDeque::new();
 
     // The sender sends the content with the proofs to the receiver
-    if let Some(tree) = peers[0].blockstore.get_tree(&root_hash).await {
+    if let Some(tree) = peers[0].blockstore().get_tree(&root_hash).await {
         for block in 0..tree.len() {
             let compr = CompressionAlgoSet::default(); // rustfmt
             let chunk = peers[0]
-                .blockstore
+                .blockstore()
                 .get(block as u32, &tree[block], compr)
                 .await
                 .expect("failed to get block from store");
@@ -260,7 +207,7 @@ async fn test_stream_verified_content() {
     }
 
     // The receiver reads the frames and puts them into its blockstore
-    let mut putter = peers[1].blockstore.put(Some(root_hash));
+    let mut putter = peers[1].blockstore().put(Some(root_hash));
     while let Some(frame) = network_wire.pop_front() {
         match frame {
             Frame::Proof(proof) => putter.feed_proof(&proof).unwrap(),
@@ -276,8 +223,8 @@ async fn test_stream_verified_content() {
     }
 
     // Make sure the content matches
-    let content1 = peers[0].blockstore.read_all_to_vec(&root_hash).await;
-    let content2 = peers[1].blockstore.read_all_to_vec(&root_hash).await;
+    let content1 = peers[0].blockstore().read_all_to_vec(&root_hash).await;
+    let content2 = peers[1].blockstore().read_all_to_vec(&root_hash).await;
     assert_eq!(content1, content2);
 
     // Clean up test
@@ -288,12 +235,10 @@ async fn test_stream_verified_content() {
 
 #[tokio::test]
 async fn test_send_and_receive() {
-    let (peers, app, path) = get_peers("send_and_receive", 49200, 2).await;
-    let query_runner = app.sync_query();
+    let (peers, path) = get_peers("send_and_receive", 49200, 2).await;
+    let query_runner = peers[0].app().sync_query();
     for peer in &peers {
-        peer.topology.start().await;
-        peer.pool.start().await;
-        peer.blockstore_server.start().await;
+        peer.inner.start().await;
     }
     tokio::time::sleep(Duration::from_millis(500)).await;
 
@@ -303,15 +248,14 @@ async fn test_send_and_receive() {
 
     let content = create_content();
     // Put some data into the blockstore of peer 1
-    let mut putter = peers[0].blockstore.put(None);
+    let mut putter = peers[0].blockstore().put(None);
     putter
         .write(&content, CompressionAlgorithm::Uncompressed)
         .unwrap();
     let hash = putter.finalize().await.unwrap();
 
     // Send a request from peer 2 to peer 1
-    let socket = peers[1].blockstore_server.get_socket();
-    println!("send request");
+    let socket = peers[1].blockstore_server().get_socket();
     let mut res = socket
         .run(ServerRequest {
             hash,
@@ -321,15 +265,15 @@ async fn test_send_and_receive() {
         .expect("Failed to send request");
     match res.recv().await.unwrap() {
         Ok(()) => {
-            let recv_content = peers[1].blockstore.read_all_to_vec(&hash).await.unwrap();
+            let recv_content = peers[1].blockstore().read_all_to_vec(&hash).await.unwrap();
             assert_eq!(recv_content, content);
         },
         Err(e) => panic!("Failed to receive content: {e:?}"),
     }
 
-    for peer in &peers {
-        peer.pool.shutdown().await;
-        peer.blockstore_server.shutdown().await;
+    for mut peer in peers {
+        peer.inner.shutdown().await;
+        drop(peer);
     }
 
     // Clean up test
