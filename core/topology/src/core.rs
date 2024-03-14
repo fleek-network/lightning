0f8beeba13f4ad1a725eacd1acaddde85904477b
Matthias Wright <matthias.s.wright@gmail.com>
2024-03-15 03:55:32 +0700

    chore(topology): refactor suggest connections
    Co-authored-by: Parsa Ghadimi <i@parsa.ooo>

--- core/topology/src/core.rs
+++ core/topology/src/core.rs
@@ -1,21 +1,28 @@
 use std::collections::{BTreeSet, HashMap};
+use std::hash::Hash;
 use std::time::Duration;
 
-use fleek_crypto::NodePublicKey;
 use lightning_interfaces::types::Epoch;
 use ndarray::{Array, Array2};
 use rand::SeedableRng;
 
 use crate::divisive::DivisiveHierarchy;
 
+type LatencyMatrix<K> = (Array2<i32>, HashMap<usize, K>, Option<usize>);
+
+pub enum Connections {
+    All(Vec<Vec<usize>>),
+    Hierarchy(Vec<Vec<Vec<usize>>>),
+}
+
 /// Build a latency matrix according to the current application state.
 /// Returns the matrix, a map of node ids to public keys, and an optional node index for
 /// ourselves if we're included in the topology.
-pub(crate) fn build_latency_matrix(
-    our_public_key: NodePublicKey,
-    latencies: HashMap<(NodePublicKey, NodePublicKey), Duration>,
-    valid_pubkeys: BTreeSet<NodePublicKey>,
-) -> (Array2<i32>, HashMap<usize, NodePublicKey>, Option<usize>) {
+pub fn build_latency_matrix<K: Hash + Eq + Copy>(
+    our_key: K,
+    latencies: HashMap<(K, K), Duration>,
+    valid_pubkeys: BTreeSet<K>,
+) -> LatencyMatrix<K> {
     let mut max_latency = Duration::ZERO;
     latencies
         .values()
@@ -23,13 +30,13 @@ pub(crate) fn build_latency_matrix(
     let max_latency: i32 = max_latency.as_millis().try_into().unwrap_or(i32::MAX);
 
     let mut matrix = Array::zeros((valid_pubkeys.len(), valid_pubkeys.len()));
-    let pubkeys: Vec<(usize, NodePublicKey)> = valid_pubkeys.iter().copied().enumerate().collect();
+    let pubkeys: Vec<(usize, K)> = valid_pubkeys.iter().copied().enumerate().collect();
 
     let mut our_index = None;
     let mut index_to_pubkey = HashMap::new();
     for (index_lhs, pubkey_lhs) in pubkeys.iter() {
         index_to_pubkey.insert(*index_lhs, *pubkey_lhs);
-        if *pubkey_lhs == our_public_key {
+        if *pubkey_lhs == our_key {
             our_index = Some(*index_lhs);
         }
         for (index_rhs, pubkey_rhs) in pubkeys[index_lhs + 1..].iter() {
@@ -47,31 +54,45 @@ pub(crate) fn build_latency_matrix(
     (matrix, index_to_pubkey, our_index)
 }
 
-pub fn suggest_connections(
+pub fn suggest_connections_from_latency_matrix<K: Hash + Eq + Copy>(
     epoch: Epoch,
-    our_public_key: NodePublicKey,
-    latencies: HashMap<(NodePublicKey, NodePublicKey), Duration>,
-    valid_pubkeys: BTreeSet<NodePublicKey>,
+    matrix: Array2<i32>,
+    mappings: &HashMap<usize, K>,
     min_nodes: usize,
     target_k: usize,
-) -> Vec<Vec<NodePublicKey>> {
-    let (matrix, mappings, our_index) =
-        build_latency_matrix(our_public_key, latencies, valid_pubkeys);
+) -> Connections {
+    // Included in the topology: collect assignments and build output
+    if mappings.len() < min_nodes {
+        // Fallback to returning all nodes, since we're less than the minimum
+        Connections::All(vec![mappings.clone().into_keys().collect()])
+    } else {
+        let mut rng = rand_chacha::ChaCha8Rng::seed_from_u64(epoch);
+        let hierarchy = DivisiveHierarchy::new(&mut rng, &matrix, target_k);
+        Connections::Hierarchy(hierarchy.connections())
+    }
+}
+
+pub fn suggest_connections<K: Hash + Eq + Copy>(
+    epoch: Epoch,
+    our_key: K,
+    latencies: HashMap<(K, K), Duration>,
+    valid_pubkeys: BTreeSet<K>,
+    min_nodes: usize,
+    target_k: usize,
+) -> Vec<Vec<K>> {
+    let (matrix, mappings, our_index) = build_latency_matrix(our_key, latencies, valid_pubkeys);
 
     if let Some(our_index) = our_index {
-        // Included in the topology: collect assignments and build output
-        if mappings.len() < min_nodes {
-            // Fallback to returning all nodes, since we're less than the minimum
-            vec![mappings.into_values().collect()]
-        } else {
-            let mut rng = rand_chacha::ChaCha8Rng::seed_from_u64(epoch);
-            let hierarchy = DivisiveHierarchy::new(&mut rng, &matrix, target_k);
-
-            hierarchy.connections()[our_index]
-                .iter()
-                .map(|ids| ids.iter().map(|idx| mappings[idx]).collect())
-                .collect()
-        }
+        let connections =
+            suggest_connections_from_latency_matrix(epoch, matrix, &mappings, min_nodes, target_k);
+        let connections = match &connections {
+            Connections::All(connections) => connections,
+            Connections::Hierarchy(connections) => &connections[our_index],
+        };
+        connections
+            .iter()
+            .map(|ids| ids.iter().map(|idx| mappings[idx]).collect())
+            .collect()
     } else {
         // Not in the topology: return all nodes to bootstrap from
         vec![mappings.into_values().collect()]
