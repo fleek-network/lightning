9654d736b2992a1a7d0ad01a5c0ac96d409e14fb
Miguel <miguel@fleek.xyz>
2024-03-13 20:29:09 -0400

    chore: cargo fmt

--- ebpf/ebpf/src/bin/access_control.rs
+++ ebpf/ebpf/src/bin/access_control.rs
@@ -2,14 +2,14 @@
 #![no_main]
 
 use aya_bpf::bindings;
-use aya_bpf::cty::{c_char};
+use aya_bpf::cty::c_char;
 use aya_bpf::macros::lsm;
 use aya_bpf::programs::LsmContext;
 use ebpf::vmlinux;
 
 const PATH_LEN: usize = 64;
 
-unsafe fn get_path(file: *const vmlinux::file,  buf: &mut [u8]) -> Result<usize, i64> {
+unsafe fn get_path(file: *const vmlinux::file, buf: &mut [u8]) -> Result<usize, i64> {
     // Todo: get path from inode.
     todo!()
 }
@@ -24,18 +24,17 @@ fn try_file_open(ctx: LsmContext) -> Result<i32, i32> {
     let path = unsafe {
         let file: *const vmlinux::file = ctx.arg(0);
         let len = get_path(file, &mut buf).map_err(|_| 0)?;
-        if len >= PATH_LEN  {
+        if len >= PATH_LEN {
             // Todo: revisit.
             return Err(0);
         }
         // Todo: revisit.
-        core::str::from_utf8(&buf[..len])
-            .map_err(|_| -1)?
+        core::str::from_utf8(&buf[..len]).map_err(|_| -1)?
     };
 
     // Todo: add other directories.
     if path.starts_with("/proc/") || path.starts_with("/sys/") || path.contains(".lightning") {
-        return Err(-1)
+        return Err(-1);
     }
 
     Ok(0)
