3c5d6b5488925ec08ceaecaa985d6a77164046cb
Parsa Ghadimi <i@parsa.ooo>
2024-03-16 17:24:08 -0400

    convert origin demuxer

--- core/origin-demuxer/src/demuxer.rs
+++ core/origin-demuxer/src/demuxer.rs
@@ -1,81 +1,34 @@
-use std::sync::Arc;
-
-use affair::Task;
+use affair::AsyncWorkerUnordered;
 use lightning_interfaces::infu_collection::Collection;
 use lightning_interfaces::types::{Blake3Hash, ImmutablePointer, OriginProvider};
 use lightning_origin_http::HttpOrigin;
 use lightning_origin_ipfs::IPFSOrigin;
-use tokio::sync::mpsc::Receiver;
-use tokio::sync::Notify;
-use tokio::task::JoinHandle;
 
 use crate::Config;
 
 pub struct Demuxer<C: Collection> {
     http: HttpOrigin<C>,
     ipfs: IPFSOrigin<C>,
-    task_rx: Receiver<Task<ImmutablePointer, anyhow::Result<Blake3Hash>>>,
+}
+
+impl<C: Collection> AsyncWorkerUnordered for Demuxer<C> {
+    type Request = ImmutablePointer;
+    type Response = anyhow::Result<Blake3Hash>;
+
+    async fn handle(&self, req: Self::Request) -> Self::Response {
+        match &req.origin {
+            OriginProvider::HTTP => self.http.fetch(&req.uri).await,
+            OriginProvider::IPFS => self.ipfs.fetch(&req.uri).await,
+            _ => Err(anyhow::anyhow!("unknown origin type")),
+        }
+    }
 }
 
 impl<C: Collection> Demuxer<C> {
-    pub fn new(
-        config: Config,
-        blockstore: C::BlockstoreInterface,
-        task_rx: Receiver<Task<ImmutablePointer, anyhow::Result<Blake3Hash>>>,
-    ) -> anyhow::Result<Self> {
+    pub fn new(config: Config, blockstore: C::BlockstoreInterface) -> anyhow::Result<Self> {
         Ok(Self {
             http: HttpOrigin::<C>::new(config.http, blockstore.clone())?,
             ipfs: IPFSOrigin::<C>::new(config.ipfs, blockstore)?,
-            task_rx,
         })
-    }
-
-    pub fn spawn(mut self) -> (JoinHandle<Self>, Arc<Notify>) {
-        let shutdown = Arc::new(Notify::new());
-        let shutdown_clone = shutdown.clone();
-        let handle = tokio::spawn(async move {
-            self.start(shutdown_clone).await;
-            self
-        });
-        (handle, shutdown)
-    }
-
-    fn handle(&mut self, task: Task<ImmutablePointer, anyhow::Result<Blake3Hash>>) {
-        match &task.request.origin {
-            OriginProvider::HTTP => {
-                let fetcher = self.http.clone();
-                tokio::spawn(async move {
-                    let hash = fetcher.fetch(&task.request.uri).await;
-                    task.respond(hash);
-                });
-            },
-            OriginProvider::IPFS => {
-                let fetcher = self.ipfs.clone();
-                tokio::spawn(async move {
-                    let hash = fetcher.fetch(&task.request.uri).await;
-                    task.respond(hash);
-                });
-            },
-            _ => {
-                task.respond(Err(anyhow::anyhow!("unknown origin type")));
-            },
-        }
-    }
-
-    async fn start(&mut self, shutdown: Arc<Notify>) {
-        loop {
-            tokio::select! {
-                biased;
-                _ = shutdown.notified() => {
-                    break;
-                }
-                task = self.task_rx.recv() => {
-                    let Some(task) = task else {
-                        break;
-                    };
-                    self.handle(task);
-                }
-            }
-        }
     }
 }
