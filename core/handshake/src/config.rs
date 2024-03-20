247ffa9ff8645d2308f040301acc101ab4483a39
Miguel <miguel@fleek.xyz>
2024-03-20 15:45:02 -0400

    ebpf: add socket path to handshake config

--- core/handshake/src/config.rs
+++ core/handshake/src/config.rs
@@ -1,6 +1,7 @@
 use std::net::SocketAddr;
 use std::path::PathBuf;
 
+use resolved_pathbuf::ResolvedPathBuf;
 use serde::{Deserialize, Serialize};
 
 use crate::transports;
@@ -12,6 +13,7 @@ pub struct HandshakeConfig {
     pub transports: Vec<TransportConfig>,
     pub http_address: SocketAddr,
     pub https: Option<HttpsConfig>,
+    pub ebpf_socket_path: Option<ResolvedPathBuf>,
 }
 
 impl Default for HandshakeConfig {
@@ -25,6 +27,12 @@ impl Default for HandshakeConfig {
             ],
             http_address: ([0, 0, 0, 0], 4220).into(),
             https: None,
+            #[cfg(not(target_os = "Linux"))]
+            ebpf_socket_path: None,
+            #[cfg(target_os = "Linux")]
+            ebpf_socket_path: "~/.lightning/ebpf"
+                .try_into()
+                .expect("Failed to resolve path"),
         }
     }
 }
