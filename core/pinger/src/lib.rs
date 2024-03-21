47b8852570aee7d8270a5f536f48eedc7211b239
Parsa Ghadimi <i@parsa.ooo>
2024-03-20 21:51:58 -0400

    pinger: remove the start and shutdown test

--- core/pinger/src/lib.rs
+++ core/pinger/src/lib.rs
@@ -1,7 +1,9 @@
 pub mod config;
 pub mod pinger;
-#[cfg(test)]
-mod tests;
 
 pub use config::Config;
 pub use pinger::Pinger;
+
+// TODO(qti3e): We should test the pinger implementation. The test should actually check that
+// the server works and a client does in fact get the data it needs from a server without any
+// crashes.
