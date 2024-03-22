671826eab750f64301995b4243785177a5392979
Miguel <miguel@fleek.xyz>
2024-03-22 19:25:15 -0400

    ebpf: use socket address

--- etc/ebpf/service/src/message.rs
+++ etc/ebpf/service/src/message.rs
@@ -1,12 +1,11 @@
 use std::io;
-use std::net::Ipv4Addr;
+use std::net::{Ipv4Addr, SocketAddrV4};
 
 use bytes::{Buf, BufMut, Bytes, BytesMut};
 
 pub struct Pf {
     pub op: u8,
-    pub ip: Ipv4Addr,
-    pub port: Option<u16>,
+    pub addr: SocketAddrV4,
 }
 
 impl Pf {
@@ -20,10 +19,8 @@ impl From<Pf> for Bytes {
     fn from(value: Pf) -> Self {
         let mut result = BytesMut::new();
         result.put_u8(value.op);
-        result.put_slice(&value.ip.octets());
-        if let Some(port) = value.port {
-            result.put_u16(port);
-        }
+        result.put_slice(&value.addr.ip().octets());
+        result.put_u16(value.addr.port());
         result.freeze()
     }
 }
@@ -32,15 +29,13 @@ impl TryFrom<Bytes> for Pf {
     type Error = io::Error;
 
     fn try_from(mut value: Bytes) -> Result<Self, Self::Error> {
-        if value.len() != 5 || value.len() != 7 {
+        if value.len() != 7 {
             return Err(io::Error::new(io::ErrorKind::InvalidInput, "invalid"));
         }
         let op = value.get_u8();
         let ip = Ipv4Addr::from(value.get_u32());
-        let mut port = None;
-        if value.len() == 7 {
-            port = Some(value.get_u16());
-        }
-        Ok(Pf { op, ip, port })
+        let port = value.get_u16();
+        let addr = SocketAddrV4::new(ip, port);
+        Ok(Pf { op, addr })
     }
 }
