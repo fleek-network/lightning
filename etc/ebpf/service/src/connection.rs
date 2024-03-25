3dded46e93894029da41580f54be132fe42ecded
Miguel <miguel@fleek.xyz>
2024-03-24 22:07:14 -0400

    ebpf: handle frame

--- etc/ebpf/service/src/connection.rs
+++ etc/ebpf/service/src/connection.rs
@@ -1,15 +1,17 @@
 use std::borrow::BorrowMut;
+use std::convert::Infallible;
 use std::sync::Arc;
 
 use anyhow::bail;
 use aya::maps::{HashMap, MapData};
 use bytes::Bytes;
 use common::IpPortKey;
+use log::error;
 use tokio::io::Interest;
 use tokio::net::UnixStream;
 use tokio::sync::Mutex;
 
-use crate::schema::Pf;
+use crate::schema::{EbpfServiceFrame, Pf};
 
 pub struct Connection<T> {
     socket: UnixStream,
@@ -21,7 +23,8 @@ impl<T: BorrowMut<MapData>> Connection<T> {
         Self { socket, block_list }
     }
 
-    async fn handle_request(&mut self, message: Pf) -> anyhow::Result<()> {
+    #[inline]
+    async fn pf_handle(&mut self, message: Pf) -> anyhow::Result<()> {
         match message.op {
             Pf::ADD => {
                 let mut map = self.block_list.lock().await;
@@ -48,6 +51,14 @@ impl<T: BorrowMut<MapData>> Connection<T> {
         Ok(())
     }
 
+    #[inline]
+    async fn handle_request(&mut self, frame: EbpfServiceFrame) -> anyhow::Result<()> {
+        match frame {
+            EbpfServiceFrame::Mac => unimplemented!(),
+            EbpfServiceFrame::Pf(pf) => self.pf_handle(pf).await,
+        }
+    }
+
     pub async fn handle(mut self) -> anyhow::Result<()> {
         loop {
             let ready = self.socket.ready(Interest::READABLE).await?;
@@ -55,6 +66,7 @@ impl<T: BorrowMut<MapData>> Connection<T> {
                 let mut read_buf = vec![0u8; 7];
                 let mut bytes_read = 0;
                 'read: loop {
+                    // Todo: read len delimited frame.
                     while bytes_read < 7 {
                         match self.socket.try_read(&mut read_buf) {
                             Ok(0) => {
@@ -73,8 +85,18 @@ impl<T: BorrowMut<MapData>> Connection<T> {
                         }
                     }
 
-                    let message = Pf::try_from(Bytes::from(read_buf.clone()))?;
-                    self.handle_request(message).await?;
+                    let bytes: Bytes = read_buf.into();
+                    match EbpfServiceFrame::try_from(bytes) {
+                        Ok(f) => {
+                            if let Err(e) = self.handle_request(f).await {
+                                error!("failed to handle request: {e:?}");
+                            }
+                        },
+                        Err(e) => {
+                            error!("failed to deserialize frame: {e:?}");
+                        },
+                    }
+
                     break 'read;
                 }
             }
