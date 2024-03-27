eccb329b66b209c645f5fca405f9543e5dd13815
Miguel <miguel@fleek.xyz>
2024-03-27 14:25:52 -0400

    ebpf: add lightning-ebpf app

--- etc/ebpf/cli/src/bin/lightning-ebpf.rs
+++ etc/ebpf/cli/src/bin/lightning-ebpf.rs
@@ -1,1 +1,63 @@
-fn main() {}
+use std::net::SocketAddrV4;
+
+use anyhow::Context;
+use aya::maps::HashMap;
+use aya::programs::{Xdp, XdpFlags};
+use aya::{include_bytes_aligned, Bpf};
+use aya_log::BpfLogger;
+use clap::Parser;
+use common::IpPortKey;
+use ebpf_service::server::Server;
+use tokio::net::UnixListener;
+use tokio::signal;
+
+#[derive(Debug, Parser)]
+struct Opts {
+    /// Interface to attach packet filter program.
+    #[clap(short, long, default_value = "eth0")]
+    iface: String,
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let opt = Opts::parse();
+
+    env_logger::init();
+
+    #[cfg(debug_assertions)]
+    let mut handle = Bpf::load(include_bytes_aligned!(
+        "../../../ebpf/target/bpfel-unknown-none/debug/packet_filter"
+    ))?;
+    #[cfg(not(debug_assertions))]
+    let mut handle = Bpf::load(include_bytes_aligned!(
+        "../../../ebpf/target/bpfel-unknown-none/release/packet_filter"
+    ))?;
+
+    if let Err(e) = BpfLogger::init(&mut handle) {
+        log::warn!("failed to initialize logger: {}", e);
+    }
+
+    let program: &mut Xdp = handle
+        .program_mut("xdp_packet_filter")
+        .unwrap()
+        .try_into()?;
+    program.load()?;
+    program
+        .attach(&opt.iface, XdpFlags::default())
+        .context("failed to attach the XDP program")?;
+
+    let blocklist: HashMap<_, IpPortKey, u32> =
+        HashMap::try_from(handle.take_map("BLOCK_LIST").unwrap())?;
+
+    let listener = UnixListener::bind(".lightning/ebpf")?;
+    let server = Server::new(listener, blocklist);
+
+    log::info!("Enter Ctrl-C to shutdown");
+
+    tokio::select! {
+        _ = server.start() => {}
+        _ = signal::ctrl_c() => {}
+    }
+
+    Ok(())
+}
