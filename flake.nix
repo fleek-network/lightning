b5bb8b57c0da359d8e52b99cb87601376bf84468
ozwaldorf <self@ossian.dev>
2024-03-29 22:31:26 -0400

    flake: add services derivation and unified package, clean up env variables

--- flake.nix
+++ flake.nix
@@ -20,7 +20,7 @@
   };
 
   outputs = { self, nixpkgs, crane, fenix, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
+    flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-darwin" ] (system:
       let
         pkgs = (import nixpkgs { inherit system; });
         inherit (pkgs) lib;
@@ -30,12 +30,21 @@
             sha256 = "4HfRQx49hyuJ8IjBWSty3OXCLOmeaZF5qZAXW6QiQNI=";
           });
 
+        llvmCraneLib = craneLib.overrideToolchain
+          (fenix.packages.${system}.complete.withComponents [
+            "cargo"
+            "llvm-tools"
+            "rustc"
+          ]);
+
         # Allow markdown and bin files for some `include!()` uses
         markdownFilter = path: _type: builtins.match ".*md$" path != null;
         binFilter = path: _type: builtins.match ".*bin$" path != null;
+        jsFilter = path: _type: builtins.match ".*js$" path != null;
         filter = path: type:
           (markdownFilter path type) || (binFilter path type)
-          || (craneLib.filterCargoSources path type);
+          || (jsFilter path type) || (craneLib.filterCargoSources path type);
+
         src = lib.cleanSourceWith {
           inherit filter;
           src = craneLib.path ./.;
@@ -60,7 +69,8 @@
         commonArgs = {
           inherit src;
           strictDeps = true;
-          pname = "lightning-node";
+          pname = "lightning";
+          version = "0.1.0";
           nativeBuildInputs = with pkgs;
             [
               pkg-config
@@ -68,8 +78,7 @@
               perl
               cmake
               clang
-              libclang.lib
-              stdenv
+              libclang
               fontconfig
               freetype
               protobuf
@@ -77,39 +86,39 @@
               openssl_3
               (rocksdb.override { enableShared = true; })
               (snappy.override { static = true; })
-              zstd.dev
-              zlib.dev
-              bzip2.dev
-              lz4.dev
+              zstd
+              zlib
+              bzip2
+              lz4
               onnxruntime
               mold-wrapped
 
               (pkgs.writeShellScriptBin "git" ''
                 # hack to fix `git rev-parse HEAD` when building in sandbox
-                if [[ $NIX_ENFORCE_PURITY -eq 1 ]]; then
-                  echo ${gitRev}
-                else 
-                  ${git}/bin/git $@
-                fi
+                [[ $NIX_ENFORCE_PURITY -eq 1 ]] && echo ${gitRev} && exit
+                "${git}/bin/git" "$@"
               '')
             ] ++ lib.optionals pkgs.stdenv.isDarwin [
               # MacOS specific packages
               pkgs.libiconv
             ];
+        } // commonVars;
 
-          PKG_CONFIG_PATH = "${pkgs.fontconfig.dev}/lib/pkgconfig";
-          LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
-          OPENSSL_NO_VENDOR = 1;
-          OPENSSL_LIB_DIR = "${pkgs.openssl_3.out}/lib";
-          OPENSSL_INCLUDE_DIR = "${pkgs.openssl_3.dev}/include";
+        commonVars = {
+          # Shared and static libraries
+          PKG_CONFIG_PATH = "${lib.getDev pkgs.fontconfig}/lib/pkgconfig";
           RUST_FONTCONFIG_DLOPEN = "on";
+          LIBCLANG_PATH = "${lib.getLib pkgs.libclang}/lib";
+          OPENSSL_NO_VENDOR = 1;
+          OPENSSL_LIB_DIR = "${lib.getLib pkgs.openssl_3}/lib";
+          OPENSSL_INCLUDE_DIR = "${lib.getDev pkgs.openssl_3.dev}/include";
           RUSTY_V8_ARCHIVE = "${librusty_v8}";
           ROCKSDB_LIB_DIR = "${pkgs.rocksdb}/lib";
-          Z_LIB_DIR = "${pkgs.zlib.dev}";
-          ZSTD_LIB_DIR = "${pkgs.zstd.dev}";
-          BZIP2_LIB_DIR = "${pkgs.bzip2.dev}";
-          SNAPPY_LIB_DIR = "${pkgs.snappy.out}/lib";
-          ORT_LIB_LOCATION = "${pkgs.onnxruntime}";
+          Z_LIB_DIR = "${lib.getLib pkgs.zlib}/lib";
+          ZSTD_LIB_DIR = "${lib.getLib pkgs.zstd}/lib";
+          BZIP2_LIB_DIR = "${lib.getLib pkgs.bzip2}/lib";
+          SNAPPY_LIB_DIR = "${lib.getLib pkgs.snappy}/lib";
+          ORT_LIB_LOCATION = "${lib.getLib pkgs.onnxruntime}/lib";
 
           # Enable mold linker
           CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER =
@@ -117,82 +126,79 @@
           CARGO_TARGET_AARCH64_APPLE_DARWIN_LINKER = "${pkgs.clang}/bin/clang";
           RUSTFLAGS =
             "--cfg tokio_unstable -Clink-arg=-fuse-ld=${pkgs.mold-wrapped}/bin/mold";
-
-          # for some reason this stuff isn't propagated to the dev shell
-          shellHook = ''
-            export LIBCLANG_PATH="${pkgs.libclang.lib}/lib"
-            export OPENSSL_NO_VENDOR=1
-            export OPENSSL_LIB_DIR="${pkgs.openssl_3.out}/lib"
-            export OPENSSL_INCLUDE_DIR="${pkgs.openssl_3.dev}/include"
-            export RUST_FONTCONFIG_DLOPEN="on"
-            export RUSTY_V8_ARCHIVE="${librusty_v8}"
-            export ROCKSDB_LIB_DIR="${pkgs.rocksdb}/lib"
-            export Z_LIB_DIR="${pkgs.zlib.dev}"
-            export ZSTD_LIB_DIR="${pkgs.zstd.dev}"
-            export BZIP2_LIB_DIR="${pkgs.bzip2.dev}"
-            export SNAPPY_LIB_DIR="${pkgs.snappy.out}/lib"
-            export ORT_LIB_LOCATION="${pkgs.onnxruntime}"
-
-            export CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER="${pkgs.clang}/bin/clang"
-            export CARGO_TARGET_AARCH64_APPLE_DARWIN_LINKER="${pkgs.clang}/bin/clang"
-            export RUSTFLAGS="--cfg tokio_unstable -Clink-arg=-fuse-ld=${pkgs.mold-wrapped}/bin/mold"
-          '';
         };
-
-        craneLibLLvmTools = craneLib.overrideToolchain
-          (fenix.packages.${system}.complete.withComponents [
-            "cargo"
-            "llvm-tools"
-            "rustc"
-          ]);
 
         # Build *just* the cargo dependencies, so we can reuse all of that
         # work (e.g. via cachix or github artifacts) when running in CI
         cargoArtifacts = craneLib.buildDepsOnly (commonArgs);
-
-        # Core node binary
-        lightning-node = craneLib.buildPackage (commonArgs // {
-          inherit cargoArtifacts;
-          doCheck = false;
-          cargoExtraArgs = "--locked --bin lightning-node";
-        });
-
       in {
+        # Allow using `nix flake check` to run tests and lints
         checks = {
-          lightning-clippy = craneLib.cargoClippy (commonArgs // {
+          # Check formatting
+          fmt = craneLib.cargoFmt { inherit (commonArgs) pname src; };
+
+          # Check clippy lints
+          clippy = craneLib.cargoClippy (commonArgs // {
             inherit cargoArtifacts;
             cargoClippyExtraArgs =
               "--all-targets --all-features -- -Dclippy::all -Dwarnings";
           });
 
-          lightning-doc =
-            craneLib.cargoDoc (commonArgs // { inherit cargoArtifacts; });
-
-          # Check formatting
-          lightning-fmt = craneLib.cargoFmt { inherit src; };
+          # Check doc tests
+          doc = craneLib.cargoDoc (commonArgs // { inherit cargoArtifacts; });
 
           # Run tests with cargo-nextest
-          lightning-nextest = craneLib.cargoNextest (commonArgs // {
+          nextest = craneLib.cargoNextest (commonArgs // {
             inherit cargoArtifacts;
             partitions = 1;
             partitionType = "count";
           });
         };
 
-        packages = {
-          default = lightning-node;
+        # Expose the node and services as packages
+        packages = rec {
+          default = lightning-node-full;
+
+          # Core node binary
+          lightning-node = craneLib.buildPackage (commonArgs // {
+            inherit cargoArtifacts;
+            pname = "lightning-node";
+            doCheck = false;
+            cargoExtraArgs =
+              lib.concatStringsSep " " [ "--locked" "--bin lightning-node" ];
+          });
+
+          # Service binaries
+          lightning-services = craneLib.buildPackage (commonArgs // {
+            inherit cargoArtifacts;
+            pname = "lightning-services";
+            doCheck = false;
+            cargoExtraArgs = lib.concatStringsSep " " [
+              "--locked"
+              "--bin fn-service-0"
+              "--bin fn-service-1"
+              "--bin fn-service-2"
+            ];
+          });
+
+          # Unified package with the node and all services
+          lightning-node-full = pkgs.symlinkJoin {
+            name = "lightning-node-full";
+            paths = [ lightning-node lightning-services ];
+          };
         } // lib.optionalAttrs (!pkgs.stdenv.isDarwin) {
-          lightning-llvm-coverage = craneLibLLvmTools.cargoLlvmCov
+          lightning-llvm-coverage = llvmCraneLib.cargoLlvmCov
             (commonArgs // { inherit cargoArtifacts; });
         };
 
-        apps.default = flake-utils.lib.mkApp { drv = lightning-node; };
+        # Allow using `nix run` on the project
+        apps.default = flake-utils.lib.mkApp { drv = self.packages.default; };
 
-        # Allow using `nix develop` to get a development environment
-        devShells.default = craneLib.devShell {
+        # Allow using `nix develop on the project
+        devShells.default = craneLib.devShell (commonVars // {
           # Inherit inputs from checks.
           checks = self.checks.${system};
           packages = [ pkgs.rust-analyzer ];
-        };
+        });
       });
 }
