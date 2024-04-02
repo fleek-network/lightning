d617ee219d4ee3e5b58449b9c7f79c3f83562376
Miguel <miguel@fleek.xyz>
2024-04-02 19:05:49 -0400

    ebpf: update shared state

--- etc/ebpf/ebpf/src/maps.rs
+++ etc/ebpf/ebpf/src/maps.rs
@@ -4,9 +4,9 @@ use common::{File, IpPortKey};
 
 // Todo: replace with HashMapOfMaps or HashMapOfArrays when aya adds support.
 #[map]
-pub static BINARY_FILES: HashMap<File, u64> = HashMap::<File, u64>::with_max_entries(1024, 0);
+pub static BIN_TO_FILE: HashMap<File, u64> = HashMap::<File, u64>::with_max_entries(1024, 0);
 #[map]
-pub static PROCESSES: HashMap<u64, u64> = HashMap::<u64, u64>::with_max_entries(1024, 0);
+pub static PROCESSES_TO_FILE: HashMap<u64, u64> = HashMap::<u64, u64>::with_max_entries(1024, 0);
 #[map]
 pub static BLOCK_LIST: HashMap<IpPortKey, u32> =
     HashMap::<IpPortKey, u32>::with_max_entries(1024, 0);
--- etc/ebpf/service/src/state.rs
+++ etc/ebpf/service/src/state.rs
@@ -8,11 +8,18 @@ use tokio::sync::Mutex;
 #[derive(Clone)]
 pub struct SharedState {
     block_list: Arc<Mutex<HashMap<MapData, IpPortKey, u32>>>,
+    proc_to_file: Arc<Mutex<HashMap<MapData, u64, u64>>>,
 }
 
 impl SharedState {
-    pub fn new(block_list: HashMap<MapData, IpPortKey, u32>) -> Self {
-        Self { block_list: Arc::new(Mutex::new(block_list)) }
+    pub fn new(
+        block_list: HashMap<MapData, IpPortKey, u32>,
+        proc_to_file: HashMap<MapData, u64, u64>,
+    ) -> Self {
+        Self {
+            block_list: Arc::new(Mutex::new(block_list)),
+            proc_to_file: Arc::new(Mutex::new(proc_to_file)),
+        }
     }
 
     pub async fn blocklist_add(&mut self, addr: SocketAddrV4) -> anyhow::Result<()> {
@@ -34,6 +41,18 @@ impl SharedState {
             ip: (*addr.ip()).into(),
             port: addr.port() as u32,
         })?;
+        Ok(())
+    }
+
+    pub async fn file_allow_open(&mut self, pid: u64) -> anyhow::Result<()> {
+        let mut map = self.proc_to_file.lock().await;
+        map.insert(pid, 0, 0)?;
+        Ok(())
+    }
+
+    pub async fn file_block_open(&mut self, pid: u64) -> anyhow::Result<()> {
+        let mut map = self.proc_to_file.lock().await;
+        map.remove(&pid)?;
         Ok(())
     }
 }
