6fd70a4db1ccc3dfc5a1e79c714bf88b961f62e9
Parsa Ghadimi <i@parsa.ooo>
2024-04-02 14:37:15 -0400

    remove left over commented code

--- core/interfaces/src/handshake.rs
+++ core/interfaces/src/handshake.rs
@@ -3,23 +3,4 @@ use fdi::BuildGraph;
 use crate::infu_collection::Collection;
 
 #[infusion::service]
-pub trait HandshakeInterface<C: Collection>: BuildGraph + Sized + Send + Sync {
-    // fn _init(
-    //     config: ::ConfigProviderInterface,
-    //     service_executor: ::ServiceExecutorInterface,
-    //     keystore: ::KeystoreInterface,
-    // ) {
-    //     Self::init(
-    //         config.get::<Self>(),
-    //         keystore.clone(),
-    //         service_executor.get_provider(),
-    //     )
-    // }
-
-    // /// Initialize a new handshake server.
-    // fn init(
-    //     config: Self::Config,
-    //     keystore: C::KeystoreInterface,
-    //     provider: c![C::ServiceExecutorInterface::Provider],
-    // ) -> anyhow::Result<Self>;
-}
+pub trait HandshakeInterface<C: Collection>: BuildGraph + Sized + Send + Sync {}
