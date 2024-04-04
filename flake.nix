c0ffb77b3e5b5f065137d0828577edc75933f78d
ozwaldorf <self@ossian.dev>
2024-04-04 14:18:38 -0400

    flake: suggest using garnix cache on run

--- flake.nix
+++ flake.nix
@@ -18,6 +18,11 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
+  nixConfig = {
+    extra-substituters = [ "https://cache.garnix.io" ];
+    extra-trusted-public-keys = [ "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g=" ];
+  };
+
   outputs =
     {
       self,
