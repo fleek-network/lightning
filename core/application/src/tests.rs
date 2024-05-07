3e98effe7f264bd52cda7e7b84ce127b824f1500
Matthias Wright <matthias.s.wright@gmail.com>
2024-05-07 19:57:28 +0800

    TESTNET: default port changes

--- core/application/src/tests.rs
+++ core/application/src/tests.rs
@@ -68,7 +68,7 @@ partial!(TestBinding {
     ApplicationInterface = Application<Self>;
 });
 
-const CHAIN_ID: ChainId = 1337;
+const CHAIN_ID: ChainId = 59330;
 
 pub struct Params {
     epoch_time: Option<u64>,
