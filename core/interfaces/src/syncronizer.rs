0cba56d36b44f87392900d7c7cde56b237bcd37d
daltoncoder <daltoncoder2@gmail.com>
2024-05-10 12:29:12 -0400

    cargo fmt

--- core/interfaces/src/syncronizer.rs
+++ core/interfaces/src/syncronizer.rs
@@ -7,7 +7,8 @@ use crate::collection::Collection;
 pub trait SyncronizerInterface<C: Collection>: BuildGraph + Sized + Send + Sync {
     /// Returns the blake3hash of the next checkpoint to load, after
     /// it has already downloaded by the blockstore server.
-    /// If it returns None it means the Node is not ever going to checkpoint and should be shutting down
+    /// If it returns None it means the Node is not ever going to checkpoint and should be shutting
+    /// down
     #[pending]
     async fn next_checkpoint_hash(&self) -> Option<Blake3Hash>;
 }
