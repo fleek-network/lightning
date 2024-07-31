56c3645658f747e36764273dbd6eaa5d81fb1302
ozwaldorf <self@ossian.dev>
2024-07-31 13:07:04 -0400

    chore(jsrt task example): catch errors in consensus responses

--- services/js-poc/examples/example_task.js
+++ services/js-poc/examples/example_task.js
@@ -1,10 +1,23 @@
 export const main = async () => {
-  // Run the task locally
-  const { responses } = await Fleek.runTask(Fleek.ServiceId.Js, {
+  const request = {
     origin: "Ipfs",
     uri: "bafkreievqw7zbdxko2fm5vsk7mzmnqaxkuxfnbukjndul5apoumssbimqa",
-  });
+  };
 
-  // Return the response directly
-  return new Uint8Array(responses[0])
+  // Run the task locally
+  const { responses } = await Fleek.runTask(Fleek.ServiceId.Js, request);
+
+  // Run the task on a single other node
+  // const { responses } = await Fleek.runTask(Fleek.ServiceId.Js, request, "single");
+
+  // Run the task on the current node's cluster
+  // const { responses } = await Fleek.runTask(Fleek.ServiceId.Js, request, "cluster");
+
+  if (responses.length == 0) {
+    return "error: failed to run task";
+  } else {
+    console.log(responses);
+    // Return the first response's payload directly
+    return new Uint8Array(responses[0])
+  }
 }
