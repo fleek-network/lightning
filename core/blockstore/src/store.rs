015bb6b6269b2c1bd4e41cafe6e831c1c2f92dae
ozwaldorf <self@ossian.dev>
2024-08-05 14:21:42 -0400

    fix(blockstore): remove trait-variant (warns the non send trait is unused)

--- core/blockstore/src/store.rs
+++ core/blockstore/src/store.rs
@@ -1,18 +1,23 @@
+use std::future::Future;
 use std::io;
 
 use lightning_interfaces::types::Blake3Hash;
 
 /// Simple block store interface.
-#[trait_variant::make(Store: Send)]
-pub trait _Store: Send + Clone {
-    async fn fetch(&self, location: &str, key: &Blake3Hash, tag: Option<usize>) -> Option<Block>;
-    async fn insert(
+pub trait Store: Send + Clone {
+    fn fetch(
+        &self,
+        location: &str,
+        key: &Blake3Hash,
+        tag: Option<usize>,
+    ) -> impl Future<Output = Option<Block>> + Send;
+    fn insert(
         &mut self,
         location: &str,
         key: Blake3Hash,
         block: &[u8],
         tag: Option<usize>,
-    ) -> io::Result<()>;
+    ) -> impl Future<Output = io::Result<()>> + Send;
 }
 
 pub type Block = Vec<u8>;
