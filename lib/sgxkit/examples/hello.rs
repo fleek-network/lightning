52a5733ad9a35eb7010bcb3293f1f957a8ece258
ozwaldorf <self@ossian.dev>
2024-08-09 00:50:51 -0400

    feat: impl initial sgxkit with hello and echo examples

---
+++ lib/sgxkit/examples/hello.rs
@@ -0,0 +1,10 @@
+use std::io::Write;
+
+use sgxkit::io::OutputWriter;
+
+fn main() {
+    let mut writer = OutputWriter::new();
+    writer
+        .write_all(b"Hello, world! From, SGX WASM <3")
+        .unwrap();
+}
