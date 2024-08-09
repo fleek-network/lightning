3280f8066fa9768bd18ebaa752db7fe6845cd915
ozwaldorf <self@ossian.dev>
2024-08-08 23:37:47 -0400

    feat(sgx): sketch out basic enclave request loop

--- services/sgx/enclave/src/main.rs
+++ services/sgx/enclave/src/main.rs
@@ -1,13 +1,59 @@
+use std::io::Read;
+use std::net::{TcpListener, TcpStream};
+
+use serde::Deserialize;
+
 mod blockstore;
 
-extern crate bytes;
+#[derive(Deserialize)]
+struct Request {
+    /// Blake3 hash of the wasm module.
+    hash: String,
+    /// Entrypoint function to call. Defaults to `main`.
+    #[serde(default = "default_function_name")]
+    function: String,
+    /// Input data string.
+    input: String,
+}
 
-fn main() -> std::io::Result<()> {
-    // - startup local load balancer with a wasm runtime on each thread
-    // - bind to userspace address for incoming requests from handshake:
-    //   - read and verify blockstore content
-    //   - submit content and request params to load balancer
-    //   - wait for response, sign, and send it
+fn default_function_name() -> String {
+    "_start".into()
+}
 
-    unimplemented!()
+fn handle_connection(conn: &mut TcpStream) -> anyhow::Result<()> {
+    // read length delimiter
+    let mut buf = [0; 4];
+    conn.read_exact(&mut buf)?;
+    let len = u32::from_be_bytes(buf);
+
+    // read payload
+    let mut payload = vec![0; len as usize];
+    conn.read_exact(&mut payload)?;
+
+    // parse payload
+    let Request {
+        hash,
+        function,
+        input,
+    } = serde_json::from_slice(&payload)?;
+
+    // fetch content from blockstore
+    let content = blockstore::get_verified_content(&hash)?;
+
+    // TODO: sign and return data in some format
+
+    Ok(())
+}
+
+fn main() -> anyhow::Result<()> {
+    // bind to userspace address for incoming requests from handshake
+    let listener = TcpListener::bind("requests.fleek.network")?;
+
+    loop {
+        let (mut conn, _) = listener.accept()?;
+        if let Err(e) = handle_connection(&mut conn) {
+            // TODO: write error to stream
+            eprintln!("Failed to handle connection: {e}");
+        }
+    }
 }
