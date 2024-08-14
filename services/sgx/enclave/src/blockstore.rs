212e2387a630fcad7125a1c47e879ab40a5fa662
ozwaldorf <self@ossian.dev>
2024-08-13 21:04:56 -0400

    feat(nix+sgx): add sgx service output, allow overriding enclave bin in build.rs

--- services/sgx/enclave/src/blockstore.rs
+++ services/sgx/enclave/src/blockstore.rs
@@ -6,6 +6,8 @@ use arrayref::array_ref;
 use blake3_tree::blake3::tree::BlockHasher;
 use blake3_tree::IncrementalVerifier;
 
+const LEADING_BIT: u32 = 1 << 31;
+
 /// Get some verified content from the userland.
 pub fn get_verified_content(hash: &str) -> anyhow::Result<Vec<u8>> {
     let raw = hex::decode(hash)?;
@@ -22,10 +24,9 @@ pub fn get_verified_content(hash: &str) -> anyhow::Result<Vec<u8>> {
     // TODO: use userspace memory allocations to avoid having public data
     //       held in the precious and limited protected memory space.
     let mut content = Vec::new();
+    let mut block = 0;
 
     while !iv.is_done() {
-        const LEADING_BIT: u32 = 1 << 31;
-
         // read leading chunk bit and length delimiter
         let mut buf = [0; 4];
         stream.read_exact(&mut buf)?;
@@ -44,11 +45,12 @@ pub fn get_verified_content(hash: &str) -> anyhow::Result<Vec<u8>> {
         } else {
             // hash block and verify it against the tree
             let mut hasher = BlockHasher::new();
-            hasher.set_block(iv.get_current_block_counter());
+            hasher.set_block(block);
             hasher.update(&payload);
             iv.verify(hasher)?;
 
             content.append(&mut payload);
+            block += 1;
         }
     }
 
