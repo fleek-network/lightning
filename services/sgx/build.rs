9021cb15a011b7ac3ecddf5b4a73d619db9f5bb1
ozwaldorf <self@ossian.dev>
2024-08-13 23:36:08 -0400

    fix(sgx): minor amends

--- services/sgx/build.rs
+++ services/sgx/build.rs
@@ -12,26 +12,30 @@ fn main() {
     println!("cargo::rerun-if-changed=enclave/*");
     println!("cargo::rerun-if-env-changed=FN_ENCLAVE_BIN_PATH");
 
-    let path = std::env::var("FN_ENCLAVE_BIN_PATH").unwrap_or_else(|_| {
-        // local build for the enclave bin
-        assert!(
-            std::process::Command::new("cargo")
-                .args(["build", "--release", "--locked"])
-                .current_dir("./enclave")
-                .env_clear()
-                .env("PATH", std::env::var("PATH").unwrap())
-                .status()
-                .unwrap()
-                .success(),
-            "failed to build enclave module"
-        );
+    let path = std::env::var("FN_ENCLAVE_BIN_PATH")
+        .ok()
+        .and_then(|v| (!v.is_empty()).then_some(v))
+        .unwrap_or_else(|| {
+            // local build for the enclave bin
+            assert!(
+                std::process::Command::new("cargo")
+                    .args(["build", "--release", "--locked"])
+                    .current_dir("./enclave")
+                    .env_clear()
+                    .env("PATH", std::env::var("PATH").unwrap())
+                    .status()
+                    .unwrap()
+                    .success(),
+                "failed to build enclave module"
+            );
 
-        // cargo output path
-        "../../target/x86_64-fortanix-unknown-sgx/release/fleek-service-sgx-enclave".into()
-    });
+            // cargo output path
+            "enclave/target/x86_64-fortanix-unknown-sgx/release/fleek-service-sgx-enclave".into()
+        });
 
     let new_path = Path::new(&std::env::var("OUT_DIR").unwrap()).join("enclave");
-    std::fs::copy(&path, &new_path).expect("failed to move sgxs to output directory");
+    std::fs::copy(&path, &new_path)
+        .unwrap_or_else(|_| panic!("failed to copy {path} to output directory"));
 
     // convert `enclave` to `enclave.sgxs`
     assert!(
