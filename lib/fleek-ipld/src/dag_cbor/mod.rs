6710c7a20ae6209e8431b6e5a3192124a0d04a3d
Juan Pablo Royo Sales <juanpablo.royo@gmail.com>
2024-08-26 19:46:15 +0200

    feat: moving forward

--- lib/fleek-ipld/src/dag_cbor/mod.rs
+++ lib/fleek-ipld/src/dag_cbor/mod.rs
@@ -3,23 +3,25 @@ use core::fmt;
 
 use libipld::cbor::DagCborCodec;
 use libipld::prelude::*;
-use libipld::{Cid, DagCbor};
+use libipld::{Cid, DagCbor, Ipld};
 
-#[derive(DagCbor, Clone, PartialEq, Eq)]
+#[derive(DagCbor, Clone, PartialEq)]
 pub struct File {
-    pub cid: Cid,
-    pub data: Vec<u8>,
+    //pub cid: Cid,
+    pub cats: String,
+    pub cheese: Vec<Ipld>,
+    pub something: Ipld,
 }
 
 impl fmt::Display for File {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "File cid: {}", self.cid)
+        write!(f, "data: {:?}", self.cheese)
     }
 }
 
 pub async fn download() -> anyhow::Result<()> {
     // Replace with the CID of the IPLD file
-    let cid_str = "bafy..."; // Example CID
+    let cid_str = "bafyreicnokmhmrnlp2wjhyk2haep4tqxiptwfrp2rrs7rzq7uk766chqvq";
     let cid = Cid::try_from(cid_str).expect("Invalid CID");
 
     // Construct the URL to fetch the IPLD data
