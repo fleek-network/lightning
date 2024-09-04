67219cd6f43d9eaf649d5c5d8d3ae40d2144faef
Steven Normore <snormore@gmail.com>
2024-09-04 18:33:20 -0400

    chore(dev): fix nextest install in dev script

--- dev/common/install-nextest.sh
+++ dev/common/install-nextest.sh
@@ -3,7 +3,7 @@ set -eou pipefail
 script_dir="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 # Install cargo-nextest using cargo binstall if necessary
-if ! cargo hakari --version > /dev/null 2>&1; then
+if ! cargo nextest --version > /dev/null 2>&1; then
     # Install cargo-binstall if necessary
     "$script_dir"/install-binstall.sh
 
