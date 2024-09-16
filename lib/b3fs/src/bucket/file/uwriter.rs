7013bc47696393fa6db27da5475544d84acc02b3
Parsa Ghadimi <i@parsa.ooo>
2024-09-16 08:49:13 -0400

    feat(b3fs): add more bucket writer api

--- lib/b3fs/src/bucket/file/uwriter.rs
+++ lib/b3fs/src/bucket/file/uwriter.rs
@@ -1,13 +1,29 @@
-use crate::bucket::Bucket;
+use std::io;
+
+use crate::bucket::{errors, Bucket};
 
 pub struct UntrustedFileWriter {}
 
 impl UntrustedFileWriter {
-    pub fn new(bucket: &Bucket) -> Self {
+    pub fn new(bucket: &Bucket, hash: [u8; 32]) -> Self {
         todo!()
     }
 
-    pub async fn feed_proof(&mut self, proof: &[u8]) {}
+    pub async fn feed_proof(&mut self, proof: &[u8]) -> Result<(), errors::FeedProofError> {
+        todo!()
+    }
 
-    pub async fn write(&mut self, bytes: &[u8]) {}
+    pub async fn write(&mut self, bytes: &[u8]) -> Result<(), errors::WriteError> {
+        todo!()
+    }
+
+    /// Finalize this write and flush the data to the disk.
+    pub async fn commit(self) -> Result<[u8; 32], errors::CommitError> {
+        todo!()
+    }
+
+    /// Cancel this write and remove anything that this writer wrote to the disk.
+    pub async fn rollback(self) -> Result<(), io::Error> {
+        todo!()
+    }
 }
