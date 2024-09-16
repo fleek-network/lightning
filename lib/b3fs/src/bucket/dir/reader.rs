74caa6bfcabec95ef253242e7cbf072435cb219e
Parsa Ghadimi <i@parsa.ooo>
2024-09-16 08:05:16 -0400

    wip: ContentHeader reader api

--- lib/b3fs/src/bucket/dir/reader.rs
+++ lib/b3fs/src/bucket/dir/reader.rs
@@ -1,10 +1,26 @@
+use tokio::fs::{self};
+use triomphe::Arc;
+
 use super::iter::DirEntriesIter;
 use crate::collections::HashTree;
 use crate::entry::BorrowedEntry;
 
-pub struct Dir {}
+pub struct B3Dir {
+    /// The number of entries in this directory.
+    num_entries: u16,
+    /// The actual header file.
+    file: Arc<fs::File>,
+}
 
-impl Dir {
+impl B3Dir {
+    pub(crate) fn new(num_entries: u32, file: Arc<fs::File>) -> Self {
+        debug_assert!(num_entries <= (u16::MAX as u32));
+        Self {
+            num_entries: num_entries as u16,
+            file,
+        }
+    }
+
     pub fn hashtree(&self) -> HashTree {
         todo!()
     }
