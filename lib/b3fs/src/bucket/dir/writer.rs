b0caa770d47da86479b97ebdb99b33208d3bbb39
Parsa Ghadimi <i@parsa.ooo>
2024-09-16 08:49:13 -0400

    feat(b3fs): add more bucket writer api

--- lib/b3fs/src/bucket/dir/writer.rs
+++ lib/b3fs/src/bucket/dir/writer.rs
@@ -1,5 +1,7 @@
-use crate::bucket::Bucket;
-use crate::entry::OwnedEntry;
+use std::io::{self, Write};
+
+use crate::bucket::{errors, Bucket};
+use crate::entry::BorrowedEntry;
 
 /// A trusted directory writer.
 pub struct DirWriter {}
@@ -9,5 +11,20 @@ impl DirWriter {
         todo!()
     }
 
-    pub async fn insert(&mut self, entry: OwnedEntry) {}
+    pub async fn insert<'a>(
+        &mut self,
+        entry: impl Into<BorrowedEntry<'a>>,
+    ) -> Result<(), errors::InsertError> {
+        todo!()
+    }
+
+    /// Finalize this write and flush the data to the disk.
+    pub async fn commit(self) -> Result<[u8; 32], errors::CommitError> {
+        todo!()
+    }
+
+    /// Cancel this write and remove anything that this writer wrote to the disk.
+    pub async fn rollback(self) -> Result<(), io::Error> {
+        todo!()
+    }
 }
