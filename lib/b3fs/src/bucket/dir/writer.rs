cc516be92ac87d0be9213e9044aae0f4f970ef2b
Parsa Ghadimi <i@parsa.ooo>
2024-09-16 08:05:16 -0400

    wip: ContentHeader reader api

--- lib/b3fs/src/bucket/dir/writer.rs
+++ lib/b3fs/src/bucket/dir/writer.rs
@@ -1,7 +1,7 @@
 use crate::bucket::Bucket;
 use crate::entry::OwnedEntry;
 
-/// A trusted
+/// A trusted directory writer.
 pub struct DirWriter {}
 
 impl DirWriter {
--- lib/b3fs/src/bucket/mod.rs
+++ lib/b3fs/src/bucket/mod.rs
@@ -43,53 +43,165 @@
 //! [1]: file::reader::File
 //! [2]: dir::reader::Dir
 
-use std::io::Result;
+use std::io::{Error, ErrorKind, Result};
 use std::path::{Path, PathBuf};
 
-use crate::utils::Digest;
+use arrayref::array_ref;
+use rand::random;
+use tokio::fs::{self, File, OpenOptions};
+use tokio::io::AsyncReadExt;
+use triomphe::Arc;
+
+use crate::utils::{to_hex, Digest};
 
 pub mod dir;
 pub mod file;
 
+/// An open b3fs bucket which can be used for both reads and writes.
 pub struct Bucket {
     root: PathBuf,
+    // The cache of `$root/{blocks,headers,wal}`
+    blocks: PathBuf,
+    headers: PathBuf,
+    wal: PathBuf,
 }
 
-pub struct ContentHeader {}
+/// Any content from the b3fs blockstore, this content might be either a file or a directory.
+pub struct ContentHeader {
+    /// Set from the first byte of the file.
+    is_file: bool,
+    /// The number of entries for this content. For a file this is the number of blocks and for a
+    /// directory this is the number of entries.
+    ///
+    /// If the content is a directory header this is always smaller or equal to u16::MAX.
+    num_entries: u32,
+    /// The actual header file that is open.
+    header_file: Arc<File>,
+}
 
 impl Bucket {
     /// Open a bucket at the given path.
-    pub fn open<P: AsRef<Path>>(path: P) -> Result<Self> {
-        todo!()
+    pub async fn open<P: AsRef<Path>>(path: P) -> Result<Self> {
+        // turn the root path into an absolute path. this will prevent any bugs from changing the
+        // cwd after the bucket is opened.
+        let mut root_path = std::env::current_dir()?;
+        root_path.push(&path);
+        fs::create_dir_all(&root_path).await?;
+
+        let mut blocks_path = root_path.clone();
+        blocks_path.push("blocks");
+        fs::create_dir(&blocks_path).await;
+
+        let mut headers_path = root_path.clone();
+        headers_path.push("headers");
+        fs::create_dir(&headers_path).await;
+
+        let mut wal_path = root_path.clone();
+        wal_path.push("wal");
+        fs::create_dir(&wal_path).await;
+
+        Ok(Self {
+            root: root_path,
+            blocks: blocks_path,
+            headers: headers_path,
+            wal: wal_path,
+        })
     }
 
-    pub async fn get(&self, hash: &[u8; 32]) -> Result<Option<ContentHeader>> {
-        todo!()
+    /// Open a content with the given provided hash. Returns a [`ContentHeader`] on success.
+    pub async fn get(&self, hash: &[u8; 32]) -> Result<ContentHeader> {
+        let path = self.get_header_path(hash);
+        let mut file = OpenOptions::new().read(true).open(path).await?;
+        let mut buf = [0u8; 8];
+
+        // read the first 8 bytes of the file. the first 8 bytes is always the same format for both
+        // the directories and files. the second 4 byte i
+        let n = file.read_exact(&mut buf).await?;
+        debug_assert_eq!(n, 8);
+
+        let version = u32::from_le_bytes(*array_ref![buf, 0, 4]);
+        if version > 1 {
+            return Err(Error::new(
+                ErrorKind::InvalidData,
+                "unsupported b3fs header version",
+            ));
+        }
+
+        let is_file = version == 0;
+        let num_entries = u32::from_le_bytes(*array_ref![buf, 4, 4]);
+        if !is_file && num_entries > (u16::MAX as u32) {
+            return Err(Error::new(
+                ErrorKind::InvalidData,
+                "too many entries in b3fs directory.",
+            ));
+        }
+
+        Ok(ContentHeader {
+            is_file,
+            num_entries,
+            header_file: Arc::new(file),
+        })
     }
 
     pub fn get_block_path(&self, counter: u32, hash: &[u8; 32]) -> PathBuf {
-        todo!()
+        // TODO(qti3e): use the counter in file name.
+        let mut path = self.blocks.clone();
+        path.push(to_hex(hash).as_str());
+        path
     }
 
     pub fn get_header_path(&self, hash: &[u8; 32]) -> PathBuf {
-        todo!()
+        let mut path = self.headers.clone();
+        path.push(to_hex(hash).as_str());
+        path
+    }
+
+    pub(crate) fn get_new_wal_path(&self) -> PathBuf {
+        let mut path = self.headers.clone();
+        let name: [u8; 32] = random();
+        path.push(to_hex(&name).as_str());
+        path
     }
 }
 
 impl ContentHeader {
+    /// Returns true if this content is a file.
     pub fn is_file(&self) -> bool {
-        todo!()
+        self.is_file
     }
 
+    /// Returns true if this content is a directory layout.
     pub fn is_dir(&self) -> bool {
         !self.is_file()
     }
 
-    pub fn into_file(self) -> Option<file::reader::File> {
-        todo!()
+    /// If this content is a file returns a [B3File][file::reader::B3File].
+    pub fn into_file(self) -> Option<file::reader::B3File> {
+        self.is_file()
+            .then(|| file::reader::B3File::new(self.num_entries, self.header_file))
     }
 
-    pub fn into_dir(self) -> Option<dir::reader::Dir> {
-        todo!()
+    /// If this content is a directory returns a [B3Dir][dir::reader::B3Dir].
+    pub fn into_dir(self) -> Option<dir::reader::B3Dir> {
+        self.is_dir()
+            .then(|| dir::reader::B3Dir::new(self.num_entries, self.header_file))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::env::temp_dir;
+
+    use super::*;
+
+    #[tokio::test]
+    async fn open_should_work_multiple_times() {
+        let mut temp_dir = temp_dir();
+        temp_dir.push("b3fs-open-multiple-times");
+        assert!(Bucket::open(&temp_dir).await.is_ok());
+        assert!(Bucket::open(&temp_dir).await.is_ok());
+        fs::remove_dir_all(&temp_dir);
+        assert!(Bucket::open(&temp_dir).await.is_ok());
+        fs::remove_dir_all(&temp_dir);
     }
 }
