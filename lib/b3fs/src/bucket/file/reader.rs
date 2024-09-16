70257d21840df118d7aca93fe88fa8f6216d5bc0
Parsa Ghadimi <i@parsa.ooo>
2024-09-16 08:05:16 -0400

    wip: ContentHeader reader api

--- lib/b3fs/src/bucket/file/reader.rs
+++ lib/b3fs/src/bucket/file/reader.rs
@@ -1,8 +1,18 @@
+use tokio::fs::{self};
+use triomphe::Arc;
+
 use crate::collections::HashTree;
 
-pub struct File {}
+pub struct B3File {
+    num_blocks: u32,
+    file: Arc<fs::File>,
+}
 
-impl File {
+impl B3File {
+    pub(crate) fn new(num_blocks: u32, file: Arc<fs::File>) -> Self {
+        Self { num_blocks, file }
+    }
+
     pub fn hashtree(&self) -> HashTree {
         todo!()
     }
