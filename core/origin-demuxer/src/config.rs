1fa2a4534557c96ec5609d75e392429943979777
Juan Pablo Royo Sales <juanpablo.royo@gmail.com>
2024-09-23 18:00:12 +0200

    feat: adding placeholders for origin-b3fs

--- core/origin-demuxer/src/config.rs
+++ core/origin-demuxer/src/config.rs
@@ -4,4 +4,5 @@ use serde::{Deserialize, Serialize};
 pub struct Config {
     pub http: lightning_origin_http::Config,
     pub ipfs: lightning_origin_ipfs::Config,
+    pub b3fs: lightning_origin_b3fs::Config,
 }
