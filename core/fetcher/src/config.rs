1dd47e1eb08b410dc948df3431a03a1001cb5bbf
Juan Pablo Royo Sales <juanpablo.royo@gmail.com>
2024-09-25 14:46:00 +0200

    feat: removing origin-demuxer

--- core/fetcher/src/config.rs
+++ core/fetcher/src/config.rs
@@ -1,15 +1,21 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone)]
 pub struct Config {
     // Maximum number of concurrent origin requests we send out.
     pub max_conc_origin_req: usize,
+    pub http: lightning_origin_http::Config,
+    pub ipfs: lightning_origin_ipfs::Config,
+    pub b3fs: lightning_origin_b3fs::Config,
 }
 
 impl Default for Config {
     fn default() -> Self {
         Self {
             max_conc_origin_req: 5,
+            http: lightning_origin_http::Config::default(),
+            ipfs: lightning_origin_ipfs::Config::default(),
+            b3fs: lightning_origin_b3fs::Config::default(),
         }
     }
 }
