c1f9ba136a11330cbf7c1e1d90e92dd7fedaf9eb
ozwaldorf <self@ossian.dev>
2024-10-04 18:43:00 -0400

    feat(sgxkit): main attribute macro to init panic handlers and print results

--- lib/sgxkit/examples/hello.rs
+++ lib/sgxkit/examples/hello.rs
@@ -2,6 +2,7 @@ use std::io::Write;
 
 use sgxkit::io::output_writer;
 
+#[sgxkit::main]
 fn main() {
     let mut writer = output_writer();
     writer
--- lib/sgxkit/src/lib.rs
+++ lib/sgxkit/src/lib.rs
@@ -1,3 +1,6 @@
 pub mod crypto;
 pub mod error;
 pub mod io;
+
+pub use sgxkit_macros::*;
+pub use sgxkit_sys::*;
