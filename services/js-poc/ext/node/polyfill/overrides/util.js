287283506e2d10b37f65fcb2463ec50104eb7471
Miguel <miguel@fleek.xyz>
2024-10-08 12:07:37 -0400

    chore: remove polyfills

--- services/js-poc/ext/node/polyfill/overrides/util.js
+++
@@ -1,4 +0,0 @@
-export * from 'https://esm.sh/v135/util@0.12.5/es2022/util.mjs'
-// We have these globally available, but the polyfill doesn't set them
-export const TextEncoder = globalThis.TextEncoder;
-export const TextDecoder = globalThis.TextDecoder;
