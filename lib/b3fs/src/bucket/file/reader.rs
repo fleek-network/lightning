1449c159f4c975f200d486869060f13670d4cc0c
Juan Pablo Royo Sales <juanpablo.royo@gmail.com>
2024-10-09 11:39:38 +0200

    feat: implement asynchashtree

--- lib/b3fs/src/bucket/file/reader.rs
+++ lib/b3fs/src/bucket/file/reader.rs
@@ -4,35 +4,68 @@ use tokio::io::AsyncReadExt;
 use triomphe::Arc;
 
 use crate::bucket::errors;
+use crate::collections::tree::AsyncHashTree;
 use crate::collections::HashTree;
 use crate::hasher::b3::KEY_LEN;
 
 pub struct B3File {
     num_blocks: u32,
     file: Arc<fs::File>,
-    content: Vec<[u8; 32]>,
 }
 
 impl B3File {
     pub(crate) fn new(num_blocks: u32, file: Arc<fs::File>) -> Self {
-        Self {
-            num_blocks,
-            file,
-            content: Vec::with_capacity(num_blocks as usize),
-        }
+        Self { num_blocks, file }
     }
 
-    pub async fn hashtree(&mut self) -> Result<HashTree<'_>, errors::ReadError> {
-        let mut read = triomphe::Arc::<tokio::fs::File>::get_mut(&mut self.file)
-            .ok_or(errors::ReadError::RefFile)?;
-        let mut bytes = BytesMut::with_capacity(KEY_LEN);
-        while read.read_exact(&mut bytes).await.is_ok() {
-            let mut key = [0u8; KEY_LEN];
-            key.copy_from_slice(&bytes);
-            self.content.push(key);
-            bytes.clear();
+    pub async fn hashtree(&mut self) -> Result<AsyncHashTree<&mut fs::File>, errors::ReadError> {
+        let hash = triomphe::Arc::<tokio::fs::File>::get_mut(&mut self.file)
+            .ok_or(errors::ReadError::RefFile)
+            .map(|r| AsyncHashTree::new(r, self.num_blocks))?;
+        Ok(hash)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::env::temp_dir;
+    use std::fmt::write;
+
+    use rand::random;
+    use tokio::io::{AsyncRead, AsyncWriteExt};
+    use tokio_stream::StreamExt;
+    use triomphe::Arc;
+
+    use super::*;
+    use crate::utils;
+
+    #[tokio::test]
+    async fn test_hashtree() {
+        let temp_file_name = random::<[u8; 32]>();
+        let temp_dir = temp_dir();
+        let file_name = temp_dir.join(format!("test_hashtree_{}", utils::to_hex(&temp_file_name)));
+        let mut file = fs::OpenOptions::new()
+            .write(true)
+            .create(true)
+            .truncate(true)
+            .open(file_name.clone())
+            .await
+            .unwrap();
+        let num_blocks = 10;
+        let mut data = BytesMut::with_capacity(num_blocks as usize * KEY_LEN);
+        for _ in 0..(num_blocks * 2) - 2 {
+            data.extend_from_slice(&random::<[u8; KEY_LEN]>());
         }
-        <&Vec<[u8; 32]> as TryInto<HashTree>>::try_into(&self.content)
-            .map_err(|_| errors::ReadError::HashTreeConversion)
+        file.write_all(&data).await.unwrap();
+
+        let file = fs::File::open(file_name.clone()).await.unwrap();
+        let file_arc = Arc::new(file);
+        let mut b3file = B3File::new(num_blocks, file_arc);
+        let mut hash = b3file.hashtree().await.unwrap();
+        for _ in 0..num_blocks {
+            let _ = hash.next().await.unwrap();
+        }
+        assert!(hash.next().await.is_none());
+        fs::remove_file(file_name).await.unwrap();
     }
 }
