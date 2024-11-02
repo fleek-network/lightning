120428e95a3d92e0b8b30399705ebaf7d421f881
Steven Normore <snormore@gmail.com>
2024-11-02 10:21:41 -0400

    test(consensus): execute tx from non committee node

--- core/consensus/tests/consensus.rs
+++ core/consensus/tests/consensus.rs
@@ -8,7 +8,7 @@ use lightning_utils::poll::{poll_until, PollUntilError};
 use types::{ExecuteTransactionOptions, ExecutionData, TransactionResponse, UpdateMethod};
 
 #[tokio::test]
-async fn test_execute_transaction() {
+async fn test_execute_transaction_as_committee_node() {
     let mut network = TestNetwork::builder()
         .with_real_consensus()
         .with_committee_nodes::<TestFullNodeComponentsWithRealConsensus>(4)
@@ -45,6 +45,67 @@ async fn test_execute_transaction() {
                 .nodes()
                 .all(|node| {
                     let nonce = node.app_query().get_node_info(&0, |node| node.nonce);
+                    // When transactions are submitted immediately after startup, they may fail to
+                    // initially make it to the mempool, in which case it will timeout and be
+                    // retried, with a backfill of the first nonce. So we need to check for either
+                    // nonce 1 or 2 (in case of retry).
+                    nonce == Some(1) || nonce == Some(2)
+                })
+                .then_some(())
+                .ok_or(PollUntilError::ConditionNotSatisfied)
+        },
+        Duration::from_secs(3),
+        Duration::from_millis(100),
+    )
+    .await
+    .unwrap();
+
+    // Shutdown the network.
+    network.shutdown().await;
+}
+
+#[tokio::test]
+async fn test_execute_transaction_as_non_committee_node() {
+    let mut network = TestNetwork::builder()
+        .with_real_consensus()
+        .with_committee_nodes::<TestFullNodeComponentsWithRealConsensus>(4)
+        .await
+        .with_non_committee_nodes::<TestFullNodeComponentsWithRealConsensus>(1)
+        .await
+        .build()
+        .await
+        .unwrap();
+
+    // Execute an increment nonce transaction from the non-committee node.
+    let non_committee_node = network.non_committee_nodes()[0];
+    let (_, receipt) = non_committee_node
+        .execute_transaction_from_node(
+            UpdateMethod::IncrementNonce {},
+            Some(ExecuteTransactionOptions {
+                // Transactions that are submitted immediately after startup will sometimes
+                // timeout and need to be retried.
+                wait: types::ExecuteTransactionWait::Receipt,
+                retry: types::ExecuteTransactionRetry::Always(Some(10)),
+                timeout: Some(Duration::from_secs(2)),
+            }),
+        )
+        .await
+        .unwrap()
+        .as_receipt();
+    assert_eq!(
+        receipt.response,
+        TransactionResponse::Success(ExecutionData::None)
+    );
+
+    // Check that the node nonce was incremented across the network.
+    poll_until(
+        || async {
+            network
+                .nodes()
+                .all(|node| {
+                    let nonce = node
+                        .app_query()
+                        .get_node_info(&non_committee_node.index(), |node| node.nonce);
                     // When transactions are submitted immediately after startup, they may fail to
                     // initially make it to the mempool, in which case it will timeout and be
                     // retried, with a backfill of the first nonce. So we need to check for either
