2dc1ba08b836b7d3df16812b23efe9f9c69b64db
Juan Pablo Royo Sales <juanpablo.royo@gmail.com>
2024-11-05 20:36:37 +0100

    feat: refactor stream

--- lib/b3fs/src/bucket/dir/iter.rs
+++ lib/b3fs/src/bucket/dir/iter.rs
@@ -1,15 +1,18 @@
 use std::future::Future;
 use std::mem;
 use std::pin::Pin;
+use std::sync::Arc;
 use std::task::{Context, Poll};
 
 use tokio::fs::File;
 use tokio::io::{AsyncBufReadExt, AsyncReadExt, AsyncSeekExt, BufReader};
+use tokio::sync::RwLock;
 use tokio_stream::Stream;
 
 use crate::bucket::errors;
 use crate::entry::{BorrowedEntry, BorrowedLink};
 use crate::hasher::dir_hasher::B3_DIR_IS_SYM_LINK;
+use crate::on_future;
 
 /// State machine states for iterating through directory entries
 enum State {
@@ -25,22 +28,23 @@ enum State {
     ReadPath,
     /// End of file reached
     Eof,
+    WaitingReadPosition(Pin<Box<dyn Future<Output = Result<u64, std::io::Error>>>>),
+    WaitingReadFlag(Pin<Box<dyn Future<Output = Result<u8, std::io::Error>>>>),
+    WaitingReadEntryName(Pin<Box<dyn Future<Output = Result<Vec<u8>, std::io::Error>>>>),
+    WaitingReadContent(Pin<Box<dyn Future<Output = Result<[u8; 32], std::io::Error>>>>),
+    WaitingReadPath(Pin<Box<dyn Future<Output = Result<Vec<u8>, std::io::Error>>>>),
 }
 
 /// Iterator over entries in a B3 directory
 pub struct DirEntriesIter<'a> {
     /// Buffered reader for the directory file
-    reader: BufReader<File>,
+    reader: Arc<RwLock<BufReader<File>>>,
     /// Current position in the file
     position: u64,
     /// Current state of the iterator state machine
     state: State,
     /// Buffer for entry name being read
     entry_name: Vec<u8>,
-    /// Buffer for content hash being read
-    current_buffer_content: [u8; 32],
-    /// Buffer for symlink path being read
-    current_buffer_link: Vec<u8>,
     /// Entry type flag
     flag: u8,
     /// Phantom data for lifetime 'a
@@ -52,31 +56,14 @@ impl<'a> DirEntriesIter<'a> {
     pub async fn new(reader: File, position_start_entries: u64) -> Result<Self, errors::ReadError> {
         let mut reader = BufReader::new(reader);
         Ok(Self {
-            reader,
+            reader: Arc::new(RwLock::new(reader)),
             position: position_start_entries,
             state: State::ReadPosition,
             entry_name: Vec::new(),
-            current_buffer_content: [0u8; 32],
-            current_buffer_link: Vec::new(),
             flag: 0,
             _marker: std::marker::PhantomData,
         })
     }
-}
-
-/// Macro for handling EOF conditions when reading from the directory file
-#[macro_use]
-macro_rules! check_eof {
-    ($self:expr, $flag:expr) => {
-        if let Err(e) = $flag {
-            if e.kind() == std::io::ErrorKind::UnexpectedEof {
-                $self.state = State::Eof;
-                return Poll::Ready(None);
-            } else {
-                return Poll::Ready(Some(Err(errors::ReadError::from(e))));
-            }
-        }
-    };
 }
 
 impl<'a> Stream for DirEntriesIter<'a> {
@@ -86,134 +73,147 @@ impl<'a> Stream for DirEntriesIter<'a> {
     fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
         let this = self.as_mut().get_mut();
 
-        loop {
-            match this.state {
-                State::Eof => return Poll::Ready(None),
-                State::ReadPosition => {
-                    // Reset buffers and seek to next entry position
-                    this.entry_name.clear();
-                    this.current_buffer_content = [0u8; 32];
-                    this.current_buffer_link.clear();
-                    this.flag = 0;
-                    let future = this.reader.seek(tokio::io::SeekFrom::Start(this.position));
-                    let mut future_mut = std::pin::pin!(future);
-                    match future_mut.as_mut().poll(cx) {
-                        Poll::Ready(result) => {
-                            check_eof!(this, result);
-                            this.state = State::ReadFlag;
-                        },
-                        Poll::Ready(Err(e)) => {
-                            return Poll::Ready(Some(Err(errors::ReadError::from(e))));
-                        },
-                        Poll::Pending => return Poll::Pending,
-                    }
-                },
-                State::ReadFlag => {
-                    // Read entry type flag
-                    let future = this.reader.read_u8();
-                    let mut future_mut = std::pin::pin!(future);
-                    match future_mut.as_mut().poll(cx) {
-                        Poll::Ready(result) => {
-                            check_eof!(this, result);
-                            match result {
-                                Ok(flag_result) => {
-                                    this.flag = flag_result;
-                                    this.state = State::ReadEntryName;
-                                },
-                                Err(e) => {
-                                    return Poll::Ready(Some(Err(errors::ReadError::from(e))));
-                                },
-                            }
-                        },
-                        Poll::Pending => return Poll::Pending,
-                    }
-                },
-                State::ReadEntryName => {
-                    // Read entry name until null terminator
-                    let future = this.reader.read_until(0x00, &mut this.entry_name);
-                    let mut future_mut = std::pin::pin!(future);
-                    match future_mut.as_mut().poll(cx) {
-                        Poll::Ready(result) => {
-                            check_eof!(this, result);
-                            match result {
-                                Ok(_) => {
-                                    this.entry_name.pop();
-                                    if this.flag == B3_DIR_IS_SYM_LINK {
-                                        this.state = State::ReadPath;
-                                    } else {
-                                        this.state = State::ReadContent;
-                                    }
-                                },
-                                Err(e) => {
-                                    return Poll::Ready(Some(Err(errors::ReadError::from(e))));
-                                },
-                            }
-                        },
-                        Poll::Pending => return Poll::Pending,
-                    }
-                },
-                State::ReadContent => {
-                    // Read content hash for regular entries
-                    let future = this.reader.read_exact(&mut this.current_buffer_content);
-                    let mut future_mut = std::pin::pin!(future);
-                    match future_mut.as_mut().poll(cx) {
-                        Poll::Ready(result) => {
-                            check_eof!(this, result);
-                            match result {
-                                Ok(_) => {
-                                    this.state = State::ReadPosition;
-                                    this.position += this.entry_name.len() as u64
-                                        + this.current_buffer_content.len() as u64
-                                        + 2;
-                                    let static_slice: &'static [u8; 32] = unsafe {
-                                        mem::transmute_copy(&this.current_buffer_content)
-                                    };
-                                    return Poll::Ready(Some(Ok(BorrowedEntry {
-                                        name: Box::leak(this.entry_name.clone().into_boxed_slice()),
-                                        link: BorrowedLink::Content(static_slice),
-                                    })));
-                                },
-                                Err(e) => {
-                                    return Poll::Ready(Some(Err(errors::ReadError::from(e))));
-                                },
-                            }
-                        },
-                        Poll::Pending => return Poll::Pending,
-                    }
-                },
-                State::ReadPath => {
-                    // Read symlink path until null terminator
-                    let future = this.reader.read_until(0x00, &mut this.current_buffer_link);
-                    let mut future_mut = std::pin::pin!(future);
-                    match future_mut.as_mut().poll(cx) {
-                        Poll::Ready(result) => {
-                            check_eof!(this, result);
-                            match result {
-                                Ok(_) => {
-                                    this.state = State::ReadPosition;
-                                    this.position += this.entry_name.len() as u64
-                                        + this.current_buffer_link.len() as u64
-                                        + 2;
-                                    let content = this.current_buffer_link
-                                        [..this.current_buffer_link.len() - 1]
-                                        .to_vec()
-                                        .into_boxed_slice();
-                                    let static_slice: &'static [u8] = Box::leak(content);
-                                    return Poll::Ready(Some(Ok(BorrowedEntry {
-                                        name: Box::leak(this.entry_name.clone().into_boxed_slice()),
-                                        link: BorrowedLink::Path(static_slice),
-                                    })));
-                                },
-                                Err(e) => {
-                                    return Poll::Ready(Some(Err(errors::ReadError::from(e))));
-                                },
-                            }
-                        },
-                        Poll::Pending => return Poll::Pending,
-                    }
-                },
-            }
+        match this.state {
+            State::Eof => Poll::Ready(None),
+            State::ReadPosition => {
+                // Reset buffers and seek to next entry position
+                this.entry_name.clear();
+                this.flag = 0;
+                let file = this.reader.clone();
+                let pos = this.position;
+                let waker = cx.waker().clone();
+                let future = async move {
+                    let result = file
+                        .write()
+                        .await
+                        .seek(tokio::io::SeekFrom::Start(pos))
+                        .await;
+                    waker.wake();
+                    result
+                };
+                let future = Box::pin(future);
+                this.state = State::WaitingReadPosition(future);
+                cx.waker().wake_by_ref();
+                Poll::Pending
+            },
+            State::WaitingReadPosition(ref mut future) => {
+                let result = future.as_mut().poll(cx);
+                on_future!(this, result, State::Eof, |r| {
+                    this.state = State::ReadFlag;
+                    cx.waker().wake_by_ref();
+                    Poll::Pending
+                })
+            },
+            State::ReadFlag => {
+                let file = this.reader.clone();
+                let waker = cx.waker().clone();
+                let future = async move {
+                    let result = file.write().await.read_u8().await;
+                    waker.wake();
+                    result
+                };
+                let future = Box::pin(future);
+                this.state = State::WaitingReadFlag(future);
+                cx.waker().wake_by_ref();
+                Poll::Pending
+            },
+            State::WaitingReadFlag(ref mut future) => {
+                let result = future.as_mut().poll(cx);
+                on_future!(this, result, State::Eof, |r| {
+                    this.state = State::ReadEntryName;
+                    this.flag = r;
+                    cx.waker().wake_by_ref();
+                    Poll::Pending
+                })
+            },
+            State::ReadEntryName => {
+                // Read entry name until null terminator
+                let file = this.reader.clone();
+                let waker = cx.waker().clone();
+                let future = async move {
+                    let mut buffer = Vec::new();
+                    file.write().await.read_until(0x00, &mut buffer).await?;
+                    waker.wake();
+                    Ok(buffer)
+                };
+                let future = Box::pin(future);
+                this.state = State::WaitingReadEntryName(future);
+                cx.waker().wake_by_ref();
+                Poll::Pending
+            },
+            State::WaitingReadEntryName(ref mut future) => {
+                let result = future.as_mut().poll(cx);
+                let state = if this.flag & B3_DIR_IS_SYM_LINK != 0 {
+                    State::ReadPath
+                } else {
+                    State::ReadContent
+                };
+                on_future!(this, result, State::Eof, |r| {
+                    this.state = state;
+                    this.entry_name = r;
+                    this.entry_name.pop();
+                    cx.waker().wake_by_ref();
+                    Poll::Pending
+                })
+            },
+            State::ReadContent => {
+                // Read content hash for regular entries
+                let file = this.reader.clone();
+                let waker = cx.waker().clone();
+                let future = async move {
+                    let mut buffer = [0u8; 32];
+                    file.write().await.read_exact(&mut buffer).await?;
+                    waker.wake();
+                    Ok(buffer)
+                };
+                let future = Box::pin(future);
+                this.state = State::WaitingReadContent(future);
+                cx.waker().wake_by_ref();
+                Poll::Pending
+            },
+            State::WaitingReadContent(ref mut fut) => {
+                let result = fut.as_mut().poll(cx);
+                on_future!(this, result, State::Eof, |buffer_content: [u8; 32]| {
+                    this.state = State::ReadPosition;
+                    this.position += this.entry_name.len() as u64 + buffer_content.len() as u64 + 2;
+                    let static_slice: &'static [u8; 32] =
+                        unsafe { mem::transmute_copy(&buffer_content) };
+                    Poll::Ready(Some(Ok(BorrowedEntry {
+                        name: Box::leak(this.entry_name.clone().into_boxed_slice()),
+                        link: BorrowedLink::Content(static_slice),
+                    })))
+                })
+            },
+            State::ReadPath => {
+                // Read symlink path until null terminator
+                let file = this.reader.clone();
+                let waker = cx.waker().clone();
+                let future = async move {
+                    let mut buffer = Vec::new();
+                    file.write().await.read_until(0x00, &mut buffer).await?;
+                    waker.wake();
+                    Ok(buffer)
+                };
+                let future = Box::pin(future);
+                this.state = State::WaitingReadPath(future);
+                cx.waker().wake_by_ref();
+                Poll::Pending
+            },
+            State::WaitingReadPath(ref mut future) => {
+                let result = future.as_mut().poll(cx);
+                on_future!(this, result, State::Eof, |buffer_link: Vec<u8>| {
+                    this.state = State::ReadPosition;
+                    this.position += this.entry_name.len() as u64 + buffer_link.len() as u64 + 2;
+                    let content = buffer_link[..buffer_link.len() - 1]
+                        .to_vec()
+                        .into_boxed_slice();
+                    let static_slice: &'static [u8] = Box::leak(content);
+                    Poll::Ready(Some(Ok(BorrowedEntry {
+                        name: Box::leak(this.entry_name.clone().into_boxed_slice()),
+                        link: BorrowedLink::Path(static_slice),
+                    })))
+                })
+            },
         }
-        Poll::Ready(None)
     }
 }
