71ffe45e4a57ec9ad9c3c07f1be91a9d6e38cf84
Juan Pablo Royo Sales <juanpablo.royo@gmail.com>
2024-12-18 13:30:48 +0100

    fix: fixing bug on uwriter

--- lib/b3fs/src/bucket/dir/uwriter.rs
+++ lib/b3fs/src/bucket/dir/uwriter.rs
@@ -94,6 +94,8 @@ impl UntrustedDirWriter {
 #[cfg(test)]
 mod tests {
 
+    use core::num;
+
     use super::*;
     use crate::bucket::dir::tests::setup_bucket;
     use crate::bucket::tests::get_random_file;
@@ -105,29 +107,33 @@ mod tests {
     use crate::stream::walker::Mode;
     use crate::test_utils::*;
 
-    fn setup_hasher() -> (Vec<[u8; 32]>, [u8; 32], OwnedEntry) {
-        let mut hasher: Blake3Hasher<Vec<[u8; 32]>> = Blake3Hasher::default();
-        let block = get_random_file(8192 * 2);
-        hasher.update(&block[..]);
-
-        let (ref mut hashes, root) = hasher.finalize_tree();
-        hashes.push(root);
+    fn setup_hasher(num_entries: usize) -> (Vec<[u8; 32]>, [u8; 32], Vec<OwnedEntry>) {
         let mut dir_hasher = DirectoryHasher::default();
-        let entry = OwnedEntry {
-            name: "test_file.txt".as_bytes().into(),
-            link: OwnedLink::Content(root),
-        };
-        let borrowed = BorrowedEntry::from(&entry);
-        dir_hasher.insert_unchecked(borrowed);
+        let mut entries = vec![];
+        for i in 0..num_entries {
+            let mut hasher: Blake3Hasher<Vec<[u8; 32]>> = Blake3Hasher::default();
+            let block = get_random_file(8192 * 2);
+            hasher.update(&block[..]);
+
+            let (ref mut hashes, root) = hasher.finalize_tree();
+            hashes.push(root);
+            let entry = OwnedEntry {
+                name: format!("test_file_{i}.txt").as_bytes().into(),
+                link: OwnedLink::Content(root),
+            };
+            let borrowed = BorrowedEntry::from(&entry);
+            dir_hasher.insert_unchecked(borrowed);
+            entries.push(entry);
+        }
         let (root, tree) = dir_hasher.finalize();
-        (tree, root, entry)
+        (tree, root, entries)
     }
 
     #[tokio::test]
     async fn test_untrusted_dir_writer_insert() {
         let bucket = setup_bucket().await.unwrap();
         let num_entries = 1;
-        let (hashes, root, entry) = setup_hasher();
+        let (hashes, root, entry) = setup_hasher(num_entries);
         let hashtree = HashTree::try_from(&hashes).unwrap();
 
         let mut writer = UntrustedDirWriter::new(&bucket, num_entries, root)
@@ -137,12 +143,12 @@ mod tests {
         writer
             .feed_proof(
                 hashtree
-                    .generate_proof(Mode::from_is_initial(false), 1)
+                    .generate_proof(Mode::from_is_initial(true), 0)
                     .as_slice(),
             )
             .await
             .unwrap();
-        let result = writer.insert(&entry, true).await;
+        let result = writer.insert(entry.first().unwrap(), true).await;
         assert!(result.is_ok());
     }
 
@@ -150,24 +156,18 @@ mod tests {
     async fn test_untrusted_dir_writer_commit() {
         let bucket = setup_bucket().await.unwrap();
         let num_entries = 1;
-        let (hashes, root, entry) = setup_hasher();
-        dbg!(&hashes);
+        let (hashes, root, entry) = setup_hasher(num_entries);
         let hashtree = HashTree::try_from(&hashes).unwrap();
 
         let mut writer = UntrustedDirWriter::new(&bucket, num_entries, root)
             .await
             .unwrap();
 
-        writer
-            .feed_proof(
-                hashtree
-                    .generate_proof(Mode::from_is_initial(false), 1)
-                    .as_slice(),
-            )
-            .await
-            .unwrap();
+        let proof = hashtree.generate_proof(Mode::from_is_initial(true), 0);
 
-        writer.insert(&entry, true).await.unwrap();
+        writer.feed_proof(proof.as_slice()).await.unwrap();
+
+        writer.insert(entry.first().unwrap(), true).await.unwrap();
 
         let result = writer.commit().await;
         assert!(result.is_ok());
@@ -178,7 +178,7 @@ mod tests {
     async fn test_untrusted_dir_writer_rollback() {
         let bucket = setup_bucket().await.unwrap();
         let num_entries = 1;
-        let (hashes, root, entry) = setup_hasher();
+        let (hashes, root, entry) = setup_hasher(num_entries);
         let hashtree = HashTree::try_from(&hashes).unwrap();
 
         let mut writer = UntrustedDirWriter::new(&bucket, num_entries, root)
@@ -188,15 +188,36 @@ mod tests {
         writer
             .feed_proof(
                 hashtree
-                    .generate_proof(Mode::from_is_initial(false), 1)
+                    .generate_proof(Mode::from_is_initial(true), 0)
                     .as_slice(),
             )
             .await
             .unwrap();
 
-        writer.insert(&entry, true).await.unwrap();
+        writer.insert(entry.first().unwrap(), true).await.unwrap();
 
         let result = writer.rollback().await;
         assert!(result.is_ok());
+    }
+    #[tokio::test]
+    async fn test_untrusted_dir_writer_several_entries() {
+        let bucket = setup_bucket().await.unwrap();
+        let num_entries = 3;
+        let (hashes, root, entries) = setup_hasher(num_entries);
+        let hashtree = HashTree::try_from(&hashes).unwrap();
+
+        let mut writer = UntrustedDirWriter::new(&bucket, num_entries, root)
+            .await
+            .unwrap();
+
+        for (i, entry) in entries.iter().enumerate() {
+            let generate_proof = hashtree.generate_proof(Mode::from_is_initial(i == 0), i);
+            writer.feed_proof(generate_proof.as_slice()).await.unwrap();
+
+            writer.insert(entry, i + 1 == num_entries).await.unwrap();
+        }
+        let result = writer.commit().await;
+        assert!(result.is_ok());
+        assert_eq!(result.unwrap().len(), 32);
     }
 }
