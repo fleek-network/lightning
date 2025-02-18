bc6c9ff99cbc387a34cd56622d526b3e4f5fa7b5
M. Meier <m.wiresurfer@gmail.com>
2025-02-18 17:06:17 -0500

    Use frequency to decide when to execute a job

--- core/watcher/src/watcher.rs
+++ core/watcher/src/watcher.rs
@@ -5,34 +5,51 @@ use fleek_crypto::NodePublicKey;
 use futures::StreamExt;
 use lightning_interfaces::schema::task_broker::{TaskRequest, TaskResponse, TaskScope};
 use lightning_interfaces::types::{ExecuteTransactionRequest, JobInfo, JobStatus};
-use lightning_interfaces::{c, NodeComponents, SignerSubmitTxSocket, TaskError, WatcherInterface};
+use lightning_interfaces::{
+    c,
+    NodeComponents,
+    NotifierInterface,
+    SignerSubmitTxSocket,
+    TaskError,
+    WatcherInterface,
+};
 use lightning_types::UpdateMethod;
 use tokio::sync::mpsc;
 use tokio::sync::mpsc::Sender;
 use tokio::task::{JoinError, JoinHandle};
+use tokio::time::{Instant, Interval};
 
 pub struct Watcher<C: NodeComponents> {
-    app_query: QueryRunner,
+    app_query: c!(C::ApplicationInterface::SyncExecutor),
     task_broker: c!(C::TaskBrokerInterface),
     signer: SignerSubmitTxSocket,
     pk: NodePublicKey,
+    notifier: C::NotifierInterface,
     tasks:
         FuturesUnordered<JoinHandle<([u8; 32], Vec<std::result::Result<TaskResponse, TaskError>>)>>,
+    interval_counter: u32,
+    interval: Interval,
 }
 
 impl<C: NodeComponents> Watcher<C> {
     pub fn new(
-        app_query: QueryRunner,
+        app_query: c!(C::ApplicationInterface::SyncExecutor),
         task_broker: c!(C::TaskBrokerInterface),
         signer: SignerSubmitTxSocket,
+        notifier: C::NotifierInterface,
         pk: NodePublicKey,
     ) -> Self {
+        let duration = app_query.get_time_interval();
+        let interval = tokio::time::interval(Duration::from_millis(duration));
+
         Self {
             app_query,
             signer,
             pk,
             task_broker,
+            notifier,
             tasks: FuturesUnordered::new(),
+            interval,
         }
     }
 
@@ -41,28 +58,16 @@ impl<C: NodeComponents> Watcher<C> {
             if let Some(jobs) = self.app_query.get_jobs_for_node(&index) {
                 for job in jobs {
                     let JobInfo {
-                        service, arguments, ..
+                        service,
+                        arguments,
+                        frequency,
+                        ..
                     } = job.info;
 
-                    // Arguments should have this information.
-                    /*
-                    pub struct Request {
-                        /// Origin to use
-                        pub origin: Origin,
-                        /// URI For the origin
-                        /// - for blake3 should be hex encoded bytes
-                        /// - for ipfs should be cid string
-                        pub uri: String,
-                        /// Optional path to provide as the window location,
-                        /// including query parameters and the fragment.
-                        pub path: Option<String>,
-                        /// Parameter to pass to the script's main function.
-                        /// For http oriented functions, an object can be passed
-                        /// here to simulate the http object with the fields for
-                        /// method, headers, query params, and the url fragment
-                        pub param: Option<serde_json::Value>,
+                    if self.interval_counter % frequency != 0 {
+                        continue;
                     }
-                     */
+
                     let job_hash = job.hash;
                     let request = TaskRequest {
                         service,
@@ -124,37 +129,49 @@ impl<C: NodeComponents> Watcher<C> {
         }
     }
 
-    pub fn spawn(mut self) -> Sender<()> {
-        let (tx, mut rx) = mpsc::channel(1);
+    /// Increments the counter.
+    fn update_counter(&mut self) {
+        self.interval_counter += 1;
+    }
 
-        tokio::spawn(async move {
-            // We assume that the duration unit is milliseconds.
-            let duration = self.app_query.get_time_interval();
-            let mut interval = tokio::time::interval(Duration::from_millis(duration));
-            loop {
-                tokio::select! {
-                    biased;
-                    shutdown = rx.recv() => {
-                        if shutdown.is_none() {
-                            break;
+    /// Resets the counter and recomputes the interval.
+    fn reset(&mut self) {
+        self.interval_counter = 0;
+        let duration = self.app_query.get_time_interval();
+        self.interval = tokio::time::interval(Duration::from_millis(duration));
+    }
+
+    fn tick(&mut self) -> Instant {
+        self.interval.as_mut().unwrap().tick()
+    }
+
+    pub async fn start(mut self) {
+        self.reset();
+
+        let mut epoch_changed_sub = self.notifier.subscribe_epoch_changed();
+
+        loop {
+            tokio::select! {
+                biased;
+                _ = epoch_changed_sub.recv() => {
+                    self.reset();
+                }
+                _ = self.interval.tick() => {
+                    self.update_counter();
+                    self.perform_scheduled_work();
+                }
+                Some(next) = self.tasks.next() => {
+                    match next {
+                        Ok((hash, result)) => {
+                            self.process_finished_jobs(hash, result);
                         }
-                    }
-                    _ = interval.tick() => {
-                        self.perform_scheduled_work();
-                    }
-                    Some(next) = self.tasks.next() => {
-                        match next {
-                            Ok((hash, result)) => {
-                                self.process_finished_jobs(hash, result);
-                            }
-                            Err(e) => {
-                                tracing::warn!("scheduled job panicked!")
-                            }
+                        Err(e) => {
+                            tracing::warn!("scheduled job panicked!")
                         }
                     }
                 }
             }
-        });
+        }
 
         tx
     }
