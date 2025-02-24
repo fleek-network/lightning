cf7f41a6723aa144a7a95c5d8d1d91bdc091459d
Matthias Wright <matthias.s.wright@gmail.com>
2025-02-25 03:08:05 +0700

    chore(fleek-crypto): remove expects from signature verification

--- lib/fleek-crypto/src/transaction.rs
+++ lib/fleek-crypto/src/transaction.rs
@@ -60,13 +60,13 @@ impl TransactionSender {
     pub fn verify(&self, signature: TransactionSignature, digest: &[u8; 32]) -> bool {
         match (self, signature) {
             (TransactionSender::NodeConsensus(pk), TransactionSignature::NodeConsensus(sig)) => {
-                pk.verify(&sig, digest).expect("invalid signature")
+                pk.verify(&sig, digest).unwrap_or(false)
             },
             (TransactionSender::AccountOwner(pk), TransactionSignature::AccountOwner(sig)) => {
                 pk.verify(&sig, digest)
             },
             (TransactionSender::NodeMain(pk), TransactionSignature::NodeMain(sig)) => {
-                pk.verify(&sig, digest).expect("invalid signature")
+                pk.verify(&sig, digest).unwrap_or(false)
             },
             _ => false,
         }
